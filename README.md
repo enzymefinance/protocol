d9ff048d25e6a86e2d3534e5cc78a487a3285667
retotrinkler <reto.trinkler@gmail.com>
2016-11-05 01:14:53 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # protocol
 Melon Protocol Specification
+
+[![Gitter](https://badges.gitter.im/melonproject/general.svg)](https://gitter.im/melonproject/general?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+## Installation
+
+1. Clone this repository
+    ```
+    git clone git@github.com:melonproject/protocol.git
+    cd protocol
+    ```
+
+2. Install dependencies, such as [Truffle](https://github.com/ConsenSys/truffle) (requires NodeJS 5.0+) and [Testrpc](https://github.com/ethereumjs/testrpc):
+    ```
+    npm install
+    ```
---
+++ truffle.js
@@ -0,0 +1,37 @@
+module.exports = {
+  build: {
+    "index.html": "index.html",
+    "app.js": [
+      "javascripts/app.js"
+    ],
+    "app.css": [
+      "stylesheets/app.css"
+    ],
+    "images/": "images/"
+  },
+  rpc: {
+    host: "localhost",
+    port: 8545
+  },
+  networks: {
+    "live": {
+      network_id: 1, // Ethereum public network
+      // optional config values
+      // host - defaults to "localhost"
+      // port - defaults to 8545
+      // gas
+      // gasPrice
+      // from - default address to use for any transaction Truffle makes during migrations
+    },
+    "morden": {
+      network_id: 2 // Official Ethereum test network
+    },
+    "staging": {
+      network_id: 1337 // custom private network
+      // use default rpc settings
+    },
+    "development": {
+      network_id: "default"
+    }
+  }
+};
