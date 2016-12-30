7c6dd11fe14854b1da89df846d6bde7d9515ef8e
retotrinkler <reto.trinkler@gmail.com>
2016-12-30 14:01:47 +0100

    KYM - know our manager

--- contracts/identity/KYM.sol
+++ contracts/identity/KYM.sol
@@ -2,5 +2,6 @@ pragma solidity ^0.4.4;
 
 import './KYMProtocol.sol';
 
-
+/// @title Know Your Manager Contract.
+/// @author Melonport AG <team@melonport.com>
 contract KYM is KYMProtocol {}
--- contracts/identity/KYMProtocol.sol
+++ contracts/identity/KYMProtocol.sol
@@ -1,3 +1,5 @@
 pragma solidity ^0.4.4;
 
+/// @title Know Your Manager Protocol
+/// @author Melonport AG <team@melonport.com>
 contract KYMProtocol {}
