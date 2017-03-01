3d92195d773a32a93dd9d252bd15f6f3a45e5ee2
retotrinkler <reto.trinkler@gmail.com>
2017-03-01 18:42:07 +0000

    invert kraken price in oracle

--- test/pricefeed.js
+++ test/pricefeed.js
@@ -10,10 +10,10 @@ contract('PriceFeed', (accounts) => {
   const OWNER = accounts[0];
   const NOT_OWNER = accounts[1];
   const assets = [
-    '0x0000000000000000000000000000000000000000',
-    '0x0000000000000000000000000000000000000001',
-    '0x0000000000000000000000000000000000000002',
-    '0x0000000000000000000000000000000000000003',
+    '0x632a40acd4975295495f45190e612ef15c84ae91',
+    '0xcb8d1b21f0ceb07959e47eb8152f25332939c0dc',
+    '0x9265c634b43bafc5305fed65c157ee1d7b6b8b50',
+    '0x6d7e5ec3d87cbe5d6efa611f86ea27da53c9a360',
   ];
 
   // Kraken example for: https://api.kraken.com/0/public/Ticker?pair=ETHXBT,REPETH,ETHEUR
@@ -29,7 +29,7 @@ contract('PriceFeed', (accounts) => {
   // Prices Relative to Ether
   const ett_eth = 1.0; // By definition
   const xbt_eth = data.result.XETHXXBT.c[0]; // Price already relavtive to ether
-  const rep_eth = data.result.XREPXETH.c[0]; // Price already relavtive to ether
+  const rep_eth = functions.invertAssetPairPrice(data.result.XREPXETH.c[0]);
   const eur_eth = data.result.XETHZEUR.c[0]; // Price already relavtive to ether
 
   // Atomize Prices realtive to Ether
