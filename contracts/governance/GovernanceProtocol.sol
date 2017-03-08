1d6274429e2665e76b0e8861ad1f5201881d4bce
retotrinkler <reto.trinkler@gmail.com>
2017-03-08 16:41:14 +0000

    renaming universe, compliance module; init GovernanceProtocol.sol

---
+++ contracts/governance/GovernanceProtocol.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.8;
+
+/// @title Version Protocol Contract
+/// @author Melonport AG <team@melonport.com>
+/// @notice This is to be considered as a protocol on how to access the underlying Governance Contract
+contract GovernanceProtocol {}
