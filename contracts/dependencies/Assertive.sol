9b8757a99b533259e2a0c868ef3edfdeefaac09e
retotrinkler <rt@melonport.com>
2017-05-22 16:31:35 +0200

    switching from assertive to hoare

--- contracts/dependencies/Assertive.sol
+++ contracts/dependencies/Assertive.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.4.11;
 /// @notice Asserts function
 contract Assertive {
 
-  function assert(bool assertion) internal {
+  /*function assert(bool assertion) internal {
       if (!assertion) throw;
-  }
+  }*/
 
 }
