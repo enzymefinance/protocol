308281371e024b59c54329802a716c4ef6e30e34
cgewecke <christophergewecke@gmail.com>
2017-06-26 17:38:20 -0700

    Initial coverage report

--- contracts/participation/RedeemProtocol.sol
+++ contracts/participation/RedeemProtocol.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.4.11;
 /// @title Redeem Protocol Contract
 /// @author Melonport AG <team@melonport.com>
 /// @notice This is to be considered as a protocol on how to access the underlying Redeem Contract
-contract RedeemProtocol {}
+contract RedeemProtocol {
+    bool x;
+}
--- contracts/participation/SubscribeProtocol.sol
+++ contracts/participation/SubscribeProtocol.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.4.11;
 /// @title Subscribe Protocol Contract
 /// @author Melonport AG <team@melonport.com>
 /// @notice This is to be considered as a protocol on how to access the underlying Subscribe Contract
-contract SubscribeProtocol {}
+contract SubscribeProtocol {
+    bool x;
+}
