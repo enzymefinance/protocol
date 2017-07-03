f11422ec101326a31735498379d7a85f8386d4ea
Travis Jacobs <trav221@gmail.com>
2017-07-03 12:36:57 -0230

    Add ERC20 contracts
    - add gnosis, golem and iconomi tokens

---
+++ contracts/assets/IconomiToken.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.11;
+
+import "./PreminedAsset.sol";
+
+/// @title Iconomi Token Contract.
+/// @author Melonport AG <team@melonport.com>
+/// @notice Premined amount used to make markets
+contract IconomiToken is PreminedAsset {
+
+    // FIELDS
+
+    // Constant token specific fields
+    string public constant name = "Iconomi Token";
+    string public constant symbol = "ICN";
+    uint public constant decimals = 18;
+    uint public constant preminedAmount = 10**18;
+
+    // NON-CONSTANT METHODS
+
+    function IconomiToken()
+        PreminedAsset(name, symbol, decimals, preminedAmount)
+    {}
+}
