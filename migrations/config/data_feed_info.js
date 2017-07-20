ec2dd680a0d8f08aec8a89b71f0bc51df557a423
retotrinkler <rt@melonport.com>
2017-07-20 17:35:50 +0200

    Update readme; config files for third-party contracts

---
+++ migrations/config/data_feed_info.js
@@ -0,0 +1,11 @@
+export const dataFeedInfo = {
+  kovan: [
+    {
+      address: '',
+      name: 'CryptoCompare',
+      data: '',
+      url: 'https://www.cryptocompare.com/',
+      githubHint: '',
+    },
+  ],
+};
---
+++ migrations/config/exchange_info.js
@@ -0,0 +1,11 @@
+export const exchangeInfo = {
+  kovan: [
+    {
+      address: '',
+      name: 'OasisDex',
+      data: '',
+      url: 'https://oasisdex.com',
+      githubHint: '',
+    },
+  ],
+};
