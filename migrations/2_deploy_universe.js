e228ceb8ed5e79585c674a02794d5f896be7e654
Travis Jacobs <trav221@gmail.com>
2017-07-21 13:44:20 -0230

    Change order of assets in migrations
    - Eth and Mln before assets

--- migrations/2_deploy_universe.js
+++ migrations/2_deploy_universe.js
@@ -4,15 +4,18 @@ const tokenInfo = require('./config/token_info.js');
 const dataFeedInfo = require('./config/data_feed_info.js');
 const exchangeInfo = require('./config/exchange_info.js');
 
+const ethTokenAddress = tokenInfo.kovan.find(t => t.symbol === 'ETH-T').address;
+const mlnTokenAddress = tokenInfo.kovan.find(t => t.symbol === 'MLN-T').address;
+const otherAddresses = tokenInfo.kovan.filter(t => t.symbol !== 'ETH-T' && t.symbol !== 'MLN-T').map(t => t.address);
 
 module.exports = async function (deployer, network) {
   // if (network === "development") return;
   try {
     await deployer.deploy(Universe,
-        tokenInfo.kovan.map(info => info.address),
-        Array(tokenInfo.kovan.length).fill(dataFeedInfo.kovan.find(info => info.name === 'CryptoCompare').address),
-        Array(tokenInfo.kovan.length).fill(exchangeInfo.kovan.find(info => info.name === 'OasisDex').address),
-      )
+      [ethTokenAddress].concat([mlnTokenAddress]).concat(otherAddresses),
+      Array(tokenInfo.kovan.length).fill(dataFeedInfo.kovan.find(info => info.name === 'CryptoCompare').address),
+      Array(tokenInfo.kovan.length).fill(exchangeInfo.kovan.find(info => info.name === 'OasisDex').address),
+    )
   } catch (e) {
     throw e;
   }
