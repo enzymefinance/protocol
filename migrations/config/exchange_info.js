3502ade862d8cc8b367a8930be8a470efabdbbec
retotrinkler <rt@melonport.com>
2017-07-24 11:27:28 +0200

    New exchange address; Version bump

--- migrations/config/exchange_info.js
+++ migrations/config/exchange_info.js
@@ -1,7 +1,7 @@
 module.exports = {
   kovan: [
     {
-      address: '0x47c158e0412a81a2dc330b6818522656519daafc',
+      address: '0x81bfb6a2db736c5ec06ddf4654478cf78b3e0be7',
       name: 'OasisDex',
       data: '',
       url: 'https://oasisdex.com',
