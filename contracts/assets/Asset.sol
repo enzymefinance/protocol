f635d807f4c23a1fc33c8ee40ac02322c70eba30
retotrinkler <rt@melonport.com>
2017-08-04 12:24:37 +0200

    Minor formatting

--- contracts/assets/Asset.sol
+++ contracts/assets/Asset.sol
@@ -16,9 +16,7 @@ contract Asset is ERC20 {
     // CONSTANT METHODS
 
     function getName() constant returns (string) { return name; }
-
     function getSymbol() constant returns (string) { return symbol; }
-
     function getDecimals() constant returns (uint) { return decimals; }
 
     // NON-CONSTANT METHODS
@@ -28,5 +26,5 @@ contract Asset is ERC20 {
         symbol = _symbol; // Set the symbol for display purposes
         decimals = _decimals; // Defined in price feed protocol
     }
-    
+
 }
