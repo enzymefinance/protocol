80ad5f7421f12485b911bcde6b99c51558fd528d
retotrinkler <rt@melonport.com>
2017-08-21 15:53:52 +0200

    Add to RiskMgmt comment

--- contracts/riskmgmt/RiskMgmtInterface.sol
+++ contracts/riskmgmt/RiskMgmtInterface.sol
@@ -5,13 +5,16 @@ import '../dependencies/ERC20.sol';
 /// @title RiskMgmtInterface Contract
 /// @author Melonport AG <team@melonport.com>
 /// @notice This is to be considered as an interface on how to access the underlying RiskMgmt Contract
+/* Remark: Checks for:
+ *  1) Liquidity: All positions have to be fairly simple to liquidate.
+ *    E.g. Cap at percentage of 30 day average trading volume of this pair
+ *  2) Market Impact: If w/in above liquidity restrictions, trade size also
+ *    restricted to have market impact below certain threshold
+ *  3) Best execution: Ensure the best execution possible for Melon fund
+ *    investors' orders.
+ */
 contract RiskMgmtInterface {
-    /* Remark: Checks for:
-     *  1) Liquidity: All positions have to be fairly simple to liquidate.
-     *    E.g. Cap at percentage of 30 day average trading volume of this pair
-     *  2) Market Impact: If w/in above liquidity restrictions, trade size also
-     *    restricted to have market impact below certain threshold
-     */
+
     function isExchangeMakePermitted(
         ERC20   haveToken,
         ERC20   wantToken,
@@ -21,12 +24,6 @@ contract RiskMgmtInterface {
         returns (bool)
     {}
 
-    /* Remark: Checks for:
-     *  1) Liquidity: All positions have to be fairly simple to liquidate.
-     *    E.g. Cap at percentage of 30 day average trading volume of this pair
-     *  2) Market Impact: If w/in above liquidity restrictions, trade size also
-     *    restricted to have market impact below certain threshold
-     */
     function isExchangeTakePermitted(
         ERC20   haveToken,
         ERC20   wantToken,
