bb9b030010ad2a386415ba72e96f1c2283ccc59b
retotrinkler <rt@melonport.com>
2017-09-04 14:46:47 +0200

    Adjust Gaslimits in scripts; Risk mgmt make orders

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -614,8 +614,8 @@ contract Vault is DBC, Owned, Shares, VaultInterface {
         pre_cond(isValidAssetPair(haveToken, wantToken))
         pre_cond(module.riskmgmt.isExchangeMakePermitted(
             0, // TODO Insert assetpair actual price (formatted the same way as reference price)
-            0, // TODO: Insert assetpair specific price
-            0 // TODO Insert buy quantity
+            module.pricefeed.getReferencePrice(haveToken, wantToken),
+            wantAmount
         ))
         returns (uint id)
     {
@@ -669,8 +669,8 @@ contract Vault is DBC, Owned, Shares, VaultInterface {
         var orderOwner = module.exchange.getOwner(id);
         require(module.riskmgmt.isExchangeTakePermitted(
             0, // TODO Insert assetpair actual price (formatted the same way as reference price)
-            0, // TODO: Insert assetpair specific price
-            0 // TODO Insert buy quantity
+            module.pricefeed.getReferencePrice(offeredBuyToken, offeredSellToken),
+            offeredSellAmount
         ));
         uint256 wantedSellAmount = wantedBuyAmount.mul(offeredSellAmount).div(offeredBuyAmount);
         approveSpending(offeredSellToken, wantedSellAmount);
