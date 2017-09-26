38c572b132a6f16336b162c2cd9a23cf5089dcde
retotrinkler <rt@melonport.com>
2017-09-26 17:53:25 +0200

    Fix some linter errors

--- test/version.js
+++ test/version.js
@@ -1,6 +1,4 @@
-const EtherToken = artifacts.require('EtherToken');
 const SimpleMarket = artifacts.require('SimpleMarket');
-const Governance = artifacts.require('Governance');
 const Participation = artifacts.require('Participation');
 const PreminedAsset = artifacts.require('PreminedAsset');
 const DataFeed = artifacts.require('DataFeed');
@@ -15,12 +13,13 @@ const assert = chai.assert;
 contract('Version', (accounts) => {
   let version;
   let feed;
+  let simpleMarket;
+  let participation;
+  let riskManagement;
+  let sphere;
 
   before('Deploy contract instances', async () => {
-    ethToken = await EtherToken.new({ from: accounts[0] });
-    eurToken = await PreminedAsset.new(
-      'Euro', 'EUR', 8, 10 ** 18, { from: accounts[0] });
-    mlnToken = await PreminedAsset.new(
+    const mlnToken = await PreminedAsset.new(
       'Melon', 'MLN', 18, 10 ** 18, { from: accounts[0] });
     version = await Version.new(mlnToken.address);
     feed = await DataFeed.new(mlnToken.address, 0, 120);
@@ -53,11 +52,11 @@ contract('Version', (accounts) => {
     const fundAddr = await version.getFund(fundId);
     const fund = await Fund.at(fundAddr);
     assert.equal(await fund.getDecimals(), 18);
-    assert.equal(await fund.getBaseUnits(), Math.pow(10, 18));
+    assert.equal(await fund.getBaseUnits(), 10 ** 18);
   });
 
   it('Can remove a fund', async () => {
-    let fundId = await version.getLastFundId();
+    const fundId = await version.getLastFundId();
     await version.shutDownFund(fundId);
   });
 });
