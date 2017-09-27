e13cd7542dfeb3aa39d7fab5a2073b57965e7956
retotrinkler <rt@melonport.com>
2017-09-27 15:09:49 +0200

    Introducing concept of deployment environment

---
+++ deployment/environment.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  kovan: {
+    networkId: '42',
+    host: 'localhost',
+    port: 8545,
+    gas: 6900000,
+    gasPrice: 100000000000,
+  },
+  development: {
+    networkId: '*',
+    host: 'localhost',
+    port: 8545,
+    gas: 6900000,
+    gasPrice: 100000000000,
+  },
+}
