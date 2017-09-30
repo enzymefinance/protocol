fc8d0dd0800d3b4bd5ccec36030b40359f125f0e
retotrinkler <rt@melonport.com>
2017-10-01 00:38:21 +0200

    Remove manualSettlement; PoE from fund

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -39,15 +39,6 @@ contract Fund is DBC, Owned, Shares, FundInterface {
         uint timestamp; // When above has been calculated
     }
 
-    struct InternalAccounting {
-        uint numberOfMakeOrders; // Number of potentially unsettled orders
-        mapping (bytes32 => bool) existsMakeOrder; // sha3(sellAsset, buyAsset) to boolean
-        mapping (bytes32 => uint) makeOrderId; // sha3(sellAsset, buyAsset) to order id
-        mapping (address => uint) quantitySentToExchange; // Quantity of asset held in custody of exchange
-        mapping (address => uint) quantityExpectedToReturn; // Quantity expected to receive of asset of what has been sent to exchange
-        mapping (address => uint) holdingsAtLastManualSettlement; // Quantity of asset held in custody of fund at time of manuel settlement
-    }
-
     enum RequestStatus { active, cancelled, executed }
     enum RequestType { subscribe, redeem }
     struct Request { // Describes and logs whenever asset enter and leave fund due to Participants
@@ -97,7 +88,6 @@ contract Fund is DBC, Owned, Shares, FundInterface {
     // Methods fields
     Modules public module; // Struct which holds all the initialised module instances
     Calculations public atLastConversion; // Calculation results at last convertUnclaimedRewards() call
-    InternalAccounting internalAccounting; // Accounts for assets not held in custody of fund
     bool public isShutDown; // Security features, if yes than investing, managing, convertUnclaimedRewards gets blocked
     Request[] public requests; // All the requests this fund received from participants
     bool public isSubscribeAllowed; // User option, if false fund rejects Melon investments
@@ -137,8 +127,6 @@ contract Fund is DBC, Owned, Shares, FundInterface {
     function getStake() constant returns (uint) { return balanceOf(this); }
     function getLastOrderId() constant returns (uint) { return orders.length - 1; }
     function getLastRequestId() constant returns (uint) { return requests.length - 1; }
-    function quantitySentToExchange(address ofAsset) constant returns (uint) { return internalAccounting.quantitySentToExchange[ofAsset]; }
-    function quantityExpectedToReturn(address ofAsset) constant returns (uint) { return internalAccounting.quantityExpectedToReturn[ofAsset]; }
 
     // CONSTANT METHODS - ACCOUNTING
 
@@ -148,7 +136,7 @@ contract Fund is DBC, Owned, Shares, FundInterface {
         for (uint i = 0; i < module.datafeed.numRegisteredAssets(); ++i) {
             address ofAsset = address(module.datafeed.getRegisteredAssetAt(i));
             uint assetHoldings = uint(ERC20(ofAsset).balanceOf(this)) // Amount of asset base units this vault holds
-                .add(quantitySentToExchange(ofAsset));
+                .add(ERC20(ofAsset).balanceOf(EXCHANGE));
             uint assetPrice = module.datafeed.getPrice(ofAsset);
             uint assetDecimals = module.datafeed.getDecimals(ofAsset);
             gav = gav.add(assetHoldings.mul(assetPrice).div(10 ** uint256(assetDecimals))); // Sum up product of asset holdings of this vault and asset prices
@@ -520,13 +508,6 @@ contract Fund is DBC, Owned, Shares, FundInterface {
         pre_cond(notShutDown())
         returns (bool, string)
     {
-        bytes32 assetPair = sha3(sellAsset, buyAsset);
-
-        // Settle existing make orders before creating a new one
-        /*if (internalAccounting.existsMakeOrder[assetPair]) {
-            manualSettlement(sellAsset, buyAsset);
-        }*/ // TODO: returns out of gas error
-
         returnError(
             module.datafeed.existsData(sellAsset, buyAsset),
             "ERR: DataFeed module: Requested asset pair not valid"
@@ -567,14 +548,6 @@ contract Fund is DBC, Owned, Shares, FundInterface {
             timestamp: now,
             fillQuantity: 0
         }));
-
-        internalAccounting.existsMakeOrder[assetPair] = true;
-        internalAccounting.quantitySentToExchange[sellAsset] =
-            quantitySentToExchange(sellAsset)
-            .add(sellQuantity);
-        internalAccounting.quantityExpectedToReturn[buyAsset] =
-            quantityExpectedToReturn(buyAsset)
-            .add(buyQuantity);
 
         OrderUpdated(id);
     }
@@ -661,82 +634,7 @@ contract Fund is DBC, Owned, Shares, FundInterface {
             "ERR: Exchange Adapter: Failed to cancel order"
         );
 
-        /*if (internalAccounting.existsMakeOrder[assetPair]) {
-            manualSettlement(sellAsset, buyAsset);
-        }*/ // TODO: returns out of gas error
-        
         OrderUpdated(id);
-    }
-
-    /// @notice Reduce exposure without exchange interaction
-    /// @dev After funds have been sent back to Melon fund, 'settle' them internally
-    /// @param sellAsset Asset (as registred in Asset registrar) to be sold
-    /// @param buyAsset Asset (as registred in Asset registrar) to be bought
-    function manualSettlement(address sellAsset, address buyAsset)
-        returns (bool, string)
-    {
-        bytes32 assetPair = sha3(sellAsset, buyAsset);
-
-        returnError(
-            isFalse(internalAccounting.existsMakeOrder[assetPair]),
-            "ERR: Make order for input asset pair not found"
-        );
-
-        uint id = internalAccounting.makeOrderId[assetPair];
-        Order memory order = orders[id];
-        var (error, ) = proofOfEmbezzlement(order.sellAsset, order.buyAsset);
-
-        returnError(
-            isFalse(error),
-            "ERR: Embezzlement has been determined"
-        );
-
-        // TODO: update order.status = OrderStatus.fullyFilled;
-        // TODO: abstract below into function
-        internalAccounting.existsMakeOrder[assetPair] = false;
-        internalAccounting.quantitySentToExchange[order.sellAsset] = 0;
-        internalAccounting.quantityExpectedToReturn[order.buyAsset] = 0;
-        // Update prev holdings
-        internalAccounting.holdingsAtLastManualSettlement[order.sellAsset] = ERC20(order.sellAsset).balanceOf(this);
-        internalAccounting.holdingsAtLastManualSettlement[order.buyAsset] = ERC20(order.buyAsset).balanceOf(this);
-    }
-
-    /// @notice Whether embezzlement happened
-    /// @dev Asset pair corresponds to unsettled (== make) order
-    /// @param sellAsset Asset (as registred in Asset registrar) to be sold
-    /// @param buyAsset Asset (as registred in Asset registrar) to be bought
-    /// @return True if embezzled otherwise false
-    function proofOfEmbezzlement(address sellAsset, address buyAsset)
-        returns (bool, string)
-    {
-        returnCriticalError(
-            (
-                ERC20(sellAsset).balanceOf(this) <=
-                internalAccounting.holdingsAtLastManualSettlement[sellAsset]
-                    .sub(quantitySentToExchange(sellAsset))
-            ), // TODO: Allocate staked shares from this to msg.sender
-            "CRITICAL ERR: Sold more than expected!"
-        );
-
-        // What is held in custody is less or equal than accounted for sell quantity (good)
-        uint factor = MELON_IN_BASE_UNITS; // Want to receive proportionally as much as sold
-        uint divisor = factor; // To reduce inaccuracy due to rounding errors
-        factor = divisor
-            .mul(internalAccounting.holdingsAtLastManualSettlement[sellAsset].sub(ERC20(sellAsset).balanceOf(this)))
-            .div(quantitySentToExchange(sellAsset));
-        // Revise return expectations, for example in case of partial fill of order
-        uint revisedReturnExpectations = quantityExpectedToReturn(buyAsset).mul(factor).div(divisor);
-
-        returnCriticalError(
-            (
-                ERC20(buyAsset).balanceOf(this) >=
-                internalAccounting.holdingsAtLastManualSettlement[buyAsset]
-                    .add(revisedReturnExpectations)
-            ), // TODO: Allocate staked shares from this to msg.sender
-            "CRITICAL ERR: Received (proportionally) less than expected buy quantity!"
-        );
-
-        return (false, "");
     }
 
     // NON-CONSTANT METHODS - REWARDS
