2495621756c9813111a58d45b8d6a8cf47539c60
retotrinkler <rt@melonport.com>
2017-10-01 19:13:35 +0200

    0.3.8-alpha.1

--- contracts/version/Version.sol
+++ contracts/version/Version.sol
@@ -52,9 +52,7 @@ contract Version is DBC, Owned {
         address ofParticipation,
         address ofRiskMgmt,
         address ofSphere
-    )
-        pre_cond(managerToFunds[msg.sender] == 0) // require only one fund per manager
-    {
+    ) {
         address fund = new Fund(
             msg.sender,
             withName,
