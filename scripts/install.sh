2ddfafbfd604adfae1648f5d4c817045270675fb
Travis Jacobs <trav221@gmail.com>
2017-10-10 13:41:01 +0200

    Fix installation script - remove some remnants of truffle configuration

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 wget https://github.com/dapphub/ethrun/releases/download/v0.2.4/ethrun-v0.2.4-linux.tar.gz
 tar -xvf ethrun-v0.2.4-linux.tar.gz
-sudo cp ethrun-v0.2.4-linux.tar.gz /usr/local/bin/ethrun
+sudo cp ethrun /usr/local/bin/
 nix-channel --add https://nix.dapphub.com/pkgs/dapphub
 nix-channel --update
 nix-env -i seth solc
