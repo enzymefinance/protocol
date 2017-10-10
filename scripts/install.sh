1b6633d4003fa5c9700923abdb443ba5db53ae81
Travis Jacobs <trav221@gmail.com>
2017-10-10 11:07:15 +0200

    First attempt at nix environment

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+wget https://github.com/dapphub/ethrun/releases/download/v0.2.4/ethrun-v0.2.4-linux.tar.gz
+tar -xvf ethrun-v0.2.4-linux.tar.gz
+sudo cp ethrun-v0.2.4-linux.tar.gz /usr/local/bin/ethrun
+nix-channel --add https://nix.dapphub.com/pkgs/dapphub
+nix-channel --update
+nix-env -i seth solc
+git clone https://github.com/dapphub/dapp
+make link -C dapp
+
