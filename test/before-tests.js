48fb449dc52e6630a11929b1ae71738fc392afea
Travis Jacobs <traviswjacobs@gmail.com>
2017-11-22 09:20:16 -0330

    Introduce delay after account generation - seems to allow the script to succeed on one attempt

--- test/before-tests.js
+++ test/before-tests.js
@@ -8,10 +8,17 @@ const provider = new Api.Provider.Http(`http://${config.host}:${config.port}`);
 const api = new Api(provider);
 const genesisAccount = "0x00248D782B4c27b5C6F42FEB3f36918C24b211A5";
 
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function main() {
   const numAccounts = testAccounts.length;
+  console.log(numAccounts)
   for (let i = 0; i < numAccounts; i += 1) {
+    console.log('account ' + i)
     const newAccount = await api.parity.newAccountFromPhrase(testAccounts[i], "password");
+    await timeout(2000);
     await api.personal.sendTransaction({
       from: genesisAccount,
       to: newAccount,
