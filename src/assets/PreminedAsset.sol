3f0c5dee2d45ef114fc9b44cc70b80529041b880
Travis Jacobs <traviswjacobs@gmail.com>
2017-12-01 18:10:41 -0330

    Replace safeMath with ds-math - fix #238 - still exists in thirdparty EtherDelta contract

--- src/assets/PreminedAsset.sol
+++ src/assets/PreminedAsset.sol
@@ -1,20 +1,18 @@
 pragma solidity ^0.4.17;
 
 import './Asset.sol';
-import '../libraries/safeMath.sol';
+import 'ds-math/math.sol';
 
 /// @title PreminedAsset Contract.
 /// @author Melonport AG <team@melonport.com>
 /// @notice An Asset with premined amount assigned to the creator, used to make markets
-contract PreminedAsset is Asset {
-    using safeMath for uint;
-
+contract PreminedAsset is Asset, DSMath {
     // METHODS
 
     function PreminedAsset(string name, string symbol, uint decimals, uint amount)
         Asset(name, symbol, decimals)
     {
-        balances[msg.sender] = balances[msg.sender].add(amount);
-        totalSupply = totalSupply.add(amount);
+        balances[msg.sender] = add(balances[msg.sender], amount);
+        totalSupply = add(totalSupply, amount);
     }
 }
