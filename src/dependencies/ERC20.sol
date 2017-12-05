b3e31dcfcc2b2de7f7d283cbe27117eea4c1f12a
Travis Jacobs <traviswjacobs@gmail.com>
2017-12-05 10:15:39 -0330

    Fix #272

--- src/dependencies/ERC20.sol
+++ src/dependencies/ERC20.sol
@@ -1,27 +1,28 @@
 pragma solidity ^0.4.17;
 
 import './ERC20Interface.sol';
+import '../libraries/safeMath.sol';
 
 /// @title ERC20 Token
 /// @author Melonport AG <team@melonport.com>
 /// @notice Original taken from https://github.com/ethereum/EIPs/issues/20
 /// @notice Checked against integer overflow
 contract ERC20 is ERC20Interface {
-
+    using safeMath for uint;
     function transfer(address _to, uint256 _value) returns (bool success) {
-        if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
+        if (balances[msg.sender] >= _value && balances[_to].add(_value) > balances[_to]) {
+            balances[msg.sender] = balances[msg.sender].sub(_value);
+            balances[_to] = balances[_to].add(_value);
             Transfer(msg.sender, _to, _value);
             return true;
         } else { throw; }
     }
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to].add(_value) > balances[_to]) {
+            balances[_to] = balances[_to].add(_value);
+            balances[_from] = balances[_from].sub(_value);
+            allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
             Transfer(_from, _to, _value);
             return true;
         } else { throw; }
