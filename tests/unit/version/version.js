eec5ad929aa373c0850598fa9d914d46b5d74258
GauthamN <gauthamnekk@gmail.com>
2017-12-13 10:30:32 +0100

    Modified version unit test

--- tests/unit/version/version.js
+++ tests/unit/version/version.js
@@ -1,5 +1,6 @@
 import test from "ava";
 import Api from "@parity/api";
+import { version } from "../../../utils/lib/utils.js";
 
 const addressBook = require("../../../addressBook.json");
 const BigNumber = require("bignumber.js");
@@ -11,15 +12,21 @@ const config = environmentConfig[environment];
 const provider = new Api.Provider.Http(`http://${config.host}:${config.port}`);
 const api = new Api(provider);
 
+// hoisted variables
 let accounts;
 let deployer;
 let manager;
 let worker;
 let investor;
 let opts;
-let version;
 
 const addresses = addressBook[environment];
+
+// mock data
+const fundName = "Super Fund";
+// const keccakedName = await api.web3.sha3(['Super Fund']);
+const keccakedFundName =
+  "0xf00030b282fd20568935f96740d5f79e0c72840d3c09a34d1c4c29210e6dddbe";
 
 test.before(async t => {
   accounts = await api.eth.accounts();
@@ -27,17 +34,10 @@ test.before(async t => {
   manager = accounts[1];
   investor = accounts[2];
   worker = accounts[3];
-  opts = { from: deployer, gas: config.gas, gasPrice: config.gasPrice };
-
-  // retrieve deployed contracts
-  version = await api.newContract(
-    JSON.parse(fs.readFileSync("out/version/Version.abi")),
-    addresses.Version,
-  );
+  opts = { from: manager, gas: config.gas, gasPrice: config.gasPrice };
 });
 
-test('Can setup and new fund', async t => {
-  const preLastFundId = await version.instance.getLastFundId.call({}, []);
+test("Can setup and new fund", async t => {
   const hash =
     "0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad";
   let sig = await api.eth.sign(manager, hash);
@@ -45,32 +45,35 @@ test('Can setup and new fund', async t => {
   const r = `0x${sig.substr(0, 64)}`;
   const s = `0x${sig.substr(64, 64)}`;
   const v = parseFloat(sig.substr(128, 2)) + 27;
-  await version.instance.setupFund.postTransaction(
-    { from: manager, gas: config.gas, gasPrice: config.gasPrice },
-    [
-      'Super Fund', // name
-      addresses.MlnToken, // reference asset
-      0,
-      0,
-      addresses.NoCompliance,
-      addresses.RMMakeOrders,
-      addresses.PriceFeed,
-      addresses.SimpleMarket,
-      v,
-      r,
-      s
-    ]
-  );
+  await version.instance.setupFund.postTransaction(opts, [
+    "Super Fund", // name
+    addresses.MlnToken, // reference asset
+    0,
+    0,
+    addresses.NoCompliance,
+    addresses.RMMakeOrders,
+    addresses.PriceFeed,
+    addresses.SimpleMarket,
+    v,
+    r,
+    s,
+  ]);
   const fundOwned = await version.instance.managerToFunds.call({}, [manager]);
-  // const keccakedName = await api.web3.sha3(['Super Fund']);
-  const keccakedName = "0xf00030b282fd20568935f96740d5f79e0c72840d3c09a34d1c4c29210e6dddbe";
-  const ownerOfFundName = await version.instance.fundNamesToOwners.call({}, [keccakedName]);
-  const listOfFunds = await version.instance.listOfFunds.call({}, [0]);
-  console.log(preLastFundId);
+  const ownerOfFundName = await version.instance.fundNamesToOwners.call({}, [
+    keccakedFundName,
+  ]);
   t.is(fundOwned.length, 42);
-  t.is(ownerOfFundName, manager)
+  t.is(ownerOfFundName, manager);
 });
 
-test('Can shutdown a fund', async t => {
-  //t.truthy(versionShutDown);
+test.serial("Can shutdown a fund", async t => {
+  const lastFundId = await version.instance.getLastFundId.call({}, []);
+  const lastFund = await version.instance.listOfFunds.call({}, [lastFundId]);
+  await version.instance.shutDownFund.postTransaction(opts, [lastFund]);
+  const ownerOfFundName = await version.instance.fundNamesToOwners.call({}, [
+    keccakedFundName,
+  ]);
+  const fundOwned = await version.instance.managerToFunds.call({}, [manager]);
+  t.is(fundOwned, "0x0000000000000000000000000000000000000000");
+  t.is(ownerOfFundName, "0x0000000000000000000000000000000000000000");
 });
