97284c0c99c692c8ef5cbeffb1c2320d2ae5d9c4
retotrinkler <rt@melonport.com>
2018-01-09 13:18:06 +0100

    Improve comments of native asset interface contract

--- src/assets/NativeAssetInterface.sol
+++ src/assets/NativeAssetInterface.sol
@@ -4,9 +4,11 @@ import "../assets/AssetInterface.sol";
 
 /// @title Native Asset Interface Contract
 /// @author Melonport AG <team@melonport.com>
-/// @notice Native asset is defined as the asset used to pay for gas on a blockchain
+/// @notice Native asset is defined as the native currency converted into erc20 compatible asset
+/// @notice Native currency is defined as the unit of exchange used to pay of gas on a blockchain
 contract NativeAssetInterface is AssetInterface {
 
+    // PUBLIC METHODS
     function deposit() public payable {}
     function withdraw(uint wad) public {}
 }
