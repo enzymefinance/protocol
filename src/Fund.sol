bd63bf2f6e3359b6dee26edb0592ecf4caad301f
Travis Jacobs <traviswjacobs@gmail.com>
2018-01-09 18:22:11 -0330

    Allow manager to shut down execution of subscription and redemption

--- src/Fund.sol
+++ src/Fund.sol
@@ -225,6 +225,7 @@ contract Fund is DSMath, DBC, Owned, Shares, FundInterface {
         external
         pre_cond(!isShutDown)
         pre_cond(requests[id].status == RequestStatus.active)
+        pre_cond((requests[id].requestType == RequestType.subscribe && isSubscribeAllowed) || (requests[id].requestType == RequestType.redeem && isRedeemAllowed))
         pre_cond(requests[id].requestType != RequestType.redeem || requests[id].shareQuantity <= balances[requests[id].participant]) // request owner does not own enough shares
         pre_cond(totalSupply == 0 || now >= add(requests[id].timestamp, mul(uint(2), module.pricefeed.getInterval()))) // PriceFeed Module: Wait at least one interval before continuing unless its the first supscription
         pre_cond(module.pricefeed.hasRecentPrice(address(REFERENCE_ASSET))) // PriceFeed Module: No recent updates for fund asset list
