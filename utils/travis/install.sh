5c3fd38ca4f64ede113b6ddf6fa957620b86c5c4
Travis Jacobs <traviswjacobs@gmail.com>
2018-02-28 10:29:15 -0330

    Install solc from apt package

--- utils/travis/install.sh
+++ utils/travis/install.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash
 set -ex
 
-# install parity dependencies
-sudo apt-get install openssl libssl-dev libudev-dev
+PARITY_VERSION=1.8.10
 
-# Install Rust
-curl https://sh.rustup.rs > rs.sh && bash rs.sh -y
+# install dependencies and compiler
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethereum software-properties-common openssl libssl-dev libudev-dev solc
+
+# install parity
+PARITY_DOWNLOAD=https://parity-downloads-mirror.parity.io/v${PARITY_VERSION}/x86_64-unknown-linux-gnu/parity
+
+# Fetch parity
+curl -L $PARITY_DOWNLOAD > parity
 
 # Install parity
-git clone https://github.com/paritytech/parity
-cd parity
-~/.cargo/bin/cargo build --release
+chmod +x parity
+sudo mv parity /usr/bin
 
 # install dapp
 wget https://github.com/dapphub/ethrun/releases/download/v0.2.4/ethrun-v0.2.4-linux.tar.gz
 tar -xvf ethrun-v0.2.4-linux.tar.gz
 sudo cp ethrun /usr/local/bin/
+
 nix-channel --add https://nix.dapphub.com/pkgs/dapphub
 nix-channel --update
-nix-env -iA dapphub.{dapp,hevm,seth,solc}
+nix-env -iA dapphub.{dapp,hevm,seth}
 
 # install oyente
-sudo apt-get install software-properties-common
-sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo apt-get update
-sudo apt-get install ethereum
 sudo pip2 install z3
 sudo pip2 install z3-solver
 sudo pip2 install oyente
