a8252e65598e54254c3e60504b3656adfd1abb50
Travis Jacobs <traviswjacobs@gmail.com>
2018-03-06 11:09:00 -0330

    Remove outer function calls for version management - clean up slightly

--- src/system/Governance.sol
+++ src/system/Governance.sol
@@ -51,46 +51,6 @@ contract Governance is DBC, Owned, DSGroup {
 
     function() payable { }
 
-    // PUBLIC METHODS
-
-    /// @notice Propose new versions of Melon
-    /// @param ofVersion Address of version contract to be proposed
-    function proposeVersion(address ofVersion) {
-        versionToProposalIds[ofVersion] = propose(address(this), new bytes(0), 0);
-    }
-
-    /// @notice Approve new versions of Melon
-    /// @param ofVersion Address of version contract to be approved
-    function approveVersion(address ofVersion) {
-        confirm(versionToProposalIds[ofVersion]);
-    }
-
-    /// @notice Trigger new versions of Melon
-    /// @param ofVersion Address of version contract to be triggered
-    function triggerVersion(address ofVersion) {
-        trigger(versionToProposalIds[ofVersion]);
-        addVersion(ofVersion);
-    }
-
-    /// @notice Propose shutdown of Melon version
-    /// @param ofVersionId Version id to be proposed for shutdown
-    function proposeShutdown(uint ofVersionId) {
-        versionIdToShutdownIds[ofVersionId] = propose(address(this), new bytes(0), 0);
-    }
-
-    /// @notice Approve shutdown of Melon version
-    /// @param ofVersionId Version id to be approved for shutdown
-    function approveShutdown(uint ofVersionId) {
-        confirm(versionIdToShutdownIds[ofVersionId]);
-    }
-
-    /// @notice Trigger shutdown of Melon version
-    /// @param ofVersionId Version id to be triggered for shutdown
-    function triggerShutdown(uint ofVersionId) {
-        trigger(versionIdToShutdownIds[ofVersionId]);
-        shutDownVersion(ofVersionId);
-    }
-
     // PUBLIC VIEW METHODS
 
     /**
@@ -110,17 +70,16 @@ contract Governance is DBC, Owned, DSGroup {
 
     // INTERNAL METHODS
 
+    /// @dev In later version, require authorities consensus
     /// @notice Add an approved version of Melon
     /// @param ofVersion Address of the version to add
     /// @return id integer ID of the version (list index)
     function addVersion(
         address ofVersion
     )
-        // In later version
-        //  require Authorities consensus
+        pre_cond(msg.sender == this)
         returns (uint id)
     {
-        require(msg.sender == address(this));
         Version memory info;
         info.version = ofVersion;
         info.active = true;
@@ -132,21 +91,20 @@ contract Governance is DBC, Owned, DSGroup {
     /// @notice Remove and shut down version of Melon
     /// @param id Id of the version to shutdown
     function shutDownVersion(uint id)
+        pre_cond(msg.sender == this)
         pre_cond(isActive(id))
     {
-        require(msg.sender == address(this));
         VersionInterface Version = VersionInterface(versions[id].version);
         Version.shutDown();
         delete versions[id];
         VersionUpdated(id);
     }
 
-    function isActive(uint id) internal returns (bool active) {
-        (, active, ) = getVersionById(id);
-    }
-
     function getVersionsLength() public view returns (uint) {
         return versions.length;
     }
 
+    function isActive(uint id) public view returns (bool active) {
+        (, active, ) = getVersionById(id);
+    }
 }
