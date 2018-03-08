d483be8f3db9d7c6811a972ee600406fcdcee52c
Travis Jacobs <traviswjacobs@gmail.com>
2018-03-08 10:57:10 -0330

    Try installing parity with snap

--- utils/travis/install.sh
+++ utils/travis/install.sh
@@ -6,17 +6,18 @@ PARITY_VERSION=1.8.10
 # install dependencies and compiler
 sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo apt-get update
-sudo apt-get install ethereum software-properties-common openssl libssl-dev libudev-dev solc
+sudo apt-get install ethereum software-properties-common openssl libssl-dev libudev-dev solc snapd
 
 # install parity
-PARITY_DOWNLOAD=https://parity-downloads-mirror.parity.io/v${PARITY_VERSION}/x86_64-unknown-linux-gnu/parity
+sudo snap install parity
+# PARITY_DOWNLOAD=https://parity-downloads-mirror.parity.io/v${PARITY_VERSION}/x86_64-unknown-linux-gnu/parity
 
 # Fetch parity
-curl -L $PARITY_DOWNLOAD > parity
+# curl -L $PARITY_DOWNLOAD > parity
 
 # Install parity
-chmod +x parity
-sudo mv parity /usr/bin
+# chmod +x parity
+# sudo mv parity /usr/bin
 
 # install dapp
 wget https://github.com/dapphub/ethrun/releases/download/v0.2.4/ethrun-v0.2.4-linux.tar.gz
