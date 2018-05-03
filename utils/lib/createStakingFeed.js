704663e02dc00250b1b1e7f405e7774c78a10b69
GauthamN <gauthamnekk@gmail.com>
2018-05-03 21:50:22 +0200

    Create staking feed through Canonical Pricefeed in deploy script

---
+++ utils/lib/createStakingFeed.js
@@ -0,0 +1,14 @@
+import api from "./api";
+import { retrieveContract } from "../lib/contracts";
+
+async function createStakingFeed(opts, canonicalPriceFeed) {
+  const txid = await canonicalPriceFeed.instance.setupStakingPriceFeed.postTransaction(opts);
+  const receipt = await api.eth.getTransactionReceipt(txid)
+  const setupLog = receipt.logs.find(
+    e => e.topics[0] === api.util.sha3('SetupPriceFeed(address)')
+  );
+  const stakingFeedAddress = api.util.toChecksumAddress(`0x${setupLog.data.slice(-40)}`);
+  return retrieveContract("pricefeeds/StakingPriceFeed", stakingFeedAddress);
+}
+
+export default createStakingFeed;
