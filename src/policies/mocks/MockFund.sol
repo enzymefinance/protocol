adf4b57363e8218f9b5ffd128a508cdeae943956
ferranbt <ferran@melonport.com>
2018-06-12 15:12:28 +0200

    Add mock fund and pricefeed

--- src/policies/mocks/MockFund.sol
+++ src/policies/mocks/MockFund.sol
@@ -1,7 +1,22 @@
 pragma solidity ^0.4.21;
 
+import "../../pricefeeds/CanonicalPriceFeed.sol";
 import "../Manager.sol";
 
 contract MockFund is PolicyManager {
-    
+    struct Modules {
+        CanonicalPriceFeed pricefeed;
+    }
+
+    Modules public modules;
+
+    function setPriceFeed(address _pricefeed) public {
+        modules.pricefeed = CanonicalPriceFeed(_pricefeed);
+    }
+
+    function testPolicy(address[4] addresses, uint[2] values) public view 
+        validPolicy(addresses, values) 
+    {
+        // dummy
+    }
 }
--- src/policies/mocks/MockPriceFeed.sol
+++ src/policies/mocks/MockPriceFeed.sol
@@ -1,5 +1,39 @@
 pragma solidity ^0.4.21;
 
-contract MockPriceFeed {
-    
+import "../../pricefeeds/SimplePriceFeedInterface.sol";
+
+contract MockPriceFeed is SimplePriceFeedInterface {
+    mapping(address => uint) addresses;
+
+    function update(address[] ofAssets, uint[] newPrices) external {
+        for(uint i = 0; i < ofAssets.length; ++i) {
+            addresses[ofAssets[i]] = newPrices[i];
+        }
+    }
+
+    function getQuoteAsset() view returns (address) {
+        return address(0x0);
+    }
+
+    function getLastUpdateId() view returns (uint) {
+        return 1;
+    }
+
+    function getPrice(address ofAsset) view returns (uint price, uint timestamp) {
+        return (addresses[ofAsset], 1);
+    }
+
+    function getPrices(address[] ofAssets) view returns (uint[] prices, uint[] timestamps) {
+        prices = new uint[](ofAssets.length);
+        timestamps = new uint[](ofAssets.length);
+
+        for(uint i = 0; i < ofAssets.length; ++i) {
+            var (price, timestamp) = getPrice(ofAssets[i]);
+
+            prices[i] = price;
+            timestamps[i] = timestamp;
+        }
+
+        return (prices, timestamps);
+    }
 }
