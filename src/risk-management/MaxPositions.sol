e2123f2d92507a08f8a018303506c062a163f623
john-0x <john0x@protonmail.ch>
2018-06-25 17:40:02 +0200

    adding MaxPositions policy

---
+++ src/risk-management/MaxPositions.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.21;
+
+import "../policies/Policy.sol";
+import "../Fund.sol";
+
+contract MaxPositions is Policy {
+    uint maxPositions;
+
+    function MaxPositions(uint _maxPositions) public {
+      // _maxPositions: 10 would indicate a maximum 10 different tokens
+      maxPositions = _maxPositions;
+    }
+
+    function rule(address[4] addresses, uint[2] values) external view returns (bool) {
+      return Fund(msg.sender).getFundHoldingsLength() < maxPositions;
+    }
+}
