e0afc66b252c9a8ca8932b232c083571cb58a974
Travis Jacobs <traviswjacobs@gmail.com>
2018-07-04 11:13:02 -0230

    Update travis and npm scripts

--- utils/travis/install.sh
+++ utils/travis/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-PARITY_VERSION=1.8.10
+# PARITY_VERSION=1.8.10
 
 # install dependencies and compiler
 sudo add-apt-repository -y ppa:ethereum/ethereum
@@ -9,7 +9,7 @@ sudo apt-get update
 sudo apt-get install ethereum software-properties-common openssl libssl-dev libudev-dev solc snapd
 
 # install parity
-sudo snap install parity
+# sudo snap install parity
 # PARITY_DOWNLOAD=https://parity-downloads-mirror.parity.io/v${PARITY_VERSION}/x86_64-unknown-linux-gnu/parity
 
 # Fetch parity
