671a821f34316bc3137d9a8b45d3d2da39d78e99
Travis Jacobs <traviswjacobs@gmail.com>
2018-07-06 10:06:24 -0230

    Add missing data lib

---
+++ utils/lib/data.js
@@ -0,0 +1,26 @@
+import web3 from "./web3";
+
+function abiEncode(name, argTypes=[]) {
+  return web3.eth.abi.encodeFunctionSignature(
+    `${name}(${argTypes.join(",")})`
+  );
+}
+
+const makeOrderSignature = abiEncode("makeOrder", [
+  "address", "address[5]", "uint256[8]", "bytes32", "uint8", "bytes32", "bytes32",
+]);
+
+const takeOrderSignature = abiEncode("takeOrder", [
+  "address", "address[5]", "uint256[8]", "bytes32", "uint8", "bytes32", "bytes32",
+]);
+
+const cancelOrderSignature = abiEncode("cancelOrder", [
+  "address", "address[5]", "uint256[8]", "bytes32", "uint8", "bytes32", "bytes32",
+]);
+
+export {
+  makeOrderSignature,
+  takeOrderSignature,
+  cancelOrderSignature,
+}
+
