dfbc7a5b3a688ee29632e4a87623c562ec06fc72
GauthamN <gauthamnekk@gmail.com>
2018-07-13 16:36:12 +0200

    Add kyber third party contracts

---
+++ src/exchange/thirdparty/kyber/KyberDependencies.sol
@@ -0,0 +1,378 @@
+pragma solidity ^0.4.21;
+
+import "../../../assets/Asset.sol";
+
+interface ERC20 {
+    function totalSupply() public view returns (uint supply);
+    function balanceOf(address _owner) public view returns (uint balance);
+    function transfer(address _to, uint _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success);
+    function approve(address _spender, uint _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public view returns (uint remaining);
+    function decimals() public view returns(uint digits);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
+
+/// @title Kyber Reserve contract
+interface KyberReserveInterface {
+
+    function trade(
+        ERC20 srcToken,
+        uint srcAmount,
+        ERC20 destToken,
+        address destAddress,
+        uint conversionRate,
+        bool validate
+    )
+        public
+        payable
+        returns(bool);
+
+    function getConversionRate(ERC20 src, ERC20 dest, uint srcQty, uint blockNumber) public view returns(uint);
+}
+
+/// @title Kyber Network interface
+interface KyberNetworkInterface {
+    function maxGasPrice() public view returns(uint);
+    function getUserCapInWei(address user) public view returns(uint);
+    function getUserCapInTokenWei(address user, ERC20 token) public view returns(uint);
+    function enabled() public view returns(bool);
+    function info(bytes32 id) public view returns(uint);
+
+    function getExpectedRate(ERC20 src, ERC20 dest, uint srcQty) public view
+        returns (uint expectedRate, uint slippageRate);
+
+    function tradeWithHint(address trader, ERC20 src, uint srcAmount, ERC20 dest, address destAddress,
+        uint maxDestAmount, uint minConversionRate, address walletId, bytes hint) public payable returns(uint);
+}
+
+contract PermissionGroups {
+
+    address public admin;
+    address public pendingAdmin;
+    mapping(address=>bool) internal operators;
+    mapping(address=>bool) internal alerters;
+    address[] internal operatorsGroup;
+    address[] internal alertersGroup;
+    uint constant internal MAX_GROUP_SIZE = 50;
+
+    function PermissionGroups() public {
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin);
+        _;
+    }
+
+    modifier onlyOperator() {
+        require(operators[msg.sender]);
+        _;
+    }
+
+    modifier onlyAlerter() {
+        require(alerters[msg.sender]);
+        _;
+    }
+
+    function getOperators () external view returns(address[]) {
+        return operatorsGroup;
+    }
+
+    function getAlerters () external view returns(address[]) {
+        return alertersGroup;
+    }
+
+    event TransferAdminPending(address pendingAdmin);
+
+    /**
+     * @dev Allows the current admin to set the pendingAdmin address.
+     * @param newAdmin The address to transfer ownership to.
+     */
+    function transferAdmin(address newAdmin) public onlyAdmin {
+        require(newAdmin != address(0));
+        TransferAdminPending(pendingAdmin);
+        pendingAdmin = newAdmin;
+    }
+
+    /**
+     * @dev Allows the current admin to set the admin in one tx. Useful initial deployment.
+     * @param newAdmin The address to transfer ownership to.
+     */
+    function transferAdminQuickly(address newAdmin) public onlyAdmin {
+        require(newAdmin != address(0));
+        TransferAdminPending(newAdmin);
+        AdminClaimed(newAdmin, admin);
+        admin = newAdmin;
+    }
+
+    event AdminClaimed( address newAdmin, address previousAdmin);
+
+    /**
+     * @dev Allows the pendingAdmin address to finalize the change admin process.
+     */
+    function claimAdmin() public {
+        require(pendingAdmin == msg.sender);
+        AdminClaimed(pendingAdmin, admin);
+        admin = pendingAdmin;
+        pendingAdmin = address(0);
+    }
+
+    event AlerterAdded (address newAlerter, bool isAdd);
+
+    function addAlerter(address newAlerter) public onlyAdmin {
+        require(!alerters[newAlerter]); // prevent duplicates.
+        require(alertersGroup.length < MAX_GROUP_SIZE);
+
+        AlerterAdded(newAlerter, true);
+        alerters[newAlerter] = true;
+        alertersGroup.push(newAlerter);
+    }
+
+    function removeAlerter (address alerter) public onlyAdmin {
+        require(alerters[alerter]);
+        alerters[alerter] = false;
+
+        for (uint i = 0; i < alertersGroup.length; ++i) {
+            if (alertersGroup[i] == alerter) {
+                alertersGroup[i] = alertersGroup[alertersGroup.length - 1];
+                alertersGroup.length--;
+                AlerterAdded(alerter, false);
+                break;
+            }
+        }
+    }
+
+    event OperatorAdded(address newOperator, bool isAdd);
+
+    function addOperator(address newOperator) public onlyAdmin {
+        require(!operators[newOperator]); // prevent duplicates.
+        require(operatorsGroup.length < MAX_GROUP_SIZE);
+
+        OperatorAdded(newOperator, true);
+        operators[newOperator] = true;
+        operatorsGroup.push(newOperator);
+    }
+
+    function removeOperator (address operator) public onlyAdmin {
+        require(operators[operator]);
+        operators[operator] = false;
+
+        for (uint i = 0; i < operatorsGroup.length; ++i) {
+            if (operatorsGroup[i] == operator) {
+                operatorsGroup[i] = operatorsGroup[operatorsGroup.length - 1];
+                operatorsGroup.length -= 1;
+                OperatorAdded(operator, false);
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * @title Contracts that should be able to recover tokens or ethers
+ * @author Ilan Doron
+ * @dev This allows to recover any tokens or Ethers received in a contract.
+ * This will prevent any accidental loss of tokens.
+ */
+contract Withdrawable is PermissionGroups {
+
+    event TokenWithdraw(ERC20 token, uint amount, address sendTo);
+
+    /**
+     * @dev Withdraw all ERC20 compatible tokens
+     * @param token ERC20 The address of the token contract
+     */
+    function withdrawToken(ERC20 token, uint amount, address sendTo) external onlyAdmin {
+        require(token.transfer(sendTo, amount));
+        TokenWithdraw(token, amount, sendTo);
+    }
+
+    event EtherWithdraw(uint amount, address sendTo);
+
+    /**
+     * @dev Withdraw Ethers
+     */
+    function withdrawEther(uint amount, address sendTo) external onlyAdmin {
+        sendTo.transfer(amount);
+        EtherWithdraw(amount, sendTo);
+    }
+}
+
+contract Utils {
+
+    ERC20 constant internal ETH_TOKEN_ADDRESS = ERC20(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
+    uint  constant internal PRECISION = (10**18);
+    uint  constant internal MAX_QTY   = (10**28); // 10B tokens
+    uint  constant internal MAX_RATE  = (PRECISION * 10**6); // up to 1M tokens per ETH
+    uint  constant internal MAX_DECIMALS = 18;
+    uint  constant internal ETH_DECIMALS = 18;
+    mapping(address=>uint) internal decimals;
+
+    function setDecimals(ERC20 token) internal {
+        if (token == ETH_TOKEN_ADDRESS) decimals[token] = ETH_DECIMALS;
+        else decimals[token] = token.decimals();
+    }
+
+    function getDecimals(ERC20 token) internal view returns(uint) {
+        if (token == ETH_TOKEN_ADDRESS) return ETH_DECIMALS; // save storage access
+        uint tokenDecimals = decimals[token];
+        // technically, there might be token with decimals 0
+        // moreover, very possible that old tokens have decimals 0
+        // these tokens will just have higher gas fees.
+        if(tokenDecimals == 0) return token.decimals();
+
+        return tokenDecimals;
+    }
+
+    function calcDstQty(uint srcQty, uint srcDecimals, uint dstDecimals, uint rate) internal pure returns(uint) {
+        require(srcQty <= MAX_QTY);
+        require(rate <= MAX_RATE);
+
+        if (dstDecimals >= srcDecimals) {
+            require((dstDecimals - srcDecimals) <= MAX_DECIMALS);
+            return (srcQty * rate * (10**(dstDecimals - srcDecimals))) / PRECISION;
+        } else {
+            require((srcDecimals - dstDecimals) <= MAX_DECIMALS);
+            return (srcQty * rate) / (PRECISION * (10**(srcDecimals - dstDecimals)));
+        }
+    }
+
+    function calcSrcQty(uint dstQty, uint srcDecimals, uint dstDecimals, uint rate) internal pure returns(uint) {
+        require(dstQty <= MAX_QTY);
+        require(rate <= MAX_RATE);
+
+        //source quantity is rounded up. to avoid dest quantity being too low.
+        uint numerator;
+        uint denominator;
+        if (srcDecimals >= dstDecimals) {
+            require((srcDecimals - dstDecimals) <= MAX_DECIMALS);
+            numerator = (PRECISION * dstQty * (10**(srcDecimals - dstDecimals)));
+            denominator = rate;
+        } else {
+            require((dstDecimals - srcDecimals) <= MAX_DECIMALS);
+            numerator = (PRECISION * dstQty);
+            denominator = (rate * (10**(dstDecimals - srcDecimals)));
+        }
+        return (numerator + denominator - 1) / denominator; //avoid rounding down errors
+    }
+}
+
+contract Utils2 is Utils {
+
+    /// @dev get the balance of a user.
+    /// @param token The token type
+    /// @return The balance
+    function getBalance(ERC20 token, address user) public view returns(uint) {
+        if (token == ETH_TOKEN_ADDRESS)
+            return user.balance;
+        else
+            return token.balanceOf(user);
+    }
+
+    function getDecimalsSafe(ERC20 token) internal returns(uint) {
+
+        if (decimals[token] == 0) {
+            setDecimals(token);
+        }
+
+        return decimals[token];
+    }
+
+    /// @dev notice, overrides previous implementation.
+    function setDecimals(ERC20 token) internal {
+        uint decimal;
+
+        if (token == ETH_TOKEN_ADDRESS) {
+            decimal = ETH_DECIMALS;
+        } else {
+            if (!address(token).call(bytes4(keccak256("decimals()")))) {/* solhint-disable-line avoid-low-level-calls */
+                //above code can only be performed with low level call. otherwise all operation will revert.
+                // call failed
+                decimal = 18;
+            } else {
+                decimal = token.decimals();
+            }
+        }
+
+        decimals[token] = decimal;
+    }
+
+    function calcDestAmount(ERC20 src, ERC20 dest, uint srcAmount, uint rate) internal view returns(uint) {
+        return calcDstQty(srcAmount, getDecimals(src), getDecimals(dest), rate);
+    }
+
+    function calcSrcAmount(ERC20 src, ERC20 dest, uint destAmount, uint rate) internal view returns(uint) {
+        return calcSrcQty(destAmount, getDecimals(src), getDecimals(dest), rate);
+    }
+
+    function calcRateFromQty(uint srcAmount, uint destAmount, uint srcDecimals, uint dstDecimals)
+        internal pure returns(uint)
+    {
+        require(srcAmount <= MAX_QTY);
+        require(destAmount <= MAX_QTY);
+
+        if (dstDecimals >= srcDecimals) {
+            require((dstDecimals - srcDecimals) <= MAX_DECIMALS);
+            return (destAmount * PRECISION / ((10 ** (dstDecimals - srcDecimals)) * srcAmount));
+        } else {
+            require((srcDecimals - dstDecimals) <= MAX_DECIMALS);
+            return (destAmount * PRECISION * (10 ** (srcDecimals - dstDecimals)) / srcAmount);
+        }
+    }
+}
+
+contract WhiteListInterface {
+    function getUserCapInWei(address user) external view returns (uint userCapWei);
+}
+
+interface ExpectedRateInterface {
+    function getExpectedRate(ERC20 src, ERC20 dest, uint srcQty) public view
+        returns (uint expectedRate, uint slippageRate);
+}
+
+interface FeeBurnerInterface {
+    function handleFees (uint tradeWeiAmount, address reserve, address wallet) public returns(bool);
+}
+
+interface ConversionRatesInterface {
+
+    function recordImbalance(
+        ERC20 token,
+        int buyAmount,
+        uint rateUpdateBlock,
+        uint currentBlock
+    )
+        public;
+
+    function getRate(ERC20 token, uint currentBlockNumber, bool buy, uint qty) public view returns(uint);
+}
+
+interface SanityRatesInterface {
+    function getSanityRate(ERC20 src, ERC20 dest) public view returns(uint);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+ * SimpleToken
+ *
+ * Very simple ERC20 Token example, where all tokens are pre-assigned
+ * to the creator. Note they can later distribute these tokens
+ * as they wish using `transfer` and other `StandardToken` functions.
+ */
+contract TestToken is Asset {
+
+    string public name = "Test";
+    string public symbol = "TST";
+    uint public decimals = 18;
+    uint public INITIAL_SUPPLY = 10 ** 27;
+
+    function TestToken(string _name, string _symbol, uint _decimals) public {
+        _totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
+}
