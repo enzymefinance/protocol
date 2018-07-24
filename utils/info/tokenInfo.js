7a6f846c47d4588379b7fe9ef232db00a08a0a8f
Travis Jacobs <traviswjacobs@gmail.com>
2018-07-24 11:39:04 +0200

    Update REP token address

--- utils/info/tokenInfo.js
+++ utils/info/tokenInfo.js
@@ -290,7 +290,7 @@ module.exports = {
       ipfsHash: '',
     },
     'REP': {
-      address: '0xe94327d07fc17907b4db788e5adf2ed424addff6',
+      address: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
       name: 'Reputation',
       decimals: 18,
       url: 'https://augur.net/',
