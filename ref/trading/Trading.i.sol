5ed086c36aaa054be05dda3c965266089b122ec7
Travis Jacobs <traviswjacobs@gmail.com>
2018-08-22 13:46:35 +0200

    Add and update more interfaces

---
+++ ref/trading/Trading.i.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.21;
+
+
+/// @notice Mediation between a Fund and exchanges
+interface Trading {
+    function callOnExchange(
+        uint exchangeIndex,
+        bytes4 method,
+        address[5] orderAddresses,
+        uint[8] orderValues,
+        bytes32 identifier,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    function addOpenMakeOrder(
+        address ofExchange,
+        address ofSellAsset,
+        uint orderId
+    );
+
+    function removeOpenMakeOrder(
+        address ofExchange,
+        address ofSellAsset
+    );
+
+    function orderUpdateHook(
+        address ofExchange,
+        bytes32 orderId,
+        UpdateType updateType,
+        address[2] orderAddresses,
+        uint[3] orderValues
+    );
+    function quantityHeldInCustodyOfExchange(address ofAsset) returns (uint);
+}
+
--- ref/vault/Vault.i.sol
+++ ref/vault/Vault.i.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.4.21;
 
+
 /// @notice Custody component
 interface VaultInterface {
     function deposit(address token, uint amount);
     function withdraw(address token, uint amount);
+    function addAssetToOwnedAssets (address ofAsset);
     function lockdown();
     function unlock();
 }
