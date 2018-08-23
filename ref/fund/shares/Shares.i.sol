0b5d29ed3ddf539be1c6a5e04e655ce955d1b004
Travis Jacobs <traviswjacobs@gmail.com>
2018-08-23 15:42:46 -0230

    Simplify initial Shares interface and implementation

--- ref/fund/shares/Shares.i.sol
+++ ref/fund/shares/Shares.i.sol
@@ -1,15 +1,8 @@
 pragma solidity ^0.4.21;
 
-/// @notice Token representing proportional ownership in the Fund
+/// @notice Token representing ownership of the Fund
 interface SharesInterface {
-    function createShares(address who, uint amount);
-    function annihilateShares(address who, uint amount);
-
-    function getName() view returns (bytes32);
-    function getSymbol() view returns (bytes8);
-    function getDecimals() view returns (uint);
-    function getCreationTime() view returns (uint);
-    function toSmallestShareUnit(uint quantity) view returns (uint);
-    function toWholeShareUnit(uint quantity) view returns (uint);
+    function createFor(address who, uint amount);
+    function destroyFor(address who, uint amount);
 }
 
--- ref/fund/shares/Shares.sol
+++ ref/fund/shares/Shares.sol
@@ -1,85 +1,20 @@
 pragma solidity ^0.4.21;
 
-import "../Asset.sol";
+import "../dependencies/ERC20.sol";
 import "./Shares.i.sol";
 
-/// @title Shares Contract for creating ERC20 compliant assets.
-/// @author Melonport AG <team@melonport.com>
-/// @notice Fund
-contract Shares is SharesInterface, Asset {
 
-    // FIELDS
+contract Shares is SharesInterface, ERC20 {
 
-    // Constructor fields
-    bytes32 public name;
-    bytes8 public symbol;
-    uint public decimal;
-    uint public creationTime;
-
-    // METHODS
-
-    // CONSTRUCTOR
-
-    /// @param _name Name these shares
-    /// @param _symbol Symbol of shares
-    /// @param _decimal Amount of decimals sharePrice is denominated in, defined to be equal as deciamls in REFERENCE_ASSET contract
-    /// @param _creationTime Timestamp of share creation
-    function Shares(bytes32 _name, bytes8 _symbol, uint _decimal, uint _creationTime) {
-        name = _name;
-        symbol = _symbol;
-        decimal = _decimal;
-        creationTime = _creationTime;
+    function createFor(address who, uint amount) onlyControllers {
+        totalSupply = add(totalSupply, amount);
+        balances[who] = add(balances[who], amount);
     }
 
-    // PUBLIC METHODS
-
-    /**
-     * @notice Send `_value` tokens to `_to` from `msg.sender`
-     * @dev Transfers sender's tokens to a given address
-     * @dev Similar to transfer(address, uint, bytes), but without _data parameter
-     * @param _to Address of token receiver
-     * @param _value Number of tokens to transfer
-     * @return Returns success of function call
-     */
-    function transfer(address _to, uint _value)
-        public
-        returns (bool success)
-    {
-        require(balances[msg.sender] >= _value); // sanity checks
-        require(balances[_to] + _value >= balances[_to]);
-
-        balances[msg.sender] = sub(balances[msg.sender], _value);
-        balances[_to] = add(balances[_to], _value);
-        emit Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    // PUBLIC VIEW METHODS
-
-    function getName() view returns (bytes32) { return name; }
-    function getSymbol() view returns (bytes8) { return symbol; }
-    function getDecimals() view returns (uint) { return decimal; }
-    function getCreationTime() view returns (uint) { return creationTime; }
-    function toSmallestShareUnit(uint quantity) view returns (uint) { return mul(quantity, 10 ** getDecimals()); }
-    function toWholeShareUnit(uint quantity) view returns (uint) { return quantity / (10 ** getDecimals()); }
-
-    // INTERNAL METHODS
-
-    /// @param recipient Address the new shares should be sent to
-    /// @param shareQuantity Number of shares to be created
-    function createShares(address recipient, uint shareQuantity) internal {
-        _totalSupply = add(_totalSupply, shareQuantity);
-        balances[recipient] = add(balances[recipient], shareQuantity);
-        emit Created(msg.sender, now, shareQuantity);
-        emit Transfer(address(0), recipient, shareQuantity);
-    }
-
-    /// @param recipient Address the new shares should be taken from when destroyed
-    /// @param shareQuantity Number of shares to be annihilated
-    function annihilateShares(address recipient, uint shareQuantity) internal {
-        _totalSupply = sub(_totalSupply, shareQuantity);
-        balances[recipient] = sub(balances[recipient], shareQuantity);
-        emit Annihilated(msg.sender, now, shareQuantity);
-        emit Transfer(recipient, address(0), shareQuantity);
+    function destroyFor(address who, uint amount) onlyControllers {
+        require(sub(balances[who], amount) >= 0);
+        totalSupply = sub(totalSupply, amount);
+        balances[who] = sub(balances[who], amount);
     }
 }
+
