9f161a71ab099b932b44c08496f8ed636d5e14fc
Travis Jacobs <traviswjacobs@gmail.com>
2018-08-24 16:38:28 -0230

    Add minimal PriceSource interface

---
+++ ref/prices/PriceSource.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.21
+
+/// @notice Returns values of various assets
+interface PriceSource {
+    function getPriceForPair(address base, address quote) view returns (uint);
+    function getPricesAgainstBase(address base, address[] quotes) view returns (uint[]);
+}
