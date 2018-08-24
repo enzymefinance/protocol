f0b5163fdacee1ade20e3619962fd5584e4b5102
Travis Jacobs <traviswjacobs@gmail.com>
2018-08-24 11:48:24 -0230

    Add Owned dependency

---
+++ ref/dependencies/Owned.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.21;
+
+
+contract Owned {
+
+    address public owner;
+
+    modifier onlyOwner {
+        require(isOwner(msg.sender));
+        _;
+    }
+
+    function Owned() {
+        owner = msg.sender;
+    }
+
+    function changeOwner(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
+
+    function isOwner(address _address) public view returns (bool) {
+        return _address == owner;
+    }
+}
