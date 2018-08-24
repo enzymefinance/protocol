4972de0e81021e34b0acf46ef9934f9251a2b150
Travis Jacobs <traviswjacobs@gmail.com>
2018-08-24 17:22:24 -0230

    Add minimal Hub

---
+++ ref/fund/hub/Hub.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.21;
+
+
+/// @notice Router for communication between components
+contract Hub {
+
+    // TODO: use the contract types instead of generic address when available
+    address public shares;
+    address public vault;
+    address public participation;
+    address public trading;
+    address public policyManager;
+    address public feeManager;
+    address public priceSource;
+    address public accounting;
+    address public version;
+
+    function Hub(
+        address _shares,
+        address _vault,
+        address _participation,
+        address _trading,
+        address _policyManager,
+        address _feeManager,
+        address _priceSource,
+        address _accounting,
+        address _version
+    ) {
+        shares = _shares;
+        vault = _vault;
+        participation = _participation;
+        trading = _trading;
+        policyManager = _policyManager;
+        feeManager = _feeManager;
+        priceSource = _priceSource;
+        accounting = _accounting;
+        version = _version;
+    }
+}
