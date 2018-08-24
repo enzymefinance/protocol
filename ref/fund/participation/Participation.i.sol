a531530a7d6238c25c253668538d13cfae9f7686
Travis Jacobs <traviswjacobs@gmail.com>
2018-08-24 11:41:14 -0230

    Add minimal Participation interface

--- ref/fund/participation/Participation.i.sol
+++ ref/fund/participation/Participation.i.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.4.21;
 /// @notice Investor Fund interactions
 /// @notice Handles redemptions and requests for investment
 interface Participation {
-    function requestInvestment(uint giveQuantity, uint shareQuantity, address investmentAsset) external;
-    function executeRequest(uint id) external;
-    function cancelRequest(uint id) external;
-    function enableInvestment(address[] ofAssets) external;
-    function disableInvestment(address[] ofAssets) external;
-    function redeemAllOwnedAssets(uint shareQuantiy) external returns (bool);
-    function emergencyRedeem(uint shareQuantity, address[] requestedAssets) public returns (bool);
-    function getLastRequestId() view returns (uint);
+    function requestInvestment(
+        uint requestedShares,
+        uint investmentAmount,
+        address investmentAsset
+    ) external;
+    function cancelRequest() external;
+    function executeRequest() external;
+    function executeRequestFor(address requestOwner) external;
+    function redeem() public;
+    function redeemWithConstraints(uint shareQuantity, address[] requestedAssets) public returns (bool);
 }
 
