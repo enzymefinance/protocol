4a73e1b9e8c46a5568bfe266977097c25ce8442c
Travis Jacobs <traviswjacobs@gmail.com>
2018-08-28 16:58:11 -0230

    Add very basic access control

---
+++ ref/dependencies/Controlled.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.21;
+
+
+/// @notice Light, append-only access control, like having multiple owners
+contract Controlled {
+
+    mapping (address => bool) controllers;
+
+    modifier onlyController {
+        require(isController(msg.sender));
+        _;
+    }
+
+    function Controlled(address[] _initialControllers) {
+        for (uint i = 0; i < _initialControllers.length; i++) {
+            controllers[_initialControllers] = true;
+        }
+    }
+
+    function addControllers(address[] _newController) public onlyController {
+        for (uint i = 0; i < _newControllers.length; i++) {
+            addController(_newControllers[i]);
+        }
+    }
+
+    function addController(address _newController) public onlyController {
+        controllers[_newController] = true;
+    }
+
+    function isController(address _address) public view returns (bool) {
+        return controllers[_address];
+    }
+}
+
