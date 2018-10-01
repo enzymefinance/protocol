ab19a9cf35a7f384a0d1c01b248c16d17d8132ad
GauthamN <onegautham@gmail.com>
2018-10-01 11:31:09 +0200

    Begin work on 0x v2 migration

--- src/Fund.sol
+++ src/Fund.sol
@@ -305,30 +305,27 @@ contract Fund is DSMath, DBC, Owned, Shares, FundInterface {
     /// @param method Signature of the adapter method to call (as per ABI spec)
     /// @param orderAddresses [0] Order maker
     /// @param orderAddresses [1] Order taker
-    /// @param orderAddresses [2] Order maker asset
-    /// @param orderAddresses [3] Order taker asset
-    /// @param orderAddresses [4] Fee recipient
-    /// @param orderValues [0] Maker token quantity
-    /// @param orderValues [1] Taker token quantity
+    /// @param orderAddresses [2] feeRecipientAddress
+    /// @param orderAddresses [3] senderAddress
+    /// @param orderValues [0] makerAssetAmount
+    /// @param orderValues [1] takerAssetAmount
     /// @param orderValues [2] Maker fee
     /// @param orderValues [3] Taker fee
-    /// @param orderValues [4] Timestamp (seconds)
+    /// @param orderValues [4] expirationTimeSeconds
     /// @param orderValues [5] Salt/nonce
     /// @param orderValues [6] Fill amount: amount of taker token to be traded
     /// @param orderValues [7] Dexy signature mode
     /// @param identifier Order identifier
-    /// @param v ECDSA recovery id
-    /// @param r ECDSA signature output r
-    /// @param s ECDSA signature output s
+    /// @param makerAssetData Encoded data specific to makerAsset.
+    /// @param takerAssetData Encoded data specific to takerAsset.
     function callOnExchange(
         uint exchangeIndex,
         bytes4 method,
-        address[5] orderAddresses,
+        address[4] orderAddresses,
         uint[8] orderValues,
         bytes32 identifier,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
+        bytes makerAssetData,
+        bytes takerAssetData
     )
         external
     {
@@ -340,7 +337,7 @@ contract Fund is DSMath, DBC, Owned, Shares, FundInterface {
         require(
             exchanges[exchangeIndex].exchangeAdapter.delegatecall(
                 method, exchanges[exchangeIndex].exchange,
-                orderAddresses, orderValues, identifier, v, r, s
+                orderAddresses, orderValues, identifier, makerAssetData, takerAssetData
             )
         );
     }
