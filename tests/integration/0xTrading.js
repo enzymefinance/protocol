68a12164a6eb1b7dfee2203f8268946e6314cf03
GauthamN <onegautham@gmail.com>
2018-10-11 06:51:29 +0200

    Use contracts wrapper to abstract away third-party take order

--- tests/integration/0xTrading.js
+++ tests/integration/0xTrading.js
@@ -443,30 +443,16 @@ test.serial("Third party fund takes the order made by the fund", async t => {
   ethToken.methods.approve(erc20ProxyAddress, order.takerAssetAmount).send(opts);
   const signedOrder = Object.assign({signature: orderSignature}, order);
   const contractsConfig = {
-    blockPollingIntervalMs: 1000,
     erc20ProxyContractAddress: "0x1dc4c1cefef38a777b15aa20260a54e584b16c48",
     erc721ProxyContractAddress: "0x1d7022f5b17d2f8b695918fb48fa1089c9f85401",
     exchangeContractAddress: "0x48bacb9266a570d521063ef5dd96e61686dbe788",
     forwarderContractAddress: "0xb69e673309512a9d726f87304c6984054f87a93b",
-    gasPrice: config.gasPrice,
-    networkId: contractsConfig.,
+    networkId: 100,
     zrxContractAddress: "0x871dd7c2b4b25e1aa18728e9d5f2af4c4e431f5c"
   }
-  const contractsWrapper = ContractWrappers(web3.currentProvider, contractsConfig);
-  //await contractWrappers.exchange.fillOrderAsync(signedOrder, takerAssetAmount, taker)
-  await zeroExExchange.methods
-    .fillOrder(
-      order,
-      order.makerAssetAmount,
-      orderSignature
-    )
-    .send({ from: deployer, gas: config.gas });
+  const contractsWrapper = new ContractWrappers(web3.currentProvider, contractsConfig);
+  await contractsWrapper.exchange.fillOrKillOrderAsync(signedOrder, new BigNumber(trade1.buyQuantity), deployer.toLowerCase());
   const post = await getAllBalances(deployed, accounts, fund);
-  const heldInExchange = await fund.methods
-    .quantityHeldInCustodyOfExchange(ethToken.options.address)
-    .call();
-
-  t.is(Number(heldInExchange), 0);
   t.deepEqual(
     post.deployer.MlnToken,
     pre.deployer.MlnToken.plus(trade1.sellQuantity)
