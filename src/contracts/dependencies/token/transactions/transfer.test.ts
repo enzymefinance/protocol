9c9f579f9657dc8f57e6f263d6f899572608ce8e
Simon Emanuel Schmid <simon@schmid.io>
2018-10-25 17:11:20 +0700

    Refactor token transfer

--- src/contracts/dependencies/token/transactions/transfer.test.ts
+++ src/contracts/dependencies/token/transactions/transfer.test.ts
@@ -1,45 +1,41 @@
-import { initTestEnvironment, constructEnvironment } from '~/utils/environment';
+import { createQuantity } from '@melonproject/token-math/quantity';
 
-import { deploy } from '..';
-import { transfer } from './transfer';
+import {
+  initTestEnvironment,
+  getGlobalEnvironment,
+  constructEnvironment,
+} from '~/utils/environment';
+
+import { Address } from '~/utils/types';
+
+import { transfer, deploy, getToken } from '..';
 
 const shared: any = {};
 
 beforeAll(async () => {
   await initTestEnvironment();
+  const environment = getGlobalEnvironment();
+
   shared.address = await deploy();
+  shared.token = await getToken(shared.address);
+  shared.accounts = (await environment.eth.getAccounts()).map(
+    account => new Address(account),
+  );
 });
 
 test('transfer', async () => {
-  const receipt = await transfer(shared.address, {
-    to: '0x1fEE0Ee72120B6A34C0FCdC93051Eb699a0D378B',
-    tokens: '1000000000000000000',
-  });
+  const howMuch = createQuantity(shared.token, '1000000000000000000');
+  const receipt = await transfer({ howMuch, to: shared.accounts[1] });
 
   expect(receipt).toBeTruthy();
 });
 
-// TODO: Make this test work
-test.skip('insufficent balance', async () => {
-  await expect(
-    transfer(shared.address, {
-      to: '0x1fEE0Ee72120B6A34C0FCdC93051Eb699a0D378B',
-      tokens: '2000000000000000000000000',
-    }),
-  ).rejects.toThrow('balance');
-});
-
-test('no account address', async () => {
-  const emptyEnvironment = constructEnvironment({});
-
-  await expect(
-    transfer(
-      shared.address,
-      {
-        to: '0x1fEE0Ee72120B6A34C0FCdC93051Eb699a0D378B',
-        tokens: '1000000000000000000',
-      },
-      emptyEnvironment,
-    ),
-  ).rejects.toThrow('No address');
-});
+// // TODO: Make this test work
+// test.skip('insufficent balance', async () => {
+// await expect(
+// transfer(shared.address, {
+// to: '0x1fEE0Ee72120B6A34C0FCdC93051Eb699a0D378B',
+// tokens: '2000000000000000000000000',
+// }),
+// ).rejects.toThrow('balance');
+// });
