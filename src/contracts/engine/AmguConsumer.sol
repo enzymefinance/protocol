500f8ad3dfa40c02998e1996f1a1d46e215b2fc1
Travis Jacobs <traviswjacobs@gmail.com>
2018-10-26 14:59:53 +0200

    Rename to AmguConsumer, make abstract and simplify

---
+++ src/contracts/engine/AmguConsumer.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.21;
+
+import "../dependencies/math.sol";
+import "../dependencies/token/ERC20.i.sol";
+import "../prices/PriceSource.sol";
+import "../version/Version.i.sol";
+import "./Engine.sol";
+
+// cleanup
+// TODO: can function know it is payable?
+// TODO: can modifier itself be payable?
+
+// TODO: collect AMGU in other contracts
+/// @notice inherit this pay AMGU
+contract AmguConsumer is DSMath {
+
+    modifier amguPayable() {
+        uint initialGas = gasleft();
+        _;
+        uint mlnPerAmgu = VersionInterface(version()).getAmguPrice();
+        uint ethPerMln;
+        (ethPerMln,) = PriceSource(priceSource()).getPrice(mlnAddress());
+        uint ethToPay = mul(
+            sub(initialGas, gasleft()),
+            mul(mlnPerAmgu, ethPerMln)
+        );
+        require(msg.value >= ethToPay);
+        Engine(engine()).payAmguInEther.value(ethToPay)();
+        msg.sender.transfer(msg.value - ethToPay);
+    }
+
+    function engine() view returns (address);
+    function mlnAddress() view returns (address);
+    function priceSource() view returns (address);
+    function version() view returns (address);
+}
+
