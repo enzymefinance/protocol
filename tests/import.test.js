90c9883cfdb97a374d4ad4fe6e1de8d85f32552b
Simon Emanuel Schmid <simon@schmid.io>
2018-10-26 11:39:51 +0200

    Fix esmodule import with tscpaths

--- tests/import.test.js
+++ tests/import.test.js
@@ -1,4 +1,20 @@
-
-test("Import this package from node", () => {
+test('Import this package from node', () => {
   const melonProtocol = require('../');
-})
+  expect(Object.keys(melonProtocol)).toEqual([
+    'utils',
+    'token',
+    'exchanges',
+    'factory',
+    'accounting',
+    'compliance',
+    'fees',
+    'hub',
+    'participation',
+    'policies',
+    'riskManagement',
+    'shares',
+    'trading',
+    'vault',
+    'prices',
+  ]);
+});
