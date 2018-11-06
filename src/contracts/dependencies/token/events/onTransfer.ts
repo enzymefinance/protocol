3c221a8fba576a03da10a62c6665b6da9d9bd353
Simon Emanuel Schmid <simon@schmid.io>
2018-11-06 19:27:16 +0100

    Postprocess events

--- src/contracts/dependencies/token/events/onTransfer.ts
+++ src/contracts/dependencies/token/events/onTransfer.ts
@@ -1,11 +1,21 @@
 import { Address } from '@melonproject/token-math/address';
 import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { getGlobalEnvironment } from '~/utils/environment';
 import { getContract, Contract } from '~/utils/solidity';
 
 export interface OnTransferFilter {
   from: Address;
+}
+
+interface ReturnValues {
+  from: string;
+  to: string;
+  value: string;
+}
+interface Log {
+  returnValues: ReturnValues;
 }
 
 const onTransfer = (
@@ -24,7 +34,13 @@ const onTransfer = (
   });
 
   // TODO: Error handling
-  return fromEvent(eventEmitter, 'data');
+  return fromEvent<Log>(eventEmitter, 'data').pipe(
+    map(log => ({
+      from: log.returnValues.from,
+      to: log.returnValues.to,
+      value: log.returnValues.value,
+    })),
+  );
 };
 
 export { onTransfer };
