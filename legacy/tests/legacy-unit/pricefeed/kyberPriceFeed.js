188d2d26ddbc108d25adc85f504f4a8d88e1b1f0
GauthamN <onegautham@gmail.com>
2018-11-08 16:29:01 +0100

    Add more test cases for Kyber pricefeed

--- legacy/tests/legacy-unit/pricefeed/kyberPriceFeed.js
+++ legacy/tests/legacy-unit/pricefeed/kyberPriceFeed.js
@@ -13,15 +13,14 @@ const precisionUnits = new BigNumber(10 ** 18).toFixed();
 // hoisted variables
 let accounts;
 let opts;
-let deployed;
 
 test.before(async () => {
   accounts = await web3.eth.getAccounts();
   opts = { from: accounts[0], gas: config.gas, gasPrice: config.gasPrice };
-  deployed = await deployEnvironment(environment);
 });
 
 test.beforeEach(async t => {
+  t.context.deployed = await deployEnvironment(environment);
   t.context.baseBuyRate = [];
   t.context.baseSellRate = [];
 });
@@ -36,9 +35,9 @@ test('Price is averaged over both sides', async t => {
   t.context.baseBuyRate.push(tokensPerEther);
   t.context.baseSellRate.push(ethersPerToken);
   const currentBlock = await web3.eth.getBlockNumber();
-  await deployed.ConversionRates.methods
+  await t.context.deployed.ConversionRates.methods
     .setBaseRate(
-      [deployed.MlnToken.options.address],
+      [t.context.deployed.MlnToken.options.address],
       t.context.baseBuyRate,
       t.context.baseSellRate,
       [],
@@ -47,8 +46,62 @@ test('Price is averaged over both sides', async t => {
       [],
     )
     .send(opts);
-  const priceFromFeed = (await deployed.KyberPriceFeed.methods
-    .getPrice(deployed.MlnToken.options.address)
+  const priceFromFeed = (await t.context.deployed.KyberPriceFeed.methods
+    .getPrice(t.context.deployed.MlnToken.options.address)
     .call()).price;
   t.deepEqual(mlnPrice, new BigNumber(priceFromFeed));
 });
+
+test('getRate smooths out the spread', async t => {
+    const actualPrice = new BigNumber(10 ** 18);
+    const spreadMultiplier = 1.001;
+    const ethersPerToken = actualPrice.mul(spreadMultiplier).toFixed();
+    const tokensPerEther = new BigNumber(precisionUnits)
+      .mul(precisionUnits).mul(spreadMultiplier)
+      .div(ethersPerToken)
+      .toFixed(0);
+    t.context.baseBuyRate.push(tokensPerEther);
+    t.context.baseSellRate.push(ethersPerToken);
+    const currentBlock = await web3.eth.getBlockNumber();
+    await t.context.deployed.ConversionRates.methods
+      .setBaseRate(
+        [t.context.deployed.MlnToken.options.address],
+        t.context.baseBuyRate,
+        t.context.baseSellRate,
+        [],
+        [],
+        currentBlock,
+        [],
+      )
+      .send(opts);
+      const priceFromFeed = (await t.context.deployed.KyberPriceFeed.methods
+        .getPrice(t.context.deployed.MlnToken.options.address)
+        .call()).price;
+      t.deepEqual(actualPrice, new BigNumber(priceFromFeed));
+  });
+
+test('Spread cannot be more than 10%', async t => {
+    const mlnPrice = new BigNumber(10 ** 18);
+    const ethersPerToken = mlnPrice.toFixed();
+    const tokensPerEther = new BigNumber(precisionUnits)
+      .mul(precisionUnits).mul(2)
+      .div(ethersPerToken)
+      .toFixed(0);
+    t.context.baseBuyRate.push(tokensPerEther);
+    t.context.baseSellRate.push(ethersPerToken);
+    const currentBlock = await web3.eth.getBlockNumber();
+    await t.context.deployed.ConversionRates.methods
+      .setBaseRate(
+        [t.context.deployed.MlnToken.options.address],
+        t.context.baseBuyRate,
+        t.context.baseSellRate,
+        [],
+        [],
+        currentBlock,
+        [],
+      )
+      .send(opts);
+    await t.throws(t.context.deployed.KyberPriceFeed.methods
+      .getPrice(t.context.deployed.MlnToken.options.address)
+      .call());    
+  });
