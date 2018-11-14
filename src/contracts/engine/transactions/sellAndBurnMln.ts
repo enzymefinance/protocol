b6e6299001c96b6c5684b40caf92b546dac6ce43
Travis Jacobs <traviswjacobs@gmail.com>
2018-11-14 20:15:46 -0330

    Add allowance.ts

--- src/contracts/engine/transactions/sellAndBurnMln.ts
+++ src/contracts/engine/transactions/sellAndBurnMln.ts
@@ -1,30 +1,24 @@
 import { isSameToken } from '@melonproject/token-math/token';
-import {
-  createQuantity,
-  isEqual,
-  greaterThan,
-} from '@melonproject/token-math/quantity';
+import { isEqual, greaterThan } from '@melonproject/token-math/quantity';
 import { Address } from '~/utils/types';
 import { getContract, transactionFactory } from '~/utils/solidity';
-import { getToken } from '~/contracts/dependencies/token';
+import { getToken, allowance } from '~/contracts/dependencies/token';
 import { ensure } from '~/utils/guards';
 import { Contracts } from '~/Contracts';
 
 const guard = async ({ quantity }, contractAddress: Address, environment) => {
   const engine = getContract(Contracts.Engine, contractAddress);
   const mlnAddress = await engine.methods.mlnToken().call();
-  const mlnTokenContract = getContract(Contracts.StandardToken, mlnAddress);
   const mlnToken = await getToken(mlnAddress);
   ensure(
     isSameToken(quantity.token, mlnToken),
     'It is only possible to burn MLN',
   );
-  const allowedMln = createQuantity(
-    mlnToken,
-    await mlnTokenContract.methods
-      .allowance(environment.wallet.address, contractAddress.toString())
-      .call(),
-  );
+  const allowedMln = await allowance(mlnAddress, {
+    owner: environment.wallet.address,
+    spender: contractAddress.toString(),
+  });
+
   ensure(
     isEqual(allowedMln, quantity) || greaterThan(allowedMln, quantity),
     `Amount must be approved prior to calling this function.`,
