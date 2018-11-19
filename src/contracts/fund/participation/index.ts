ec09e61aa092546973b00fb0f80a5e3f82243e8f
Travis Jacobs <traviswjacobs@gmail.com>
2018-11-19 14:37:06 -0330

    Change transaction argument type

--- src/contracts/fund/participation/index.ts
+++ src/contracts/fund/participation/index.ts
@@ -7,6 +7,5 @@ export * from './transactions/executeRequest';
 export * from './transactions/executeRequestFor';
 export * from './transactions/redeem';
 export * from './transactions/redeemQuantity';
-export * from './transactions/requestInvestment';
 export * from './transactions/redeemWithConstraints';
 export * from './transactions/requestInvestment';
