3d629e8b3fa74c5df93933926206de46f98c56b1
Travis Jacobs <traviswjacobs@gmail.com>
2018-11-20 19:21:30 -0330

    WIP: Add invest/cancel tests

--- src/contracts/fund/participation/unit/investment.test.ts
+++ src/contracts/fund/participation/unit/investment.test.ts
@@ -29,4 +29,63 @@ test('Invest fails in shut down fund', async () => {
     .send({ from: shared.user, gas: 8000000 });
 
   await shared.hub.methods.setShutDownState(true).send({ from: shared.user });
+
+  await expect(
+    shared.participation.methods
+      .executeRequest()
+      .send({ from: shared.user, gas: 8000000 }),
+  ).rejects.toThrow(errorMessage);
+
+  await shared.hub.methods.setShutDownState(false).send({ from: shared.user });
+});
+
+test('Request must exist to execute', async () => {
+  console.log(shared.participation.options.address);
+  console.log(shared.weth.options.address);
+  console.log(shared.priceSource.options.address);
+  console.log(await shared.participation.methods.routes().call());
+  console.log(shared.user);
+  console.log(await shared.participation.methods.requests(shared.user).call());
+  const errorMessage = 'No request for this address';
+  await shared.participation.methods
+    .cancelRequest()
+    .send({ from: shared.user, gas: 8000000 });
+  const requestExists = await shared.participation.methods
+    .hasRequest(shared.user)
+    .call();
+
+  expect(requestExists).toBe(false);
+  await expect(
+    shared.participation.methods
+      .executeRequest()
+      .send({ from: shared.user, gas: 8000000 }),
+  ).rejects.toThrow(errorMessage);
+
+  await shared.participation.methods
+    .requestInvestment(0, 0, shared.weth.options.address)
+    .send({ from: shared.user, gas: 8000000 });
+
+  await expect(
+    shared.participation.methods
+      .executeRequest()
+      .send({ from: shared.user, gas: 8000000 }),
+  ).rejects.toThrow(errorMessage);
+});
+
+test('Need fresh price to executeRequest', async () => {
+  const errorMessage = 'Price not recent';
+  const amount = '1000000000000000000';
+  await shared.participation.methods
+    .requestInvestment(amount, amount, shared.weth.options.address)
+    .send({ from: shared.user, gas: 8000000 });
+  const requestExists = await shared.participation.methods
+    .hasRequest(shared.user)
+    .call();
+
+  expect(requestExists).toBe(true);
+  await expect(
+    shared.participation.methods
+      .executeRequestFor(shared.user)
+      .send({ from: shared.user, gas: 8000000 }),
+  ).rejects.toThrow(errorMessage);
 });
