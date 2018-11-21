c5a8899b82af93c07fccb352e3b1e81f4b01ccd2
GauthamN <onegautham@gmail.com>
2018-11-21 16:45:57 +0100

    Test for returnToVault

--- src/contracts/fund/trading/unit/trading.test.ts
+++ src/contracts/fund/trading/unit/trading.test.ts
@@ -2,7 +2,10 @@ import { Contracts } from '~/Contracts';
 import { initTestEnvironment } from '~/utils/environment';
 import { deployMockSystem } from '~/utils';
 import { deploy, getContract } from '~/utils/solidity';
+import { emptyAddress } from '~/utils/constants';
 import { randomAddress } from '~/utils/helpers';
+import { share } from 'rxjs/operators';
+import { BigNumber } from 'bignumber.js';
 
 let shared: any = {};
 
@@ -17,10 +20,7 @@ beforeAll(async () => {
   shared.env = await initTestEnvironment();
   shared = Object.assign(shared, await deployMockSystem());
   shared.user = shared.env.wallet.address;
-});
-
-test('Exchanges are properly initialized', async () => {
-  const trading = getContract(
+  shared.trading = getContract(
     Contracts.Trading,
     await deploy(Contracts.Trading, [
       shared.hub.options.address,
@@ -29,21 +29,38 @@ test('Exchanges are properly initialized', async () => {
       takesCustodyMasks,
     ]),
   );
+  await shared.trading.methods
+    .initialize([
+      emptyAddress,
+      emptyAddress,
+      emptyAddress,
+      emptyAddress,
+      emptyAddress,
+      emptyAddress,
+      shared.vault.options.address,
+      emptyAddress,
+      emptyAddress,
+      emptyAddress,
+      emptyAddress,
+      emptyAddress,
+    ])
+    .send({ from: shared.user, gas: 8000000 });
+});
 
+test('Exchanges are properly initialized', async () => {
   for (const i of Array.from(Array(mockExchanges.length).keys())) {
-    const exchangeObject = await trading.methods.exchanges(i).call();
+    const exchangeObject = await shared.trading.methods.exchanges(i).call();
     expect(exchangeObject.exchange.toLowerCase()).toBe(mockExchanges[i]);
     expect(exchangeObject.adapter.toLowerCase()).toBe(mockExchangeAdapters[i]);
     expect(exchangeObject.takesCustody).toBe(takesCustodyMasks[i]);
     await expect(
-      trading.methods.exchangeIsAdded(mockExchanges[i]).call(),
+      shared.trading.methods.exchangeIsAdded(mockExchanges[i]).call(),
     ).toBeTruthy();
   }
 });
 
 test('Exchanges cant be initialized without its adapter', async () => {
   const errorMessage = 'Array lengths unequal';
-
   await expect(
     deploy(Contracts.Trading, [
       shared.hub.options.address,
@@ -52,4 +69,49 @@ test('Exchanges cant be initialized without its adapter', async () => {
       takesCustodyMasks,
     ]),
   ).rejects.toThrow(errorMessage);
+});
+
+test('returnToVault sends back token balances to the vault', async () => {
+  const tokenQuantity = new BigNumber(10 ** 20);
+  await shared.mln.methods
+    .transfer(shared.trading.options.address, new BigNumber(10 ** 20).toFixed())
+    .send({ from: shared.user, gas: 8000000 });
+  await shared.weth.methods
+    .transfer(shared.trading.options.address, new BigNumber(10 ** 20).toFixed())
+    .send({ from: shared.user, gas: 8000000 });
+
+  const preMlnTrading = new BigNumber(
+    await shared.mln.methods.balanceOf(shared.trading.options.address).call(),
+  );
+  const preWethTrading = new BigNumber(
+    await shared.weth.methods.balanceOf(shared.trading.options.address).call(),
+  );
+  const preMlnVault = new BigNumber(
+    await shared.mln.methods.balanceOf(shared.vault.options.address).call(),
+  );
+  const preWethVault = new BigNumber(
+    await shared.weth.methods.balanceOf(shared.vault.options.address).call(),
+  );
+
+  await shared.trading.methods
+    .returnToVault([shared.mln.options.address, shared.weth.options.address])
+    .send({ from: shared.user, gas: 8000000 });
+
+  const postMlnTrading = new BigNumber(
+    await shared.mln.methods.balanceOf(shared.trading.options.address).call(),
+  );
+  const postWethTrading = new BigNumber(
+    await shared.weth.methods.balanceOf(shared.trading.options.address).call(),
+  );
+  const postMlnVault = new BigNumber(
+    await shared.mln.methods.balanceOf(shared.vault.options.address).call(),
+  );
+  const postWethVault = new BigNumber(
+    await shared.weth.methods.balanceOf(shared.vault.options.address).call(),
+  );
+
+  expect(postMlnTrading).toEqual(new BigNumber(0));
+  expect(postWethTrading).toEqual(new BigNumber(0));
+  expect(postMlnVault).toEqual(preMlnVault.add(tokenQuantity));
+  expect(postWethVault).toEqual(preWethVault.add(tokenQuantity));
 });
