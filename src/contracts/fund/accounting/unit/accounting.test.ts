5681ef13f15ca11871e914e5af35afb47a10a492
GauthamN <onegautham@gmail.com>
2018-11-22 18:47:12 +0100

    Use resolves for async tests

--- src/contracts/fund/accounting/unit/accounting.test.ts
+++ src/contracts/fund/accounting/unit/accounting.test.ts
@@ -16,31 +16,37 @@ import {
 
 let shared: any = {};
 
-// Mock data
-const mockDefaultAssets = [randomAddress(), randomAddress()];
-
-const mockQuoteAsset = randomAddress();
-
 beforeAll(async () => {
   shared.env = await initTestEnvironment();
   shared = Object.assign(shared, await deployMockSystem());
   shared.user = shared.env.wallet.address;
+  shared.mockDefaultAssets = [shared.weth.options.address];
+  shared.mockQuoteAsset = shared.weth.options.address;
   shared.accounting = getContract(
     Contracts.Accounting,
     await deploy(Contracts.Accounting, [
       shared.hub.options.address,
-      mockQuoteAsset,
-      mockDefaultAssets,
+      shared.mockQuoteAsset,
+      shared.mockDefaultAssets,
     ]),
   );
 });
 
 test('Accounting is properly initialized', async () => {
-  for (const i of Array.from(Array(mockDefaultAssets.length).keys())) {
+  for (const i of Array.from(Array(shared.mockDefaultAssets.length).keys())) {
     const defaultAsset = await shared.accounting.methods.ownedAssets(i).call();
-    expect(defaultAsset.toLowerCase()).toBe(mockDefaultAssets[i]);
+    expect(defaultAsset).toBe(shared.mockDefaultAssets[i]);
     await expect(
-      shared.accounting.methods.isInAssetList(mockDefaultAssets[i]).call(),
-    ).toBeTruthy();
+      shared.accounting.methods
+        .isInAssetList(shared.mockDefaultAssets[i])
+        .call(),
+    ).resolves.toBeTruthy();
   }
+
+  await expect(shared.accounting.methods.QUOTE_ASSET().call()).resolves.toBe(
+    shared.mockQuoteAsset,
+  );
+  // await expect(shared.accounting.methods.calcSharePrice().call()).resolves.toBe(
+  //   `${new BigInteger(10 ** 18)}`,
+  // );
 });
