f20956d22e84da1d11168ac27a5d7f55719e23ff
GauthamN <onegautham@gmail.com>
2018-11-23 17:38:25 +0100

    Remove toLowerCase from randomAddress

--- src/utils/helpers/randomAddress.ts
+++ src/utils/helpers/randomAddress.ts
@@ -1,5 +1,4 @@
 import * as Web3Utils from 'web3-utils';
 import { Address } from '~/utils/types';
 
-export const randomAddress = () =>
-  new Address(Web3Utils.randomHex(20)).toLowerCase();
+export const randomAddress = () => new Address(Web3Utils.randomHex(20));
