6265842b5416def73bda9e28ebdbc3cca5aecd8b
Simon Emanuel Schmid <simon@schmid.io>
2018-11-28 11:21:20 +0100

    Fix in-memory ganache

--- src/utils/environment/initTestEnvironment.ts
+++ src/utils/environment/initTestEnvironment.ts
@@ -7,7 +7,11 @@ const getGanache = () => {
   debug('Setting Ganache up');
   // tslint:disable-next-line:variable-name
   const Ganache = require('@melonproject/ganache-cli');
-  const provider = Ganache.provider();
+  const provider = Ganache.provider({
+    gasLimit: '0x7a1200',
+    // tslint:disable-next-line:object-literal-sort-keys
+    default_balance_ether: 10000000000000,
+  });
   debug('Ganache setup finished');
   return provider;
 };
