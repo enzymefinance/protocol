aef45f9527c3a55e4a676e8089e301e55cd75080
Travis Jacobs <traviswjacobs@gmail.com>
2018-11-30 13:46:46 -0330

    Re-enable a couple checks

--- src/contracts/fund/hub/transactions/shutDownFund.ts
+++ src/contracts/fund/hub/transactions/shutDownFund.ts
@@ -1,22 +1,21 @@
 import { transactionFactory } from '~/utils/solidity/transactionFactory';
-// import { ensure } from '~/utils/guards/ensure';
+import { ensure } from '~/utils/guards/ensure';
 import { Contracts } from '~/Contracts';
 // import { ensureIsManager } from '~/contracts/fund/hub/guards/ensureIsManager';
-// import { ensureIsNotShutDown } from '~/contracts/fund/hub/guards/ensureIsNotShutDown';
-// import { isShutDown } from '~/contracts/fund/hub/calls/isShutDown';
+import { ensureIsNotShutDown } from '~/contracts/fund/hub/guards/ensureIsNotShutDown';
+import { isShutDown } from '~/contracts/fund/hub/calls/isShutDown';
 
 const guard = async (params, contractAddress, environment) => {
   // TODO: re-enable check (for hub address, not this Version contract)
-  // await ensureIsNotShutDown(contractAddress, environment);
-  // await ensureIsManager(contractAddress, environment);
+  await ensureIsNotShutDown(`${params.hub}`, environment);
+  // await ensureIsManager(`${contractAddress}`, environment);
 };
 
 const prepareArgs = async ({ hub }) => [`${hub}`];
 
 const postProcess = async (receipt, params, contractAddress, environment) => {
-  // TODO: re-enable check (for hub address, not this Version contract)
-  // const shutDown = await isShutDown(contractAddress, null, environment);
-  // ensure(shutDown, 'Fund is not shut down');
+  const shutDown = await isShutDown(`${params.hub}`, null, environment);
+  ensure(shutDown, 'Fund is not shut down');
   return true;
 };
 
