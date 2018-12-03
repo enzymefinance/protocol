014df679fd6c4ace5a0faa81fcfe70bfb0811426
Sebastian Siemssen <fubhy@fubhy.com>
2018-12-03 14:12:58 +0100

    Export getHub call.

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export {
 } from '~/utils/solidity/getDeployment';
 export { balanceOf } from '~/contracts/dependencies/token/calls/balanceOf';
 export { managersToHubs } from '~/contracts/factory/calls/managersToHubs';
+export { getHub } from '~/contracts/fund/hub/calls/getHub';
