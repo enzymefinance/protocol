0752f452eee64b625a861b0cd1187ce539c3225a
Travis Jacobs <traviswjacobs@gmail.com>
2018-12-05 11:55:05 +0100

    Re-enable check

--- src/contracts/fund/hub/transactions/shutDownFund.ts
+++ src/contracts/fund/hub/transactions/shutDownFund.ts
@@ -1,14 +1,13 @@
 import { transactionFactory } from '~/utils/solidity/transactionFactory';
 import { ensure } from '~/utils/guards/ensure';
 import { Contracts } from '~/Contracts';
-// import { ensureIsManager } from '~/contracts/fund/hub/guards/ensureIsManager';
+import { ensureIsManager } from '~/contracts/fund/hub/guards/ensureIsManager';
 import { ensureIsNotShutDown } from '~/contracts/fund/hub/guards/ensureIsNotShutDown';
 import { isShutDown } from '~/contracts/fund/hub/calls/isShutDown';
 
 const guard = async (params, contractAddress, environment) => {
-  // TODO: re-enable check (for hub address, not this Version contract)
   await ensureIsNotShutDown(`${params.hub}`, environment);
-  // await ensureIsManager(`${contractAddress}`, environment);
+  await ensureIsManager(`${params.hub}`, environment);
 };
 
 const prepareArgs = async ({ hub }) => [`${hub}`];
