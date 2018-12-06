4111efb51c69bf3e91ffc800b39650e7f78d81c9
Sebastian Siemssen <fubhy@fubhy.com>
2018-12-06 15:16:15 +0100

    Adding root exports.

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,7 @@ export { getSettings } from '~/contracts/fund/hub/calls/getSettings';
 export {
   performCalculations,
 } from '~/contracts/fund/accounting/calls/performCalculations';
+export { hasRecentPrice } from '~/contracts/prices/calls/hasRecentPrice';
+export { getQuoteToken } from '~/contracts/prices/calls/getQuoteToken';
+export { getPrice } from '~/contracts/prices/calls/getPrice';
+export { getPrices } from '~/contracts/prices/calls/getPrices';
