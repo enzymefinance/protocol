a7c6fa9b8935a7b533c3a0e47dc4e2c35262f75c
Sebastian Siemssen <fubhy@fubhy.com>
2018-12-06 14:31:42 +0100

    Add hasRecentPrice call.

---
+++ src/contracts/prices/calls/hasRecentPrice.ts
@@ -0,0 +1,18 @@
+import { TokenInterface } from '@melonproject/token-math/token';
+import { Environment } from '~/utils/environment/Environment';
+import { getContract } from '~/utils/solidity/getContract';
+import { Contracts } from '~/Contracts';
+
+export const hasRecentPrice = async (
+  contractAddress: string,
+  token: TokenInterface,
+  environment?: Environment,
+): Promise<boolean> => {
+  const contract = await getContract(
+    Contracts.TestingPriceFeed,
+    contractAddress,
+    environment,
+  );
+
+  return contract.methods.hasRecentPrice(token.address).call();
+};
