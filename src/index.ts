215c7c6a7a64f860a9e3d2421299c037612f6f72
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-12-13 10:10:30 +0100

    Add exports

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,39 @@
-export { constructEnvironment } from '~/utils/environment/constructEnvironment';
+export { approve } from '~/contracts/dependencies/token/transactions/approve';
 export { balanceOf } from '~/contracts/dependencies/token/calls/balanceOf';
-export { getInfo } from '~/contracts/dependencies/token/calls/getInfo';
-export { managersToHubs } from '~/contracts/factory/calls/managersToHubs';
-export { getFundDetails } from '~/contracts/factory/calls/getFundDetails';
-export { getHub } from '~/contracts/fund/hub/calls/getHub';
-export { getName } from '~/contracts/fund/hub/calls/getName';
-export { getManager } from '~/contracts/fund/hub/calls/getManager';
-export { getSettings } from '~/contracts/fund/hub/calls/getSettings';
-export {
-  performCalculations,
-} from '~/contracts/fund/accounting/calls/performCalculations';
-export { hasRecentPrice } from '~/contracts/prices/calls/hasRecentPrice';
-export { getQuoteToken } from '~/contracts/prices/calls/getQuoteToken';
-export { getPrice } from '~/contracts/prices/calls/getPrice';
-export { getPrices } from '~/contracts/prices/calls/getPrices';
-export {
-  createComponents,
-} from '~/contracts/factory/transactions/createComponents';
+export { constructEnvironment } from '~/utils/environment/constructEnvironment';
 export {
   continueCreation,
 } from '~/contracts/factory/transactions/continueCreation';
-export { setupFund } from '~/contracts/factory/transactions/setupFund';
 export {
-  requestInvestment,
-} from '~/contracts/fund/participation/transactions/requestInvestment';
+  createComponents,
+} from '~/contracts/factory/transactions/createComponents';
+export { deploySystem } from '~/utils/deploySystem';
 export {
   executeRequest,
 } from '~/contracts/fund/participation/transactions/executeRequest';
-export { approve } from '~/contracts/dependencies/token/transactions/approve';
-export { isEmptyAddress } from '~/utils/checks/isEmptyAddress';
+export { getFundDetails } from '~/contracts/factory/calls/getFundDetails';
+export {
+  getFundHoldings,
+} from '~/contracts/fund/accounting/calls/getFundHoldings';
+export { getHub } from '~/contracts/fund/hub/calls/getHub';
+export { getInfo } from '~/contracts/dependencies/token/calls/getInfo';
+export { getManager } from '~/contracts/fund/hub/calls/getManager';
+export { getName } from '~/contracts/fund/hub/calls/getName';
+export { getPrice } from '~/contracts/prices/calls/getPrice';
+export { getPrices } from '~/contracts/prices/calls/getPrices';
+export { getQuoteToken } from '~/contracts/prices/calls/getQuoteToken';
+export { getSettings } from '~/contracts/fund/hub/calls/getSettings';
+export { getStepFor } from '~/contracts/factory/calls/getStepFor';
+export { getTokenBySymbol } from '~/utils/environment/getTokenBySymbol';
+export { hasRecentPrice } from '~/contracts/prices/calls/hasRecentPrice';
 export { isAddress } from '~/utils/checks/isAddress';
+export { isEmptyAddress } from '~/utils/checks/isEmptyAddress';
+export { managersToHubs } from '~/contracts/factory/calls/managersToHubs';
+export {
+  performCalculations,
+} from '~/contracts/fund/accounting/calls/performCalculations';
+export {
+  requestInvestment,
+} from '~/contracts/fund/participation/transactions/requestInvestment';
+export { setupFund } from '~/contracts/factory/transactions/setupFund';
+export { update } from '~/contracts/prices/transactions/update';
