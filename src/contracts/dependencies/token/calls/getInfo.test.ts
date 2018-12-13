a29ec6e0447b5e692087390ee84606f54e38da99
Simon Emanuel Schmid <simon@schmid.io>
2018-12-13 18:51:29 +0100

    Deployment token info overrides on-chain token-info

--- src/contracts/dependencies/token/calls/getInfo.test.ts
+++ src/contracts/dependencies/token/calls/getInfo.test.ts
@@ -1,12 +1,21 @@
 import { initTestEnvironment } from '~/tests/utils/initTestEnvironment';
 import { getInfo } from '../calls/getInfo';
 import { deployToken } from '../transactions/deploy';
+import { Environment } from '~/utils/environment/Environment';
+import testJson from '~/../deployments/test.json';
 
 describe('getInfo', () => {
-  const shared: any = {};
+  const shared: {
+    env?: Environment;
+    [p: string]: any;
+  } = {};
 
   beforeAll(async () => {
-    shared.env = await initTestEnvironment();
+    const env = await initTestEnvironment();
+    shared.env = {
+      ...env,
+      deployment: testJson,
+    };
     shared.address = await deployToken(shared.env);
   });
 
@@ -17,5 +26,29 @@ describe('getInfo', () => {
     expect(info.name).toBe('Premined Token');
     expect(info.decimals).toBe(18);
     expect(info.totalSupply).toBe(1000000 * 10 ** 18);
+  });
+
+  it('Info gets overridden', async () => {
+    const tokenAddress = await deployToken(shared.env, 'TEST');
+
+    const modifiedEnv = {
+      ...shared.env,
+      deployment: {
+        ...shared.env.deployment,
+        thirdpartyContracts: {
+          ...shared.env.deployment.thirdpartyContracts,
+          tokens: [
+            {
+              address: tokenAddress.toString(),
+              decimals: 18,
+              symbol: 'NOT-TEST',
+            },
+          ],
+        },
+      },
+    };
+
+    const info = await getInfo(modifiedEnv, tokenAddress);
+    expect(info.symbol).toBe('NOT-TEST');
   });
 });
