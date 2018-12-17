79070b8311ba0a27b2d7e5858eafbbe866a41969
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-12-17 11:00:59 +0100

    Fix shareprice call & add getNativeToken

---
+++ src/contracts/fund/accounting/calls/getNativeToken.ts
@@ -0,0 +1,23 @@
+import { callFactoryWithoutParams } from '~/utils/solidity/callFactory';
+import { Contracts } from '~/Contracts';
+import { getToken } from '~/contracts/dependencies/token/calls/getToken';
+import { TokenInterface } from '@melonproject/token-math/token';
+
+const postProcess = async (
+  environment,
+  result,
+  prepared,
+): Promise<TokenInterface> => {
+  const fundToken = await getToken(environment, result);
+  return fundToken;
+};
+
+const getNativeToken = callFactoryWithoutParams(
+  'NATIVE_ASSET',
+  Contracts.Accounting,
+  {
+    postProcess,
+  },
+);
+
+export { getNativeToken };
--- src/contracts/fund/accounting/calls/performCalculations.ts
+++ src/contracts/fund/accounting/calls/performCalculations.ts
@@ -5,11 +5,15 @@ import {
   createQuantity,
   QuantityInterface,
 } from '@melonproject/token-math/quantity';
+import { getHub } from '~/contracts/fund/hub/calls/getHub';
+import { getSettings } from '../../hub/calls/getSettings';
+import { getToken } from '~/contracts/dependencies/token/calls/getToken';
+import { getPrice, PriceInterface } from '@melonproject/token-math/price';
 
 interface PerformCalculationsResult {
   gav: QuantityInterface;
   nav: QuantityInterface;
-  sharePrice: QuantityInterface;
+  sharePrice: PriceInterface;
 }
 
 const postProcess = async (
@@ -18,10 +22,17 @@ const postProcess = async (
   prepared,
 ): Promise<PerformCalculationsResult> => {
   const quoteToken = await getQuoteToken(environment, prepared.contractAddress);
+  const hub = await getHub(environment, prepared.contractAddress);
+  const settings = await getSettings(environment, hub);
+  const fundToken = await getToken(environment, settings.sharesAddress);
+
   const calculations = {
     gav: createQuantity(quoteToken, result.gav),
     nav: createQuantity(quoteToken, result.nav),
-    sharePrice: createQuantity(quoteToken, result.sharePrice),
+    sharePrice: getPrice(
+      createQuantity(fundToken, 1),
+      createQuantity(quoteToken, result.sharePrice),
+    ),
   };
 
   return calculations;
