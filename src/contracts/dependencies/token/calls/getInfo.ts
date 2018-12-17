244170f1be16ec2fbc0f4921c2b8b47ce85e5f56
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-12-17 16:47:09 +0100

    Fix totalSupply in getInfo

--- src/contracts/dependencies/token/calls/getInfo.ts
+++ src/contracts/dependencies/token/calls/getInfo.ts
@@ -15,7 +15,7 @@ export const getInfo = async (
   const symbol = await contract.methods.symbol().call();
   const name = await contract.methods.name().call();
   const decimals = parseInt(await contract.methods.decimals().call(), 10);
-  const totalSupply = parseInt(await contract.methods.totalSupply().call(), 10);
+  const totalSupply = await contract.methods.totalSupply().call();
 
   const fromChain = {
     decimals,
