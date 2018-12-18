fdaf8ce99e02174b1896be212db987bcf5341fb4
Simon Emanuel Schmid <simon@schmid.io>
2018-12-18 18:36:57 +0100

    Setup a first system test linking to kovan through infura

---
+++ src/utils/environment/withNewAccount.ts
@@ -0,0 +1,24 @@
+import { default as Web3Accounts } from 'web3-eth-accounts';
+
+import { Environment } from './Environment';
+
+const withNewAccount = (environment: Environment) => {
+  const web3Accounts = new Web3Accounts(environment.eth.currentProvider);
+
+  const account = web3Accounts.create();
+
+  const signer = unsignedTransaction =>
+    account.signTransaction(unsignedTransaction);
+
+  const withWallet = {
+    ...environment,
+    wallet: {
+      address: account.address,
+      sign: signer,
+    },
+  };
+
+  return withWallet;
+};
+
+export { withNewAccount };
