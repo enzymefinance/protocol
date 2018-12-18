18904d1183739bb9a307e83c61cda4f1c181ab58
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-12-18 11:31:52 +0100

    Add quoteAsset to fundRanking

--- src/contracts/factory/FundRanking.sol
+++ src/contracts/factory/FundRanking.sol
@@ -7,11 +7,12 @@ import "Accounting.sol";
 contract FundRanking {
     function getFundDetails(address _factory)
         view
-        returns(address[], uint[], uint[], string[])
+        returns(address[], address[], uint[], uint[], string[])
     {
         FundFactory factory = FundFactory(_factory);
         uint numberOfFunds = factory.getLastFundId() + 1;
         address[] memory hubs = new address[](numberOfFunds);
+        address[] memory quoteAssets = new address[](numberOfFunds);
         uint[] memory sharePrices = new uint[](numberOfFunds);
         uint[] memory creationTimes = new uint[](numberOfFunds);
         string[] memory names = new string[](numberOfFunds);
@@ -19,8 +20,10 @@ contract FundRanking {
         for (uint i = 0; i < numberOfFunds; i++) {
             address hubAddress = factory.funds(i);
             Hub hub = Hub(hubAddress);
+            Account accounting = Accounting(hub.accounting());
             hubs[i] = hubAddress;
-            sharePrices[i] = Accounting(hub.accounting()).calcSharePrice();
+            sharePrices[i] = accounting.calcSharePrice();
+            quoteAssets[i] = accounting.QUOTE_ASSET();
             creationTimes[i] = hub.creationTime();
             names[i] = hub.name();
         }
