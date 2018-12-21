b751d72a6bf86baee64d7a5c8a70c6464aec5d97
Ivan Herger <ivan.herger@gmail.com>
2018-12-21 13:43:53 +0100

    Comment/uncomment

--- src/tests/utils/setupInvestedTestFund.ts
+++ src/tests/utils/setupInvestedTestFund.ts
@@ -1,4 +1,4 @@
-import { createQuantity } from '@melonproject/token-math/quantity';
+// import { createQuantity } from '@melonproject/token-math/quantity';
 import { randomString } from '~/utils/helpers/randomString';
 import { beginSetup } from '~/contracts/factory/transactions/beginSetup';
 import { completeSetup } from '~/contracts/factory/transactions/completeSetup';
@@ -10,9 +10,9 @@ import { createShares } from '~/contracts/factory/transactions/createShares';
 import { createTrading } from '~/contracts/factory/transactions/createTrading';
 import { createVault } from '~/contracts/factory/transactions/createVault';
 import { getRoutes } from '~/contracts/fund/hub/calls/getRoutes';
-import { requestInvestment } from '~/contracts/fund/participation/transactions/requestInvestment';
-import { approve } from '~/contracts/dependencies/token/transactions/approve';
-import { executeRequest } from '~/contracts/fund/participation/transactions/executeRequest';
+// import { requestInvestment } from '~/contracts/fund/participation/transactions/requestInvestment';
+// import { approve } from '~/contracts/dependencies/token/transactions/approve';
+// import { executeRequest } from '~/contracts/fund/participation/transactions/executeRequest';
 import { Environment } from '~/utils/environment/Environment';
 
 const setupInvestedTestFund = async (environment: Environment) => {
