dec85aa1aa6f92520b1b673554ee15a758cddbf3
Simon Emanuel Schmid <simon@schmid.io>
2018-12-28 19:04:43 +0100

    Oasis trade in systemtest

--- src/tests/system/playground-testing.systest.ts
+++ src/tests/system/playground-testing.systest.ts
@@ -1,86 +1,117 @@
-import { getBalance } from '~/utils/evm/getBalance';
-import { withNewAccount } from '~/utils/environment/withNewAccount';
 import {
-  toFixed,
   createQuantity,
   greaterThan,
   isEqual,
 } from '@melonproject/token-math/quantity';
-import {
-  createPrice,
-  toFixed as toFixedPrice,
-} from '@melonproject/token-math/price';
+import { createPrice } from '@melonproject/token-math/price';
+
+import { getBalance } from '~/utils/evm/getBalance';
+import { withNewAccount } from '~/utils/environment/withNewAccount';
 import { sendEth } from '~/utils/evm/sendEth';
 import { setupInvestedTestFund } from '../utils/setupInvestedTestFund';
-// import { getAmguToken } from '~/contracts/engine/calls/getAmguToken';
-
 import { deposit } from '~/contracts/dependencies/token/transactions/deposit';
 import { getTokenBySymbol } from '~/utils/environment/getTokenBySymbol';
 import { setAmguPrice } from '~/contracts/engine/transactions/setAmguPrice';
 import { getAmguToken } from '~/contracts/engine/calls/getAmguToken';
-
 import { getPrice } from '~/contracts/prices/calls/getPrice';
 import { update } from '~/contracts/prices/transactions/update';
 import { getSystemTestEnvironment } from '../utils/getSystemTestEnvironment';
 import { toBeTrueWith } from '../utils/toBeTrueWith';
-// import { setAmguPrice } from '~/contracts/engine/transactions/setAmguPrice';
+import { transfer } from '~/contracts/dependencies/token/transactions/transfer';
+import { Exchanges } from '~/Contracts';
+import { makeOrderFromAccountOasisDex } from '~/contracts/exchanges/transactions/makeOrderFromAccountOasisDex';
+import { takeOasisDexOrder } from '~/contracts/fund/trading/transactions/takeOasisDexOrder';
+import { performCalculations } from '~/contracts/fund/accounting/calls/performCalculations';
+import { getLogCurried } from '~/utils/environment/getLogCurried';
 
 expect.extend({ toBeTrueWith });
 
+const getLog = getLogCurried('melon:protocol:systemTest:playground');
+
 describe('playground', () => {
   test('Happy path', async () => {
-    const masterEnvironment = await getSystemTestEnvironment();
+    const master = await getSystemTestEnvironment();
 
-    const { melonContracts } = masterEnvironment.deployment;
+    const log = getLog(master);
 
-    const environment = withNewAccount(masterEnvironment);
+    const { melonContracts } = master.deployment;
 
-    const amguToken = await getAmguToken(
-      masterEnvironment,
-      melonContracts.version,
-    );
+    const manager = withNewAccount(master);
+    const trader = withNewAccount(master);
+
+    const amguToken = await getAmguToken(master, melonContracts.version);
     const amguPrice = createQuantity(amguToken, '1000000000');
-    await setAmguPrice(masterEnvironment, melonContracts.engine, amguPrice);
+    await setAmguPrice(master, melonContracts.engine, amguPrice);
 
-    const weth = getTokenBySymbol(environment, 'WETH');
-    const mln = getTokenBySymbol(environment, 'MLN');
+    const weth = getTokenBySymbol(manager, 'WETH');
+    const mln = getTokenBySymbol(manager, 'MLN');
 
-    await update(masterEnvironment, melonContracts.priceSource, [
+    const matchingMarket =
+      master.deployment.exchangeConfigs[Exchanges.MatchingMarket].exchange;
+
+    await update(master, melonContracts.priceSource, [
       createPrice(createQuantity(weth, 1), createQuantity(weth, 1)),
       createPrice(createQuantity(mln, 1), createQuantity(weth, 2)),
     ]);
 
     const mlnPrice = await getPrice(
-      masterEnvironment,
+      master,
       melonContracts.priceSource.toString(),
       amguToken,
     );
 
-    console.log('MLN Price', toFixedPrice(mlnPrice));
+    log.debug('MLN Price', mlnPrice);
 
-    const masterBalance = await getBalance(masterEnvironment);
+    const masterBalance = await getBalance(master);
 
     expect(masterBalance).toBeTrueWith(
       greaterThan,
       createQuantity(masterBalance.token, 6),
     );
 
-    await sendEth(masterEnvironment, {
-      howMuch: createQuantity('ETH', 5),
-      to: environment.wallet.address,
+    await sendEth(master, {
+      howMuch: createQuantity('ETH', 2),
+      to: manager.wallet.address,
     });
 
-    const balance = await getBalance(environment);
+    await sendEth(master, {
+      howMuch: createQuantity('ETH', 1),
+      to: trader.wallet.address,
+    });
 
-    expect(balance).toBeTrueWith(isEqual, createQuantity('ETH', 5));
+    await transfer(master, {
+      howMuch: createQuantity(mln, 2),
+      to: trader.wallet.address,
+    });
 
-    const quantity = createQuantity(weth, 2);
+    const quantity = createQuantity(weth, 1);
 
-    await deposit(environment, quantity.token.address, undefined, {
+    await deposit(manager, quantity.token.address, undefined, {
       value: quantity.quantity.toString(),
     });
 
-    const settings = await setupInvestedTestFund(environment);
+    const routes = await setupInvestedTestFund(manager);
+
+    const order = await makeOrderFromAccountOasisDex(trader, matchingMarket, {
+      buy: createQuantity(weth, 0.1),
+      sell: createQuantity(mln, 2),
+    });
+
+    await takeOasisDexOrder(manager, routes.tradingAddress, {
+      id: order.id,
+      maker: order.maker,
+      makerQuantity: order.sell,
+      takerQuantity: order.buy,
+    });
+
+    const calculations = await performCalculations(
+      manager,
+      routes.accountingAddress,
+    );
+
+    log.debug({ calculations });
+
+    expect(calculations.gav).toBeTrueWith(isEqual, createQuantity(weth, 4.9));
 
     const a = 1;
     expect(a).toBe(1);
