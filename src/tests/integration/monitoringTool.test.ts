2b960aee4ed559d4a036750481c91f14b0a1143b
Ivan Herger <ivan.herger@gmail.com>
2019-01-08 10:38:24 +0100

    Update monitoringTool.test.ts
    Start with events

--- src/tests/integration/monitoringTool.test.ts
+++ src/tests/integration/monitoringTool.test.ts
@@ -34,6 +34,8 @@ import { getHub } from '~/contracts/fund/hub/calls/getHub';
 import { approve } from '~/contracts/dependencies/token/transactions/approve';
 import { requestInvestment } from '~/contracts/fund/participation/transactions/requestInvestment';
 import { executeRequest } from '~/contracts/fund/participation/transactions/executeRequest';
+import { getContract } from '~/utils/solidity/getContract';
+import { Contracts } from '~/Contracts';
 
 describe('monitoringTool', () => {
   const shared: any = {};
@@ -69,7 +71,9 @@ describe('monitoringTool', () => {
     // const defaultTokens = [quoteToken, baseToken];
     const amguToken = await getAmguToken(shared.env, version);
     const amguPrice = createQuantity(amguToken, '1000000000');
+    const newAmguPrice = createQuantity(amguToken, '5000000000');
     await setAmguPrice(shared.env, engine, amguPrice);
+    await setAmguPrice(shared.env, engine, newAmguPrice);
     const myAmguPrice = await getAmguPrice(shared.env, engine);
 
     console.log(
@@ -78,38 +82,46 @@ describe('monitoringTool', () => {
       ' ',
       myAmguPrice.token.symbol,
     );
-  });
 
-  test('Read exchange rates', async () => {
-    // exchange rates
-    let rates = {
-      ETHMLN: 0,
-      MLNUSD: 0,
-      ETHUSD: 0,
-    };
-
-    let axinst = axios.create({
-      baseURL: 'https://rest.coinapi.io',
-      headers: { 'X-CoinAPI-Key': '6F388926-927B-4582-AE90-B1C8CD3D5B60' },
+    // read Engine Contract events
+    const contract = getContract(shared.env, Contracts.Engine, engine);
+    let events = await contract.getPastEvents('allEvents', {
+      fromBlock: 0,
+      toBlock: 'latest',
     });
-
-    const getRate = async (a, b) => {
-      try {
-        const response = await axinst.get('/v1/exchangerate/' + a + '/' + b);
-        return response.data;
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    for (let [key] of Object.entries(rates)) {
-      let a = key.substr(0, 3);
-      let b = key.substr(3, 3);
-
-      rates[key] = await getRate(a, b);
-      // console.log(key + ': ', rates[key]);
-    }
+    console.log('Engine Events: ', events);
   });
+
+  // test('Read exchange rates', async () => {
+  //   // exchange rates
+  //   let rates = {
+  //     ETHMLN: 0,
+  //     MLNUSD: 0,
+  //     ETHUSD: 0,
+  //   };
+
+  //   let axinst = axios.create({
+  //     baseURL: 'https://rest.coinapi.io',
+  //     headers: { 'X-CoinAPI-Key': '6F388926-927B-4582-AE90-B1C8CD3D5B60' },
+  //   });
+
+  //   const getRate = async (a, b) => {
+  //     try {
+  //       const response = await axinst.get('/v1/exchangerate/' + a + '/' + b);
+  //       return response.data;
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   for (let [key] of Object.entries(rates)) {
+  //     let a = key.substr(0, 3);
+  //     let b = key.substr(3, 3);
+
+  //     rates[key] = await getRate(a, b);
+  //     // console.log(key + ': ', rates[key]);
+  //   }
+  // });
 
   test('Reading fund list', async () => {
     const {
@@ -124,7 +136,7 @@ describe('monitoringTool', () => {
 
     // setup fund
     const fundAddress = await setupInvestedTestFund(shared.env);
-    console.log('Fund: ', fundAddress);
+    // console.log('Fund: ', fundAddress);
 
     const investmentAmount = createQuantity(weth, 1);
 
@@ -143,44 +155,47 @@ describe('monitoringTool', () => {
       shared.env,
       fundAddress.accountingAddress,
     );
-    console.log('Fund holdings: ', fundHoldings, '\n\n\n');
+    // console.log('Fund holdings: ', fundHoldings, '\n\n\n');
 
     const hub = await getHub(shared.env, fundAddress.accountingAddress);
-    console.log('Hub: ', hub);
+    // console.log('Hub: ', hub);
 
-    // second fund
+    // setup a number of additional funds (max 9)
 
-    const env2 = await withDifferentAccount(shared.env, shared.accounts[2]);
+    const n = 3;
+    for (let i = 1; i <= n; i++) {
+      const envi = await withDifferentAccount(shared.env, shared.accounts[i]);
 
-    const balance = await getBalance(shared.env);
-    console.log('Balance: ', balance);
+      const balance = await getBalance(shared.env);
+      //console.log('Balance: ', balance);
 
-    await sendEth(shared.env, {
-      howMuch: createQuantity(weth, 5),
-      to: env2.wallet.address,
-    });
+      await sendEth(shared.env, {
+        howMuch: createQuantity(weth, 5),
+        to: envi.wallet.address,
+      });
 
-    const b2 = await getBalance(env2);
-    console.log('Balance 2: ', b2);
+      const bi = await getBalance(envi);
+      // console.log('Balance: ', bi);
 
-    const fund2 = await setupInvestedTestFund(env2);
-    console.log('2nd Fund: ', fund2);
+      const fundi = await setupInvestedTestFund(envi);
+      // console.log('Fund: ', fundi);
 
-    const investmentAmount2 = createQuantity(weth, 1);
+      const investmentAmount2 = createQuantity(weth, 1);
 
-    await approve(shared.env, {
-      howMuch: investmentAmount2,
-      spender: fund2.participationAddress,
-    });
+      await approve(shared.env, {
+        howMuch: investmentAmount2,
+        spender: fundi.participationAddress,
+      });
 
-    await requestInvestment(shared.env, fund2.participationAddress, {
-      investmentAmount,
-    });
+      await requestInvestment(shared.env, fundi.participationAddress, {
+        investmentAmount,
+      });
 
-    await executeRequest(shared.env, fund2.participationAddress);
+      await executeRequest(shared.env, fundi.participationAddress);
 
-    const hub2 = await getHub(shared.env, fund2.accountingAddress);
-    console.log('Hub: ', hub2);
+      const hubi = await getHub(shared.env, fundi.accountingAddress);
+      // console.log('Hub: ', hubi);
+    }
 
     // get fund list
     const fundList = await getFundDetails(shared.env, ranking, version);
@@ -189,25 +204,36 @@ describe('monitoringTool', () => {
     // loop through funds to get interesting quantities
     for (let f of fundList) {
       let shutDown = await isShutDown(shared.env, f.address);
-      console.log('Is Shut Down: ', shutDown);
+      // console.log('Is Shut Down: ', shutDown);
 
       let routes = await getRoutes(shared.env, f.address);
-      console.log('Route: ', routes);
-      console.log('Accounting address: ', routes.accountingAddress);
+      // console.log('Route: ', routes);
+      // console.log('Accounting address: ', routes.accountingAddress);
 
       let holdings = await getFundHoldings(
         shared.env,
         routes.accountingAddress,
       );
-      console.log('Holdings: ', holdings);
+      // console.log('Holdings: ', holdings);
 
       let calcs = await performCalculations(
         shared.env,
         routes.accountingAddress,
       );
-      console.log('Calculations: ', calcs);
+      // console.log('Calculations: ', calcs);
     }
 
     // console.log('Share price: ', fundList[0].sharePrice.base, fundList[0].sharePrice.quote)
+
+    const fundListContract = getContract(
+      shared.env,
+      Contracts.Version,
+      version,
+    );
+    let fundListEvents = await fundListContract.getPastEvents('allEvents', {
+      fromBlock: 0,
+      toBlock: 'latest',
+    });
+    console.log('Fund List Events', fundListEvents);
   });
 });
