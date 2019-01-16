fe160312bfa5cedd892337ad440d6b95a90abe2f
luongnt95 <luongnt.58@gmail.com>
2019-01-16 14:35:18 +0700

    Add takeOrderOnKyber

---
+++ src/contracts/fund/trading/calls/isKyberTakePermitted.ts
@@ -0,0 +1,69 @@
+import web3EthAbi from 'web3-eth-abi';
+import { getContract } from '~/utils/solidity/getContract';
+import { Contracts, Exchanges } from '~/Contracts';
+import { getRoutes } from '~/contracts/fund/hub/calls/getRoutes';
+import { getHub } from '~/contracts/fund/hub/calls/getHub';
+import { FunctionSignatures } from '../utils/FunctionSignatures';
+import { Environment } from '~/utils/environment/Environment';
+import { Address, QuantityInterface } from '@melonproject/token-math';
+
+const isKyberTakePermitted = async (
+  environment: Environment,
+  tradingContractAddress: Address,
+  makerQuantity: QuantityInterface,
+  takerQuantity: QuantityInterface,
+  fillTakerQuantity: QuantityInterface,
+) => {
+  const hubAddress = await getHub(environment, tradingContractAddress);
+  const { policyManagerAddress, tradingAddress } = await getRoutes(
+    environment,
+    hubAddress,
+  );
+
+  // TODO: Jenna said we need this later!
+  //
+  // const priceFeedContract = await getContract(
+  //   Contracts.TestingPriceFeed,
+  //   priceSourceAddress,
+  //   environment,
+  // );
+
+  // const orderPrice = await priceFeedContract.methods.getOrderPriceInfo(
+  //   makerQuantity.token.address,
+  //   takerQuantity.token.address,
+  //   makerQuantity.quantity,
+  //   takerQuantity.quantity,
+  // ).call();
+
+  const policyManager = await getContract(
+    environment,
+    Contracts.PolicyManager,
+    policyManagerAddress,
+  );
+
+  const exchangeAddress =
+    environment.deployment.exchangeConfigs[Exchanges.KyberNetwork].exchange;
+
+  const result = await policyManager.methods
+    .preValidate(
+      web3EthAbi.encodeFunctionSignature(FunctionSignatures.takeOrder),
+      [
+        tradingAddress.toString(), // orderAddresses[0],
+        '0x0000000000000000000000000000000000000000', // orderAddresses[1],
+        makerQuantity.token.address.toString(), // orderAddresses[2],
+        takerQuantity.token.address.toString(), // orderAddresses[3],
+        exchangeAddress.toString(), // exchanges[exchangeIndex].exchange
+      ],
+      [
+        makerQuantity.quantity.toString(), // orderValues[0],
+        takerQuantity.quantity.toString(), // orderValues[1],
+        fillTakerQuantity.quantity.toString(), // orderValues[6]
+      ],
+      '0x0', // identifier
+    )
+    .call();
+
+  return !!result;
+};
+
+export { isKyberTakePermitted };
