a0132c4b62be4de36297cf4eb11923ade5427c9b
GauthamN <onegautham@gmail.com>
2019-01-17 16:39:18 +0100

    Update outdated comments

--- src/contracts/fund/policies/Policy.sol
+++ src/contracts/fund/policies/Policy.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.4.21;
 contract Policy {
     enum Applied { pre, post }
 
-    // addresses: Order maker, Order taker, Order maker asset and Order taker asset
-    // values: Maker token quantity and Taker token quantity
+    // In Trading context:
+    // addresses: Order maker, Order taker, Order maker asset, Order taker asset, Exchange address
+    // values: Maker token quantity, Taker token quantity, Fill Taker Quantity
+
+    // In Participation context:
+    // address[0]: Investor address, address[3]: Investment asset
     function rule(bytes4 sig, address[5] addresses, uint[3] values, bytes32 identifier) external view returns (bool);
 
     function position() external view returns (Applied);
