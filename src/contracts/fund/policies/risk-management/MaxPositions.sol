0a73323318e55ca2c0e671bd815fe2acaf01922b
GauthamN <onegautham@gmail.com>
2019-01-28 19:42:02 +0100

    Fix inconsistency in policies

--- src/contracts/fund/policies/risk-management/MaxPositions.sol
+++ src/contracts/fund/policies/risk-management/MaxPositions.sol
@@ -9,8 +9,8 @@ import "TradingSignatures.sol";
 contract MaxPositions is TradingSignatures, Policy {
     uint public maxPositions;
 
-    /// @dev _maxPositions = 10 means max 10 different non-quote asset tokens
-    /// @dev _maxPositions = 0 means no non-quote asset tokens are investable
+    /// @dev _maxPositions = 10 means max 10 different asset tokens
+    /// @dev _maxPositions = 0 means no asset tokens are investable
     constructor(uint _maxPositions) { maxPositions = _maxPositions; }
 
     function rule(bytes4 sig, address[5] addresses, uint[3] values, bytes32 identifier)
@@ -18,12 +18,11 @@ contract MaxPositions is TradingSignatures, Policy {
         view
         returns (bool)
     {
-        address pricefeed = Hub(Trading(msg.sender).hub()).priceSource();
-        address quoteAsset = PriceSourceInterface(pricefeed).getQuoteAsset();
+        Accounting accounting = Accounting(Hub(Trading(msg.sender).hub()).accounting());
+        address denominationAsset = accounting.DENOMINATION_ASSET();
         // Always allow a trade INTO the quote asset
         address incomingToken = (sig == TAKE_ORDER) ? addresses[2] : addresses[3];
-        if (quoteAsset == incomingToken) { return true; }
-        Accounting accounting = Accounting(Hub(Trading(msg.sender).hub()).accounting());
+        if (denominationAsset == incomingToken) { return true; }
         return accounting.getOwnedAssetsLength() <= maxPositions;
     }
 
