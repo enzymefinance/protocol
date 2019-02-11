22eaf56a0ae29af31917c52c5951b36c22d70476
Matthias Gemperli <hello@matthiasgemperli.ch>
2019-02-11 10:08:52 +0100

    Fix ranking
    - Fix https://github.com/melonproject/melon-lab/issues/281

--- src/contracts/factory/calls/getFundDetails.ts
+++ src/contracts/factory/calls/getFundDetails.ts
@@ -1,5 +1,11 @@
 import * as web3Utils from 'web3-utils';
-import { Address, createPrice, createQuantity } from '@melonproject/token-math';
+import {
+  Address,
+  createPrice,
+  createQuantity,
+  greaterThan,
+  isEqual,
+} from '@melonproject/token-math';
 
 import { getContract } from '~/utils/solidity/getContract';
 import { Contracts } from '~/Contracts';
@@ -29,28 +35,43 @@ export const getFundDetails = async (
     4: denominationAsset,
   } = fundDetails;
 
-  const result = addresses.map((address, index) => {
-    const denominationToken = getTokenByAddress(
-      environment,
-      denominationAsset[index],
-    );
+  const result = addresses
+    .map((address, index) => {
+      const denominationToken = getTokenByAddress(
+        environment,
+        denominationAsset[index],
+      );
 
-    const fundToken = {
-      decimals: 18,
-      symbol: 'MLNF',
-    };
+      const fundToken = {
+        decimals: 18,
+        symbol: 'MLNF',
+      };
 
-    return {
-      address,
-      creationTime: new Date(creationTimes[index] * 1000),
-      name: web3Utils.toUtf8(names[index]),
-      rank: index + 1,
-      sharePrice: createPrice(
-        createQuantity(fundToken, 1),
-        createQuantity(denominationToken, sharePrices[index]),
-      ),
-    };
-  });
+      return {
+        address,
+        creationTime: new Date(creationTimes[index] * 1000),
+        name: web3Utils.toUtf8(names[index]),
+        sharePrice: createPrice(
+          createQuantity(fundToken, 1),
+          createQuantity(denominationToken, sharePrices[index]),
+        ),
+      };
+    })
+    .sort((a, b) => {
+      if (isEqual(a.sharePrice, b.sharePrice)) {
+        return 0;
+      } else if (greaterThan(a.sharePrice.quote, b.sharePrice.quote)) {
+        return -1;
+      } else {
+        return 1;
+      }
+    })
+    .map((fund, index) => {
+      return {
+        ...fund,
+        rank: index + 1,
+      };
+    });
 
   return result;
 };
