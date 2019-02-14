c7b126bcce7e5b81d019c70d87b262fdc9535e49
Simon Emanuel Schmid <simon@schmid.io>
2019-02-14 11:00:57 +0100

    Make triggerRewardAllFees work

--- src/contracts/fund/fees/transactions/triggerRewardAllFees.ts
+++ src/contracts/fund/fees/transactions/triggerRewardAllFees.ts
@@ -1,35 +1,18 @@
+import * as R from 'ramda';
 import { transactionFactory } from '~/utils/solidity/transactionFactory';
-import { createQuantity } from '@melonproject/token-math';
 import { Contracts } from '~/Contracts';
-import { getToken } from '~/contracts/dependencies/token/calls/getToken';
-// tslint:disable-next-line:max-line-length
-// import { ensureIsNotShutDown } from '~/contracts/fund/hub/guards/ensureIsNotShutDown';
-import { getHub } from '~/contracts/fund/hub/calls/getHub';
-import { getRoutes } from '~/contracts/fund/hub/calls/getRoutes';
+import { ensure } from '~/utils/guards/ensure';
 
-const guard = async (environment, params, contractAddress) => {
-  // const hub = await getHub(environment, contractAddress);
-  // TODO: check if any other pre flights necessary
-  // await ensureIsNotShutDown(environment, hub);
-};
-
-const postProcess = async (environment, receipt, params, contractAddress) => {
-  const hub = await getHub(environment, contractAddress);
-  const routes = await getRoutes(environment, hub);
-  const fundToken = await getToken(environment, routes.sharesAddress);
-  return {
-    receipt,
-    shareQuantity: createQuantity(
-      fundToken,
-      receipt.events.FeeRewarded.returnValues.shareQuantity,
-    ),
-  };
+const postProcess = async (_, receipt) => {
+  const feeReward = R.path(['events', 'FeeReward'], receipt);
+  ensure(!!feeReward, 'No FeeReward event found in receipt');
+  return true;
 };
 
 const triggerRewardAllFees = transactionFactory(
   'triggerRewardAllFees',
   Contracts.Accounting,
-  guard,
+  undefined,
   undefined,
   postProcess,
 );
