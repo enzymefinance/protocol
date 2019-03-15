4020ef4d47f6c17694a764eb43829ea416366ed1
Jenna Zenk <zenkjenna@gmail.com>
2019-03-15 18:53:14 +0100

    Rename

--- src/contracts/prices/calls/getLastUpdate.ts
+++ src/contracts/prices/calls/getLastUpdate.ts
@@ -1,13 +1,12 @@
-import { Address, TokenInterface } from '@melonproject/token-math';
+import { Address } from '@melonproject/token-math';
 import { Environment } from '~/utils/environment/Environment';
 import { getContract } from '~/utils/solidity/getContract';
 import { Contracts } from '~/Contracts';
-import { getToken } from '~/contracts/dependencies/token/calls/getToken';
 
 export const getLastUpdate = async (
   environment: Environment,
   contractAddress: Address,
-): Promise<TokenInterface> => {
+) => {
   const contract = await getContract(
     environment,
     Contracts.PriceSourceInterface,
