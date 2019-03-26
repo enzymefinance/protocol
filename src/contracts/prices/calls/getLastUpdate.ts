d60e9dcb37ff4edf2ba4deb0089ad091f900f25e
Sebastian Siemssen <fubhy@fubhy.com>
2019-03-26 13:59:23 +0100

    Removing unused import.

--- src/contracts/prices/calls/getLastUpdate.ts
+++ src/contracts/prices/calls/getLastUpdate.ts
@@ -2,7 +2,6 @@ import { Address, TokenInterface } from '@melonproject/token-math';
 import { Environment } from '~/utils/environment/Environment';
 import { getContract } from '~/utils/solidity/getContract';
 import { Contracts } from '~/Contracts';
-import { getToken } from '~/contracts/dependencies/token/calls/getToken';
 
 export const getLastUpdate = async (
   environment: Environment,
