93b19945f3f20c4c3798c4a4c974e7711a2edf2c
Travis Jacobs <traviswjacobs@gmail.com>
2019-11-04 22:16:24 +0900

    Record with proper contract names

--- new/deploy_melon.sh
+++ new/deploy_melon.sh
@@ -16,10 +16,12 @@ jot() {
 }
 
 # get address from input, or create it
+# TODO: add "explicit" mode, where artifact key and name are passed separately
 nab() {
-  addr=$(jq -r $1 "$D_IN")
+  set -e
+  addr=$(jq -r ".$1 // empty" "$D_IN")
   if [[ -z $addr ]]; then
-    addr=$(dapp create "${@:2}")
+    addr=$(dapp create "${@}")
   fi
   jot $1 $addr
   echo $addr
@@ -27,7 +29,6 @@ nab() {
 
 set -x
 
-# export SETH_CHAIN="kovan"
 export ETH_FROM="0xbe1ac5962e318d0335b8d8aabff55dc4bad01826"
 export ETH_PASSWORD="./passfile" # TODO: come up with a solution for this
 export ETH_KEYSTORE="./allkeys" # TODO: come up with a solution for this
@@ -46,59 +47,54 @@ DEFAULT_KYBERFEED_MAX_SPREAD=$(seth --to-uint256 $(seth --to-wei 0.1 eth))
 DEFAULT_PRICEFEED_TOKEN=$WETH
 DEFAULT_VERSION_OWNER=$ETH_FROM # should be in config or something
 
-# TODO: for most of these, get the contract if we don't need to deploy
-ethfinexAdapter=$(nab '.melonContracts.adapters.ethfinexAdapter' EthfinexAdapter)
-kyberAdapter=$(nab '.melonContracts.adapters.kyberAdapter' KyberAdapter)
-matchingMarketAdapter=$(nab '.melonContracts.adapters.matchingMarketAdapter' MatchingMarketAdapter)
-matchingMarketAccessor=$(nab '.melonContracts.adapters.matchingMarketAccessor' MatchingMarketAccessor)
-zeroExV2Adapter=$(nab '.melonContracts.adapters.zeroExAdapter' ZeroExV2Adapter)
-engineAdapter=$(nab '.melonContracts.adapters.engineAdapter' EngineAdapter)
+EthfinexAdapter=$(nab EthfinexAdapter)
+KyberAdapter=$(nab KyberAdapter)
+MatchingMarketAdapter=$(nab MatchingMarketAdapter)
+MatchingMarketAccessor=$(nab MatchingMarketAccessor)
+ZeroExV2Adapter=$(nab ZeroExV2Adapter)
+EngineAdapter=$(nab EngineAdapter)
 
-priceTolerance=$(nab '.melonContracts.policies.priceTolerance' PriceTolerance $DEFAULT_PRICE_TOL)
-userWhitelist=$(nab '.melonContracts.policies.userWhitelist' UserWhitelist $DEFAULT_USER_WHITELIST)
+PriceTolerance=$(nab PriceTolerance $DEFAULT_PRICE_TOL)
+UserWhitelist=$(nab UserWhitelist $DEFAULT_USER_WHITELIST)
 
-managementFee=$(nab '.melonContracts.fees.managementFee' ManagementFee)
-performanceFee=$(nab '.melonContracts.fees.performanceFee' PerformanceFee)
+ManagementFee=$(nab ManagementFee)
+PerformanceFee=$(nab PerformanceFee)
 
-accountingFactory=$(nab '.melonContracts.factories.accountingFactory' AccountingFactory)
-feeManagerFactory=$(nab '.melonContracts.factories.feeManagerFactory' FeeManagerFactory)
-participationFactory=$(nab '.melonContracts.factories.participationFactory' ParticipationFactory)
-policyManagerFactory=$(nab '.melonContracts.factories.policyManagerFactory' PolicyManagerFactory)
-sharesFactory=$(nab '.melonContracts.factories.sharesFactory' SharesFactory)
-tradingFactory=$(nab '.melonContracts.factories.tradingFactory' TradingFactory)
-vaultFactory=$(nab '.melonContracts.factories.vaultFactory' VaultFactory)
+AccountingFactory=$(nab AccountingFactory)
+FeeManagerFactory=$(nab FeeManagerFactory)
+ParticipationFactory=$(nab ParticipationFactory)
+PolicyManagerFactory=$(nab PolicyManagerFactory)
+SharesFactory=$(nab SharesFactory)
+TradingFactory=$(nab TradingFactory)
+VaultFactory=$(nab VaultFactory)
 
-registry=$(nab '.melonContracts.registry' Registry $DEFAULT_REGISTRY_OWNER)
-engine=$(nab '.melonContracts.engine' Engine $DEFAULT_ENGINE_DELAY $registry)
+Registry=$(nab Registry $DEFAULT_REGISTRY_OWNER)
+Engine=$(nab Engine $DEFAULT_ENGINE_DELAY $Registry)
 
-fundRanking=$(nab '.melonContracts.ranking' FundRanking)
+FundRanking=$(nab FundRanking)
 
 if [[ "$TRACK" == "KYBER_PRICE" ]]; then # TODO: consider less branching
-  priceSource=$(nab '.melonContracts.priceSource' KyberPriceFeed \
-    $registry \
-    $DEFAULT_KYBER_NET_PROXY \
-    $DEFAULT_KYBERFEED_MAX_SPREAD \
-    $DEFAULT_PRICEFEED_TOKEN)
+  priceSource=$(nab KyberPriceFeed $Registry $DEFAULT_KYBER_NET_PROXY \
+    $DEFAULT_KYBERFEED_MAX_SPREAD $DEFAULT_PRICEFEED_TOKEN)
 elif [[ "$TRACK" == "TESTING" ]]; then
-  priceSource=$(nab '.melonContracts.priceSource' TestingPriceFeed $DEFAULT_PRICEFEED_TOKEN)
+  priceSource=$(nab TestingPriceFeed $DEFAULT_PRICEFEED_TOKEN)
 fi
 
 # TODO: check whether these are already set correctly first
-seth send $registry 'setPriceSource(address)' $priceSource
-seth send $registry 'setNativeAsset(address)' $WETH
-seth send $registry 'setMlnToken(address)' $MLN
-seth send $registry 'setEngine(address)' $engine
-seth send $registry 'setMGM(address)' $ETH_FROM # TODO: fill in correct address here
-seth send $registry 'setEthfinexWrapperRegistry(address)' $ETH_FROM # TODO: make correct
+seth send $Registry 'setPriceSource(address)' $priceSource
+seth send $Registry 'setNativeAsset(address)' $WETH
+seth send $Registry 'setMlnToken(address)' $MLN
+seth send $Registry 'setEngine(address)' $Engine
+seth send $Registry 'setMGM(address)' $ETH_FROM # TODO: fill in correct address here
+seth send $Registry 'setEthfinexWrapperRegistry(address)' $ETH_FROM # TODO: make correct
 
 # TODO: check if fees registered
-seth send $registry 'registerFees(address[])' "[${managementFee#0x},${performanceFee#0x}]"
+seth send $Registry 'registerFees(address[])' "[${ManagementFee#0x},${PerformanceFee#0x}]"
 
 # TODO: move above? does it matter if we set this up before the sends just prior?
-version=$(nab '.melonContracts.version' Version \
-  $accountingFactory $feeManagerFactory $participationFactory $sharesFactory \
-  $tradingFactory $vaultFactory $policyManagerFactory $registry \
-  $DEFAULT_VERSION_OWNER)
+version=$(nab Version $AccountingFactory $FeeManagerFactory \
+  $ParticipationFactory $SharesFactory $TradingFactory $VaultFactory \
+  $PolicyManagerFactory $Registry $DEFAULT_VERSION_OWNER)
 
 makeSig=$(seth sig 'makeOrder(address,address[6],uint256[8],bytes32,bytes,bytes,bytes)')
 takeSig=$(seth sig 'takeOrder(address,address[6],uint256[8],bytes32,bytes,bytes,bytes)')
@@ -106,33 +102,33 @@ cancelSig=$(seth sig 'cancelOrder(address,address[6],uint256[8],bytes32,bytes,by
 withdrawSig=$(seth sig 'withdrawTokens(address,address[6],uint256[8],bytes32,bytes,bytes,bytes)')
 
 # TODO: more sophisticated checking (do we really need to update?)
-exchanges=$(jq -r '.exchangeConfigs | keys[]' $D_IN)
+exchanges=$(jq -r '.exchangeConfigs | keys_unsorted[]' $D_IN)
 for name in $exchanges; do
-  adapter=$(jq -r --arg n "$name" '.exchangeConfigs[$n].adapter' $D_IN)
-  exchange=$(jq -r --arg n "$name" '.exchangeConfigs[$n].exchange' $D_IN)
-  takesCustody=$(jq -r --arg n "$name" '.exchangeConfigs[$n].takesCustody' $D_IN)
+  adapter=$(jq -r ".exchangeConfigs.${name}.adapter" $D_IN)
+  exchange=$(jq -r ".exchangeConfigs.${name}.exchange" $D_IN)
+  takesCustody=$(jq -r ".exchangeConfigs.${name}.takesCustody" $D_IN)
   sigs="[${makeSig#0x},${takeSig#0x},${cancelSig#0x},${withdrawSig#0x}]"
-  registered=$(seth call $registry 'exchangeAdapterIsRegistered(address)(bool)' $adapter)
+  registered=$(seth call $Registry 'exchangeAdapterIsRegistered(address)(bool)' $adapter)
   if [[ "$registered" == true ]]; then
-    seth send $registry 'updateExchangeAdapter(address,address,bool,bytes4[])' \
+    seth send $Registry 'updateExchangeAdapter(address,address,bool,bytes4[])' \
       $exchange $adapter $takesCustody $sigs
   else
-    seth send $registry 'registerExchangeAdapter(address,address,bool,bytes4[])' \
+    seth send $Registry 'registerExchangeAdapter(address,address,bool,bytes4[])' \
       $exchange $adapter $takesCustody $sigs
   fi
 done
 
-syms=$(jq -r '.thirdPartyContracts.tokens | keys[]' $D_IN)
+syms=$(jq -r '.tokens | keys_unsorted[]' $D_IN)
 for sym in $syms; do
-  addr=$(jq -r --arg s "$sym" '.thirdPartyContracts.tokens[$s].address' $D_IN)
-  name=$(jq -r --arg s "$sym" '.thirdPartyContracts.tokens[$s].name // empty' $D_IN)
-  url=$(jq -r --arg s "$sym" '.thirdPartyContracts.tokens[$s].url // empty' $D_IN)
-  reserveMin=$(jq -r --arg s "$sym" '.thirdPartyContracts.tokens[$s].reserveMin // 0' $D_IN)
+  addr=$(jq -r ".tokens.${sym}.address" $D_IN)
+  name=$(jq -r ".tokens.${sym}.name // empty" $D_IN)
+  url=$(jq -r ".tokens.${sym}.url // empty" $D_IN)
+  reserveMin=$(jq -r ".tokens.${sym}.reserveMin // 0" $D_IN)
   standards="[]"
   sigs="[]"
-  registered=$(seth call $registry 'assetIsRegistered(address)(bool)' $addr)
+  registered=$(seth call $Registry 'assetIsRegistered(address)(bool)' $addr)
   if [[ "$registered" == false ]]; then
-    seth send $registry 'registerAsset(address,string,string,string,uint,uint[],bytes4[])' \
+    seth send $Registry 'registerAsset(address,string,string,string,uint,uint[],bytes4[])' \
       "$addr" "$name" "$sym" "$url" "$reserveMin" "$standards" "$sigs"
   fi
   if [[ "$TRACK" == "TESTING" ]]; then
@@ -148,3 +144,26 @@ elif [[ "$TRACK" == "TESTING" ]]; then
   echo nothing
 fi
 
+cat > "./addrs.json" <<EOF
+{
+  "EthfinexAdapter": "$EthfinexAdapter",
+  "KyberAdapter": "$KyberAdapter",
+  "MatchingMarketAdapter": "$MatchingMarketAdapter",
+  "MatchingMarketAccessor": "$MatchingMarketAccessor",
+  "ZeroExV2Adapter": "$ZeroExV2Adapter",
+  "EngineAdapter": "$EngineAdapter",
+  "PriceTolerance": "$PriceTolerance",
+  "UserWhitelist": "$UserWhitelist",
+  "ManagementFee": "$PerformanceFee",
+  "AccountingFactory": "$AccountingFactory",
+  "FeeManagerFactory": "$FeeManagerFactory",
+  "ParticipationFactory": "$ParticipationFactory",
+  "PolicyManagerFactory": "$PolicyManagerFactory",
+  "SharesFactory": "$SharesFactory",
+  "TradingFactory": "$TradingFactory",
+  "VaultFactory": "$VaultFactory",
+  "Registry": "$Registry",
+  "Engine": "$Engine",
+  "FundRanking": "$FundRanking"
+}
+EOF
