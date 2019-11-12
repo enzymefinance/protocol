5ef7c51f38541c59146813d553362fb49f12a435
Sean Casey <sean@seanjcasey.com>
2019-11-12 12:39:29 +1100

    refactor: finish userWhitelist.test.ts and convert to js

---
+++ src/tests/integration/high-level/userWhitelist.test.js
@@ -0,0 +1,95 @@
+import { BigNumber } from 'bignumber.js';
+import web3EthAbi from 'web3-eth-abi';
+
+import { Contracts } from '~/Contracts';
+import { deployUserWhitelist } from '~/contracts/fund/policies/compliance/transactions/deployUserWhitelist';
+import { FunctionSignatures } from '~/contracts/fund/trading/utils/FunctionSignatures';
+import { setupInvestedTestFund } from '~/tests/utils/setupInvestedTestFund';
+import { Environment, Tracks } from '~/utils/environment/Environment';
+import { getTokenBySymbol } from '~/utils/environment/getTokenBySymbol';
+import { deployContract } from '~/utils/solidity/deployContract';
+import { getContract } from '~/utils/solidity/getContract';
+import { deployAndInitTestEnv } from '../../utils/deployAndInitTestEnv';
+
+describe('Happy Path', () => {
+  const s = {};
+
+  beforeAll(async () => {
+    s.env = await deployAndInitTestEnv();
+    expect(s.env.track).toBe(Tracks.TESTING);
+
+    // Define user accounts
+    s.accounts = await s.env.eth.getAccounts();
+    s.user = s.env.wallet.address;
+    s.userAlt = s.accounts[1];
+    s.standardGas = 8000000;
+
+    // Setup necessary contracts
+    s.routes = await setupInvestedTestFund(s.env);
+    const policyManager = getContract(
+      s.env,
+      Contracts.PolicyManager,
+      s.routes.policyManagerAddress.toString()
+    );
+    const userWhitelist = await deployContract(
+      s.env,
+      Contracts.UserWhitelist,
+      [[s.user],]
+    );
+    const functionSig = web3EthAbi.encodeFunctionSignature(FunctionSignatures.requestInvestment);
+    await policyManager.methods
+      .register(
+        functionSig,
+        userWhitelist.toString()
+      )
+      .send({ from: s.user, gas: s.standardGas });
+
+    // Define shared contracts
+    s.weth = getTokenBySymbol(s.env, 'WETH');
+    s.wethInterface = getContract(
+      s.env,
+      Contracts.Weth,
+      s.weth.address,
+    );
+    s.participation = getContract(
+      s.env,
+      Contracts.Participation,
+      s.routes.participationAddress.toString(),
+    );
+
+    // Define shared params
+    s.investmentAmount = new BigNumber('1e+18').toString();
+    s.requestedShares = new BigNumber('1e+18').toString();
+    s.amguAmount = new BigNumber('1e+18').toString();
+    s.investmentAsset = s.weth.address;
+    s.participationAddress = s.routes.participationAddress.toString();
+  });
+
+  test('Request investment fails if user is not whitelisted', async () => {
+    await s.wethInterface.methods
+      .transfer(s.userAlt, s.investmentAmount)
+      .send({ from: s.user, gas: s.standardGas });
+
+    await s.wethInterface.methods
+      .approve(s.participationAddress, s.investmentAmount)
+      .send({ from: s.userAlt, gas: s.standardGas });
+
+    await expect(
+      s.participation.methods
+        .requestInvestment(s.requestedShares, s.investmentAmount, s.investmentAsset)
+        .send({ from: s.userAlt, value: s.amguAmount, gas: s.standardGas }),
+    ).rejects.toThrow();
+  });
+
+  test('Request investment passes if user is whitelisted', async () => {
+    await s.wethInterface.methods
+      .approve(s.participationAddress, s.investmentAmount)
+      .send({ from: s.user, gas: s.standardGas });
+
+    await expect(
+      s.participation.methods
+        .requestInvestment(s.requestedShares, s.investmentAmount, s.investmentAsset)
+        .send({ from: s.user, value: s.amguAmount, gas: s.standardGas }),
+    ).resolves.not.toThrow();
+  });
+});
