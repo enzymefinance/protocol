14d1e156ef3f6f3b062dbbbe47cc28e72911352d
Travis Jacobs <traviswjacobs@gmail.com>
2019-11-20 16:51:07 -0330

    Unlock all accounts (hacky)

--- new/deploy/get-web3.js
+++ new/deploy/get-web3.js
@@ -7,9 +7,15 @@ const web3 = new Web3(
   null,
   { transactionConfirmationBlocks: 1 }
 );
-const keystore = fs.readFileSync(process.env.KEYSTORE, 'utf8');
+const keystore = fs.readFileSync(process.env.KEYSTORE, 'utf8'); // TODO: move away from env var
 const password = fs.readFileSync(process.env.PASSFILE, 'utf8').trim();
 const unlocked = web3.eth.accounts.decrypt(keystore, password);
 web3.eth.accounts.wallet.add(unlocked.privateKey);
+
+// get private keys stored in a file, like that produced by ganache --acctKeys
+if (process.env.PRIVATE_KEYS) { // TODO: move away from env var
+  const keys = JSON.parse(fs.readFileSync(process.env.PRIVATE_KEYS));
+  for (const v of Object.values(keys)) { web3.eth.accounts.wallet.add(v); }
+}
 
 module.exports = web3;
