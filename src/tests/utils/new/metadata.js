974e3c529c449d90316fedb13af1025e2bdd790c
Sean Casey <sean@seanjcasey.com>
2019-11-22 10:33:44 +1100

    refactor: add util to get encoded event from tx receipt

--- src/tests/utils/new/metadata.js
+++ src/tests/utils/new/metadata.js
@@ -1,5 +1,25 @@
+import web3EthAbi from 'web3-eth-abi';
+
 export const getABI = contractName =>
   require(`~/../out/${contractName}.abi.json`);
+
+export const getEventFromReceipt = (receiptEvents, contractName, eventName) => {
+  const abi = getABI(contractName);
+  const eventAbi = abi.find(e => e.type === 'event' && e.name === eventName);
+
+  for (const receiptEvent of Object.values(receiptEvents)) {
+    const rawData = receiptEvent.raw;
+    if (rawData.topics[0] === web3EthAbi.encodeEventSignature(eventAbi)) {
+      return web3EthAbi.decodeLog(
+        eventAbi.inputs,
+        rawData.data,
+        rawData.topics.slice(1)
+      );
+    }
+  }
+
+  return null;
+}
 
 // this will fail in the case where there is an overload on the abi
 export const getFunctionSignature = (contractName, functionName) => {
