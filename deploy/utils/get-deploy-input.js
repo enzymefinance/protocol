add49440a05cbfe1e1d66c19b60bbc798930b49a
Travis Jacobs <traviswjacobs@gmail.com>
2019-11-28 13:32:30 -0330

    Add temporary deploy:new script

--- deploy/utils/get-deploy-input.js
+++ deploy/utils/get-deploy-input.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-const deployIn = process.env.CONF; // || './deploy_in.json';
+const deployIn = process.env.DEPLOY_CONF;
 
 module.exports = JSON.parse(fs.readFileSync(deployIn, 'utf8'));
--- deploy/utils/get-web3.js
+++ deploy/utils/get-web3.js
@@ -7,15 +7,13 @@ const web3 = new Web3(
   null,
   { transactionConfirmationBlocks: 1 }
 );
-const keystore = fs.readFileSync(process.env.KEYSTORE, 'utf8'); // TODO: move away from env var
-const password = fs.readFileSync(process.env.PASSFILE, 'utf8').trim();
-const unlocked = web3.eth.accounts.decrypt(keystore, password);
-web3.eth.accounts.wallet.add(unlocked.privateKey);
+
+// TODO: get keys from a non-ganache setup
 
 // get private keys stored in a file, like that produced by ganache --acctKeys
-if (process.env.PRIVATE_KEYS) { // TODO: move away from env var
-  const keys = JSON.parse(fs.readFileSync(process.env.PRIVATE_KEYS));
-  for (const v of Object.values(keys)) { web3.eth.accounts.wallet.add(v); }
+if (process.env.GANACHE_KEYS) { // TODO: move away from env var
+  const keys = JSON.parse(fs.readFileSync(process.env.GANACHE_KEYS));
+  for (const v of Object.values(keys.private_keys)) { web3.eth.accounts.wallet.add(v); }
 }
 
 module.exports = web3;
