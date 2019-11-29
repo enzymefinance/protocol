8c8d3090ba756e400e68ab20d56727f1fed8de7a
Travis Jacobs <traviswjacobs@gmail.com>
2019-11-29 16:46:14 -0330

    Bring fundCompliance test into new deployment system

--- src/tests/integration/fundCompliance.test.js
+++ src/tests/integration/fundCompliance.test.js
@@ -1,102 +1,58 @@
 import { encodeFunctionSignature } from 'web3-eth-abi';
 import { BN, toWei } from 'web3-utils';
-
-import { deployAndInitTestEnv } from '~/tests/utils/deployAndInitTestEnv';
-import { setupInvestedTestFund } from '~/tests/utils/setupInvestedTestFund';
-import { deployContract } from '~/utils/solidity/deployContract';
-import { getContract } from '~/utils/solidity/getContract';
-
 import { CONTRACT_NAMES } from '~/tests/utils/new/constants';
 import { getFunctionSignature } from '~/tests/utils/new/metadata';
+const web3 = require('../../../deploy/utils/get-web3');
+import { deploy } from '../../../deploy/utils/deploy-contract';
+const deploySystem = require('../../../deploy/scripts/deploy-system');
+const setupInvestedTestFund = require('../utils/new/setupInvestedTestFund');
+const {increaseTime} = require('../utils/new/rpc');
 
-let environment;
 let deployer, manager, investor, badInvestor;
 let defaultTxOpts, managerTxOpts, investorTxOpts, badInvestorTxOpts;
-let addresses, contracts;
 let requestInvestmentFunctionSig;
+let mln, weth, priceSource, userWhitelist;
+let fund;
+let contracts;
 
 beforeAll(async () => {
-  environment = await deployAndInitTestEnv();
-  [deployer, manager, investor, badInvestor] = await environment.eth.getAccounts();
+  const accounts = await web3.eth.getAccounts();
+  [deployer, manager, investor, badInvestor] = accounts;
   defaultTxOpts = { from: deployer, gas: 8000000 };
   managerTxOpts = { ...defaultTxOpts, from: manager };
   investorTxOpts = { ...defaultTxOpts, from: investor };
   badInvestorTxOpts = { ...defaultTxOpts, from: badInvestor };
 
-  addresses = environment.deployment;
+  const deployment = await deploySystem(JSON.parse(require('fs').readFileSync(process.env.CONF))); // TODO: change from reading file each time
+  contracts = deployment.contracts;
 
-  const mln = getContract(
-    environment,
-    CONTRACT_NAMES.BURNABLE_TOKEN,
-    addresses.thirdPartyContracts.tokens.find(
-      token => token.symbol === 'MLN'
-    ).address
-  );
-  const priceSource = getContract(
-    environment,
-    CONTRACT_NAMES.TESTING_PRICEFEED,
-    addresses.melonContracts.priceSource
-  );
-  const weth = getContract(
-    environment,
-    CONTRACT_NAMES.WETH,
-    addresses.thirdPartyContracts.tokens.find(
-      token => token.symbol === 'WETH'
-    ).address
-  );
-  contracts = { mln, priceSource, weth };
+  mln = contracts.MLN;
+  weth = contracts.WETH;
+  priceSource = contracts.TestingPriceFeed;
+
+  userWhitelist = await deploy(CONTRACT_NAMES.USER_WHITELIST, [[investor]]);
 
   requestInvestmentFunctionSig = getFunctionSignature(
     CONTRACT_NAMES.PARTICIPATION,
     'requestInvestment',
   );
 
+  await weth.methods.transfer(manager, toWei('10', 'ether')).send(defaultTxOpts);
   await weth.methods.transfer(investor, toWei('10', 'ether')).send(defaultTxOpts);
 });
 
 describe('Fund 1: user whitelist', () => {
-  let fundAddresses, fundContracts, policyContracts;
   let amguAmount, offeredValue, wantedShares;
 
   beforeAll(async () => {
-    const { weth } = contracts;
+    fund = await setupInvestedTestFund(contracts, manager);
 
-    fundAddresses = await setupInvestedTestFund(environment);
-
-    const participation = getContract(
-      environment,
-      CONTRACT_NAMES.PARTICIPATION,
-      fundAddresses.participationAddress
-    );
-    const policyManager = getContract(
-      environment,
-      CONTRACT_NAMES.POLICY_MANAGER,
-      fundAddresses.policyManagerAddress
-    );
-    const shares = getContract(
-      environment,
-      CONTRACT_NAMES.SHARES,
-      fundAddresses.sharesAddress
-    );
-    fundContracts = { participation, policyManager, shares };
-
-    const userWhitelist = getContract(
-      environment,
-      CONTRACT_NAMES.USER_WHITELIST,
-      await deployContract(
-        environment,
-        CONTRACT_NAMES.USER_WHITELIST,
-        [[investor]]
-      )
-    );
-    policyContracts = { userWhitelist };
-
-    await policyManager.methods
+    await fund.policyManager.methods
       .register(
         encodeFunctionSignature(requestInvestmentFunctionSig),
         userWhitelist.options.address,
       )
-      .send(defaultTxOpts);
+      .send(managerTxOpts);
 
     amguAmount = toWei('.01', 'ether');
     offeredValue = toWei('1', 'ether');
@@ -104,8 +60,7 @@ describe('Fund 1: user whitelist', () => {
   });
 
   test('Confirm policies have been set', async () => {
-    const { policyManager } = fundContracts;
-    const { userWhitelist } = policyContracts;
+    const { policyManager } = fund;
 
     const requestInvestmentPoliciesRes = await policyManager.methods
       .getPoliciesBySig(encodeFunctionSignature(requestInvestmentFunctionSig))
@@ -121,8 +76,7 @@ describe('Fund 1: user whitelist', () => {
   });
 
   test('Bad request investment: user not on whitelist', async () => {
-    const { weth } = contracts;
-    const { participation } = fundContracts;
+    const { participation } = fund;
 
     await weth.methods
       .approve(participation.options.address, offeredValue)
@@ -136,8 +90,7 @@ describe('Fund 1: user whitelist', () => {
   });
 
   test('Good request investment: user is whitelisted', async () => {
-    const { mln, priceSource, weth } = contracts;
-    const { participation, shares } = fundContracts;
+    const { participation, shares } = fund;
 
     await weth.methods
       .approve(participation.options.address, offeredValue)
@@ -147,21 +100,12 @@ describe('Fund 1: user whitelist', () => {
       .send({ ...investorTxOpts, value: amguAmount });
 
     // Need price update before participation executed
-    environment.eth.currentProvider.send(
-      {
-        id: 121,
-        jsonrpc: '2.0',
-        method: 'evm_increaseTime',
-        params: [1800], // 30 mins
-      },
-      (err, res) => {},
-    );
+    await increaseTime(1800);
     await priceSource.methods
       .update(
         [weth.options.address, mln.options.address],
         [toWei('1', 'ether'), toWei('0.5', 'ether')],
-      )
-      .send(defaultTxOpts);
+      ).send(defaultTxOpts);
 
     await participation.methods
       .executeRequestFor(investor)
