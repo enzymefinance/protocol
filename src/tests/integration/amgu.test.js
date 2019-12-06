d30ab3945441f043761cea497dffe4703a27e64e
luongnt95 <luongnt.58@gmail.com>
2019-12-07 02:27:15 +0700

    Add test amgu with incentive attached to amgu.test

--- src/tests/integration/amgu.test.js
+++ src/tests/integration/amgu.test.js
@@ -8,16 +8,17 @@ import { stringToBytes } from '~/tests/utils/formatting';
 import getFundComponents from '~/tests/utils/getFundComponents';
 
 describe('amgu', () => {
-  let user, defaultTxOpts;
+  let user, defaultTxOpts, defaultTxOptsWithValue;
   let baseToken, quoteToken;
   let engine, version, priceSource, registry;
   let amguPrice, oldAmguPrice;
   let fundName;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const accounts = await web3.eth.getAccounts();
     user = accounts[0];
     defaultTxOpts = { from: user, gas: 8000000 };
+    defaultTxOptsWithValue = { ...defaultTxOpts, value: toWei('1', 'ether') };
 
     const deployed = await partialRedeploy([
       CONTRACT_NAMES.VERSION,
@@ -95,6 +96,91 @@ describe('amgu', () => {
     const realUserCost = new BN(preUserBalance).sub(new BN(postUserBalance));
 
     expect(txCostInWei.lt(realUserCost)).toBe(true);
+    expect(estimatedTotalUserCost.gt(realUserCost)).toBe(true);
+  });
+
+  it('set amgu with incentive attatched and check its usage', async () => {
+    await engine.methods
+      .setAmguPrice(amguPrice)
+      .send(defaultTxOpts)
+    const newAmguPrice = await engine.methods.getAmguPrice().call();
+
+    expect(newAmguPrice.toString()).toBe(amguPrice.toString());
+
+    const newPrice = toWei('2', 'ether');
+    await priceSource.methods
+      .update([baseToken.options.address], [newPrice])
+      .send(defaultTxOpts)
+
+    const price = await priceSource.methods
+      .getPrices([baseToken.options.address])
+      .call();
+
+    expect(price[0][0].toString()).toBe(newPrice.toString());
+
+    await version.methods
+      .beginSetup(
+        stringToBytes(fundName, 32),
+        [],
+        [],
+        [],
+        [],
+        [],
+        quoteToken.options.address,
+        [baseToken.options.address, quoteToken.options.address]
+      ).send(defaultTxOpts);
+    await version.methods.createAccounting().send(defaultTxOptsWithValue);
+    await version.methods.createFeeManager().send(defaultTxOptsWithValue);
+    await version.methods.createParticipation().send(defaultTxOptsWithValue);
+    await version.methods.createPolicyManager().send(defaultTxOptsWithValue);
+    await version.methods.createShares().send(defaultTxOptsWithValue);
+    await version.methods.createTrading().send(defaultTxOptsWithValue);
+    await version.methods.createVault().send(defaultTxOptsWithValue);
+    const res = await version.methods.completeSetup().send(defaultTxOptsWithValue);
+    const hubAddress = res.events.NewFund.returnValues.hub;
+    const fund = await getFundComponents(hubAddress);
+
+    const requestedShares = toWei('100', 'ether');
+    const investmentAmount = toWei('100', 'ether');
+
+    await quoteToken.methods
+      .approve(fund.participation.options.address, investmentAmount)
+      .send(defaultTxOpts);
+
+    await registry.methods.setIncentive(toWei('100', 'ether')).send(defaultTxOpts);
+    const incentiveAmount = await registry.methods.incentive().call();
+    expect(incentiveAmount).toBe(toWei('100', 'ether'));
+
+    const amguTx = fund.participation.methods
+      .requestInvestment(
+        requestedShares,
+        investmentAmount,
+        quoteToken.options.address,
+      )
+
+    const preUserBalance = await web3.eth.getBalance(user);
+    const result = await amguTx.send({ ...defaultTxOpts, value: toWei('101', 'ether') });
+    const postUserBalance = await web3.eth.getBalance(user);
+
+    const gasPrice = await web3.eth.getGasPrice();
+    const gasUsed = result.gasUsed;
+    const estimatedGasUsedWithoutAmgu = result.gasUsed;
+
+    const nativeAssetAddress = await registry.methods.nativeAsset().call();
+    const mlnAddress = await version.methods.mlnToken().call();
+
+    const mlnAmount = new BN(amguPrice).mul(new BN(estimatedGasUsedWithoutAmgu));
+    const ethToPay = await priceSource.methods.convertQuantity(
+      mlnAmount.toString(),
+      mlnAddress,
+      nativeAssetAddress,
+    ).call();
+
+    const txCostInWei = new BN(gasPrice).mul(new BN(gasUsed));
+    const estimatedTotalUserCost = new BN(ethToPay).add(new BN(txCostInWei)).add(new BN(incentiveAmount));
+    const realUserCost = new BN(preUserBalance).sub(new BN(postUserBalance));
+
+    expect(txCostInWei.add(new BN(incentiveAmount)).lt(realUserCost)).toBe(true);
     expect(estimatedTotalUserCost.gt(realUserCost)).toBe(true);
   });
 });
