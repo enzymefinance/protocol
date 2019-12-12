17cabcc205e3b9116361c62e4f5b2abf0b3e75e2
luongnt95 <luongnt.58@gmail.com>
2019-12-13 00:55:42 +0700

    Remove exchange third-party contracts

--- src/contracts/exchanges/third-party/kyber/SanityRatesInterface.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.13;
-
-
-import "./ERC20Interface.sol";
-
-interface SanityRatesInterface {
-    function getSanityRate(ERC20KyberClone src, ERC20KyberClone dest) public view returns(uint);
-}
--- src/contracts/exchanges/third-party/kyber/WhiteListInterface.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.5.13;
-
-
-contract WhiteListInterface {
-    function getUserCapInWei(address user) external view returns (uint userCapWei);
-}
