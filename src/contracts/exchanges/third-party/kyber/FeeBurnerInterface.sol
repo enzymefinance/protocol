ed65eaf8d84ef5c37d97064de55c91c48966ce30
luongnt95 <luongnt.58@gmail.com>
2019-12-13 00:55:42 +0700

    Remove exchange third-party contracts

--- src/contracts/exchanges/third-party/kyber/FeeBurnerInterface.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.5.13;
-
-
-interface FeeBurnerInterface {
-    function handleFees (uint tradeWeiAmount, address reserve, address wallet) public returns(bool);
-    function setReserveData(address reserve, uint feesInBps, address kncWallet) public;
-}
