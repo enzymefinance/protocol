16d5e2ed2dd7114705af9ebc602784c5847d1653
luongnt95 <luongnt.58@gmail.com>
2019-12-17 18:36:36 +0700

    refactor: remove unused PriceUpdated event

--- src/contracts/prices/UpdatableFeed.i.sol
+++ src/contracts/prices/UpdatableFeed.i.sol
@@ -2,7 +2,6 @@ pragma solidity ^0.5.13;
 
 /// @notice Updates values stored internally
 interface IUpdatableFeed {
-    event PriceUpdated(bytes32 hash);
     function update(address[] calldata _assets, uint[] calldata _prices) external;
     function getLastUpdateId() external view returns (uint);
 }
