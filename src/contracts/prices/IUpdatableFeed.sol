b335e6f05a496c4d01a9661a660e2c7ee9e805ac
Travis Jacobs <traviswjacobs@gmail.com>
2019-12-18 10:38:40 -0330

    refactor: remove unused IUpdatableFeed

--- src/contracts/prices/IUpdatableFeed.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.5.15;
-
-/// @notice Updates values stored internally
-interface IUpdatableFeed {
-    function update(address[] calldata _assets, uint[] calldata _prices) external;
-    function getLastUpdateId() external view returns (uint);
-}
