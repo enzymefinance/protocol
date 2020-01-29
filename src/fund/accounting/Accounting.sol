06d7ec95490effb1a225d41a24da20869b371f76
Sean Casey <sean@seanjcasey.com>
2020-01-29 17:34:07 +1100

    refactor: clean up Accounting contract formatting, storage var packing, unused imports

--- src/fund/accounting/Accounting.sol
+++ src/fund/accounting/Accounting.sol
@@ -1,13 +1,11 @@
 pragma solidity 0.6.1;
 
-import "../../dependencies/token/StandardToken.sol";
 import "../../factory/Factory.sol";
 import "../../prices/IPriceSource.sol";
 import "../fees/FeeManager.sol";
 import "../hub/Spoke.sol";
 import "../shares/Shares.sol";
 import "../trading/ITrading.sol";
-import "../vault/Vault.sol";
 import "../../engine/AmguConsumer.sol";
 
 contract Accounting is AmguConsumer, Spoke {
@@ -16,22 +14,23 @@ contract Accounting is AmguConsumer, Spoke {
     event AssetRemoval(address indexed asset);
 
     struct Calculations {
-        uint gav;
-        uint nav;
-        uint allocatedFees;
-        uint totalSupply;
-        uint timestamp;
+        uint256 gav;
+        uint256 nav;
+        uint256 allocatedFees;
+        uint256 totalSupply;
+        uint256 timestamp;
     }
 
-    uint constant public MAX_OWNED_ASSETS = 20;
-    address[] public ownedAssets;
-    mapping (address => bool) public isInAssetList;
-    uint public constant SHARES_DECIMALS = 18;
-    address public NATIVE_ASSET;
+    uint8 constant public MAX_OWNED_ASSETS = 20;
+    uint8 constant public SHARES_DECIMALS = 18;
+    uint8 public DENOMINATION_ASSET_DECIMALS;
+    uint256 public DEFAULT_SHARE_PRICE;
     address public DENOMINATION_ASSET;
-    uint public DENOMINATION_ASSET_DECIMALS;
-    uint public DEFAULT_SHARE_PRICE;
+    address public NATIVE_ASSET;
+    address[] public ownedAssets;
     Calculations public atLastAllocation;
+
+    mapping (address => bool) public isInAssetList;
 
     constructor(address _hub, address _denominationAsset, address _nativeAsset)
         public
@@ -40,7 +39,7 @@ contract Accounting is AmguConsumer, Spoke {
         DENOMINATION_ASSET = _denominationAsset;
         NATIVE_ASSET = _nativeAsset;
         DENOMINATION_ASSET_DECIMALS = ERC20WithFields(DENOMINATION_ASSET).decimals();
-        DEFAULT_SHARE_PRICE = 10 ** uint(DENOMINATION_ASSET_DECIMALS);
+        DEFAULT_SHARE_PRICE = 10 ** uint256(DENOMINATION_ASSET_DECIMALS);
     }
 
     function getOwnedAssetsLength() external view returns (uint256) {
@@ -59,102 +58,112 @@ contract Accounting is AmguConsumer, Spoke {
     }
 
     /// @dev Returns sparse array
-    function getFundHoldings() external returns (uint[] memory, address[] memory) {
-        uint[] memory _quantities = new uint[](ownedAssets.length);
+    function getFundHoldings() external returns (uint256[] memory, address[] memory) {
+        uint256[] memory _quantities = new uint256[](ownedAssets.length);
         address[] memory _assets = new address[](ownedAssets.length);
-        for (uint i = 0; i < ownedAssets.length; i++) {
+        for (uint256 i = 0; i < ownedAssets.length; i++) {
             address ofAsset = ownedAssets[i];
             // assetHoldings formatting: mul(exchangeHoldings, 10 ** assetDecimal)
-            uint quantityHeld = assetHoldings(ofAsset);
+            uint256 quantityHeld = assetHoldings(ofAsset);
             _assets[i] = ofAsset;
             _quantities[i] = quantityHeld;
         }
         return (_quantities, _assets);
     }
 
-    function calcAssetGAV(address _queryAsset) external returns (uint) {
-        uint queryAssetQuantityHeld = assetHoldings(_queryAsset);
+    function calcAssetGAV(address _queryAsset) external returns (uint256) {
+        uint256 queryAssetQuantityHeld = assetHoldings(_queryAsset);
         return IPriceSource(priceSource()).convertQuantity(
             queryAssetQuantityHeld, _queryAsset, DENOMINATION_ASSET
         );
     }
 
     // prices are quoted in DENOMINATION_ASSET so they use denominationDecimals
-    function calcGav() public returns (uint gav) {
-        for (uint i = 0; i < ownedAssets.length; ++i) {
+    function calcGav() public returns (uint256) {
+        uint256 gav;
+        for (uint256 i = 0; i < ownedAssets.length; ++i) {
             address asset = ownedAssets[i];
             // assetHoldings formatting: mul(exchangeHoldings, 10 ** assetDecimals)
-            uint quantityHeld = assetHoldings(asset);
+            uint256 quantityHeld = assetHoldings(asset);
             // Dont bother with the calculations if the balance of the asset is 0
             if (quantityHeld == 0) {
                 continue;
             }
-            // gav as sum of mul(assetHoldings, assetPrice) with formatting: mul(mul(exchangeHoldings, exchangePrice), 10 ** shareDecimals)
+            // gav as sum of mul(assetHoldings, assetPrice) with formatting:
+            // mul(mul(exchangeHoldings, exchangePrice), 10 ** shareDecimals)
             gav = add(
                 gav,
                 IPriceSource(priceSource()).convertQuantity(
-                    quantityHeld, asset, DENOMINATION_ASSET
+                    quantityHeld,
+                    asset,
+                    DENOMINATION_ASSET
                 )
             );
         }
         return gav;
     }
 
-    function calcNav(uint gav, uint unclaimedFeesInDenominationAsset) public pure returns (uint) {
-        return sub(gav, unclaimedFeesInDenominationAsset);
+    function calcNav(uint256 _gav, uint256 _unclaimedFeesInDenominationAsset)
+        public
+        pure
+        returns (uint256)
+    {
+        return sub(_gav, _unclaimedFeesInDenominationAsset);
     }
 
-    function valuePerShare(uint totalValue, uint numShares) public pure returns (uint) {
-        require(numShares > 0, "No shares to calculate value for");
-        return (totalValue * 10 ** uint(SHARES_DECIMALS)) / numShares;
+    function valuePerShare(uint256 _totalValue, uint256 _numShares) public pure returns (uint256) {
+        require(_numShares > 0, "No shares to calculate value for");
+        return (_totalValue * 10 ** uint256(SHARES_DECIMALS)) / _numShares;
     }
 
     function performCalculations()
         public
         returns (
-            uint gav,
-            uint feesInDenominationAsset,  // unclaimed amount
-            uint feesInShares,             // unclaimed amount
-            uint nav,
-            uint sharePrice,
-            uint gavPerShareNetManagementFee
+            uint256 gav_,
+            uint256 feesInDenominationAsset_,  // unclaimed amount
+            uint256 feesInShares_,             // unclaimed amount
+            uint256 nav_,
+            uint256 sharePrice_,
+            uint256 gavPerShareNetManagementFee_
         )
     {
-        gav = calcGav();
-        uint totalSupply = Shares(routes.shares).totalSupply();
-        feesInShares = FeeManager(routes.feeManager).totalFeeAmount();
-        feesInDenominationAsset = (totalSupply == 0) ?
+        gav_ = calcGav();
+        uint256 totalSupply = Shares(routes.shares).totalSupply();
+        feesInShares_ = FeeManager(routes.feeManager).totalFeeAmount();
+        feesInDenominationAsset_ = (totalSupply == 0) ?
             0 :
-            mul(feesInShares, gav) / add(totalSupply, feesInShares);
-        nav = calcNav(gav, feesInDenominationAsset);
+            mul(feesInShares_, gav_) / add(totalSupply, feesInShares_);
+        nav_ = calcNav(gav_, feesInDenominationAsset_);
 
-        // The total share supply including the value of feesInDenominationAsset, measured in shares of this fund
-        uint totalSupplyAccountingForFees = add(totalSupply, feesInShares);
-        sharePrice = (totalSupply > 0) ?
-            valuePerShare(gav, totalSupplyAccountingForFees) :
+        // The total share supply including the value of feesInDenominationAsset_,
+        // measured in shares of this fund
+        uint256 totalSupplyAccountingForFees = add(totalSupply, feesInShares_);
+        sharePrice_ = (totalSupply > 0) ?
+            valuePerShare(gav_, totalSupplyAccountingForFees) :
             DEFAULT_SHARE_PRICE;
-        gavPerShareNetManagementFee = (totalSupply > 0) ?
-            valuePerShare(gav, add(totalSupply, FeeManager(routes.feeManager).managementFeeAmount())) :
+        gavPerShareNetManagementFee_ = (totalSupply > 0) ?
+            valuePerShare(
+                gav_,
+                add(totalSupply, FeeManager(routes.feeManager).managementFeeAmount())
+            ) :
             DEFAULT_SHARE_PRICE;
-        return (gav, feesInDenominationAsset, feesInShares, nav, sharePrice, gavPerShareNetManagementFee);
     }
 
     function calcGavPerShareNetManagementFee()
         public
-        returns (uint gavPerShareNetManagementFee)
+        returns (uint256 gavPerShareNetManagementFee_)
     {
-        (,,,,,gavPerShareNetManagementFee) = performCalculations();
-        return gavPerShareNetManagementFee;
+        (,,,,,gavPerShareNetManagementFee_) = performCalculations();
     }
 
-    function getShareCostInAsset(uint _numShares, address _altAsset)
+    function getShareCostInAsset(uint256 _numShares, address _altAsset)
         external
-        returns (uint)
+        returns (uint256)
     {
-        uint denominationAssetQuantity = mul(
+        uint256 denominationAssetQuantity = mul(
             _numShares,
             calcGavPerShareNetManagementFee()
-        ) / 10 ** uint(SHARES_DECIMALS);
+        ) / 10 ** uint256(SHARES_DECIMALS);
         return IPriceSource(priceSource()).convertQuantity(
             denominationAssetQuantity, DENOMINATION_ASSET, _altAsset
         );
@@ -186,7 +195,7 @@ contract Accounting is AmguConsumer, Spoke {
 
     /// @dev Check holdings for all assets, and adjust list
     function updateOwnedAssets() public {
-        for (uint i = 0; i < ownedAssets.length; i++) {
+        for (uint256 i = 0; i < ownedAssets.length; i++) {
             address asset = ownedAssets[i];
             if (
                 assetHoldings(asset) == 0 &&
@@ -224,7 +233,7 @@ contract Accounting is AmguConsumer, Spoke {
         if (!isInAssetList[_asset]) { return; }
 
         isInAssetList[_asset] = false;
-        for (uint i; i < ownedAssets.length; i++) {
+        for (uint256 i; i < ownedAssets.length; i++) {
             if (ownedAssets[i] == _asset) {
                 ownedAssets[i] = ownedAssets[ownedAssets.length - 1];
                 ownedAssets.pop();
@@ -234,10 +243,18 @@ contract Accounting is AmguConsumer, Spoke {
         emit AssetRemoval(_asset);
     }
 
-    function engine() public view override(AmguConsumer, Spoke) returns (address) { return Spoke.engine(); }
-    function mlnToken() public view override(AmguConsumer, Spoke) returns (address) { return Spoke.mlnToken(); }
-    function priceSource() public view override(AmguConsumer, Spoke) returns (address) { return Spoke.priceSource(); }
-    function registry() public view override(AmguConsumer, Spoke) returns (address) { return Spoke.registry(); }
+    function engine() public view override(AmguConsumer, Spoke) returns (address) {
+        return Spoke.engine();
+    }
+    function mlnToken() public view override(AmguConsumer, Spoke) returns (address) {
+        return Spoke.mlnToken();
+    }
+    function priceSource() public view override(AmguConsumer, Spoke) returns (address) {
+        return Spoke.priceSource();
+    }
+    function registry() public view override(AmguConsumer, Spoke) returns (address) {
+        return Spoke.registry();
+    }
 }
 
 contract AccountingFactory is Factory {
@@ -248,11 +265,13 @@ contract AccountingFactory is Factory {
         address nativeAsset
     );
 
-    function createInstance(address _hub, address _denominationAsset, address _nativeAsset) external returns (address) {
+    function createInstance(address _hub, address _denominationAsset, address _nativeAsset)
+        external
+        returns (address)
+    {
         address accounting = address(new Accounting(_hub, _denominationAsset, _nativeAsset));
         childExists[accounting] = true;
         emit NewInstance(_hub, accounting, _denominationAsset, _nativeAsset);
         return accounting;
     }
 }
-
