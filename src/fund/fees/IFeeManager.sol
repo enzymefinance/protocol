08da7caefb98fa39df74e779f625b9e147b9b643
Sean Casey <sean@seanjcasey.com>
2020-02-05 22:51:12 +1100

    refactor: update (and add) interfaces used in fund contracts to include all externally available functions

--- src/fund/fees/IFeeManager.sol
+++ src/fund/fees/IFeeManager.sol
@@ -1,4 +1,36 @@
 pragma solidity 0.6.1;
+pragma experimental ABIEncoderV2;
+
+import "./IFee.sol";
+import "../hub/IHub.sol";
+
+interface IFeeManager {
+    // STORAGE
+    function feeIsRegistered(address) external view returns (bool);
+    function fees(uint256 _index) external view returns (IFee);
+
+    // FUNCTIONS
+    function managementFeeAmount() external returns (uint256);
+    function performanceFeeAmount() external returns (uint256);
+    function rewardManagementFee() external;
+    function totalFeeAmount() external returns (uint256);
+
+    // Caller: Auth only
+    function rewardAllFees() external;
+
+    // INHERITED: ISpoke
+    // STORAGE
+    function hub() external view returns (IHub);
+    function initialized() external view returns (bool);
+    function routes() external view returns (IHub.Routes memory);
+
+    // FUNCTIONS
+    function engine() external view returns (address);
+    function mlnToken() external view returns (address);
+    function priceSource() external view returns (address);
+    function version() external view returns (address);
+    function registry() external view returns (address);
+}
 
 interface IFeeManagerFactory {
     function createInstance(
