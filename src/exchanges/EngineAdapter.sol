bc4ad28cb979fac1745054f0c97aaa5482775de4
Sean Casey <sean@seanjcasey.com>
2020-02-14 09:10:13 -0800

    refactor: update exchange adapters to use interfaces instead of full imports, and add canonical functions for getting native asset and mln token addresses

--- src/exchanges/EngineAdapter.sol
+++ src/exchanges/EngineAdapter.sol
@@ -3,46 +3,37 @@ pragma experimental ABIEncoderV2;
 
 import "./ExchangeAdapter.sol";
 import "./OrderFiller.sol";
-import "../engine/Engine.sol";
-import "../dependencies/DSMath.sol";
 import "../dependencies/WETH.sol";
-import "../dependencies/TokenUser.sol";
+import "../engine/IEngine.sol";
 
 /// @notice Trading adapter to Melon Engine
-contract EngineAdapter is DSMath, TokenUser, ExchangeAdapter, OrderFiller {
-    /// @notice Buys Ether from the engine, selling MLN
-    /// @param targetExchange Address of the engine
-    /// @param orderValues [0] Min Eth to receive from the engine
-    /// @param orderValues [1] MLN quantity
-    /// @param orderValues [6] Same as orderValues[1]
-    /// @param orderAddresses [2] WETH token
-    /// @param orderAddresses [3] MLN token
+contract EngineAdapter is ExchangeAdapter, OrderFiller {
+    /// @notice Buys Ether from the Melon Engine, selling MLN
+    /// @param _targetExchange Address of the engine
+    /// @param _orderValues [0] Expected min ETH quantity (maker quantity)
+    /// @param _orderValues [1] Expected MLN quantity (taker quantity)
+    /// @param _orderValues [6] Same as orderValues[1]
+    /// @param _orderAddresses [2] WETH token (maker asset)
+    /// @param _orderAddresses [3] MLN token (taker asset)
     function takeOrder (
-        address targetExchange,
-        address[8] memory orderAddresses,
-        uint[8] memory orderValues,
-        bytes[4] memory orderData,
-        bytes32 identifier,
-        bytes memory signature
+        address _targetExchange,
+        address[8] memory _orderAddresses,
+        uint[8] memory _orderValues,
+        bytes[4] memory _orderData,
+        bytes32 _identifier,
+        bytes memory _signature
     )
         public
         override
     {
-        require(
-            orderAddresses[2] == Registry(getHub().registry()).nativeAsset(),
-            "maker asset doesnt match nativeAsset on registry"
-        );
-        require(
-            orderValues[1] == orderValues[6],
-            "fillTakerQuantity must equal takerAssetQuantity"
-        );
+        validateTakeOrderParams(_orderAddresses, _orderValues);
 
         (
             address[] memory fillAssets,
             uint256[] memory fillExpectedAmounts
-        ) = formatFillTakeOrderArgs(orderAddresses, orderValues);
+        ) = formatFillTakeOrderArgs(_orderAddresses, _orderValues);
 
-        fillTakeOrder(targetExchange, fillAssets, fillExpectedAmounts);
+        fillTakeOrder(_targetExchange, fillAssets, fillExpectedAmounts);
     }
 
     // INTERNAL FUNCTIONS
@@ -65,15 +56,6 @@ contract EngineAdapter is DSMath, TokenUser, ExchangeAdapter, OrderFiller {
         return (fillAssets, fillExpectedAmounts);
     }
 
-    function executeTakeOrderOnExchange(address _targetExchange, uint256 _expectedTakerAmount)
-        internal
-        returns (uint256 ethFilledAmount_)
-    {
-        uint256 preEthBalance = payable(address(this)).balance;
-        Engine(_targetExchange).sellAndBurnMln(_expectedTakerAmount);
-        ethFilledAmount_ = sub(payable(address(this)).balance, preEthBalance);
-    }
-
     function fillTakeOrder(
         address _targetExchange,
         address[] memory _fillAssets,
@@ -89,10 +71,33 @@ contract EngineAdapter is DSMath, TokenUser, ExchangeAdapter, OrderFiller {
         // Approve taker asset
         approveAsset(_fillAssets[1], _targetExchange, _fillExpectedAmounts[1], "takerAsset");
 
-        // Fill order
-        uint256 ethFilledAmount = executeTakeOrderOnExchange(_targetExchange, _fillExpectedAmounts[1]);
+        // Fill order on Engine
+        uint256 preEthBalance = payable(address(this)).balance;
+        IEngine(_targetExchange).sellAndBurnMln(_fillExpectedAmounts[1]);
+        uint256 ethFilledAmount = sub(payable(address(this)).balance, preEthBalance);
 
         // Return ETH to WETH
         WETH(payable(_fillAssets[0])).deposit.value(ethFilledAmount)();
+    }
+
+    function validateTakeOrderParams(
+        address[8] memory _orderAddresses,
+        uint256[8] memory _orderValues
+    )
+        internal
+        view
+    {
+        require(
+            _orderAddresses[2] == getNativeAssetAddress(),
+            "validateTakeOrderParams: maker asset does not match nativeAsset"
+        );
+        require(
+            _orderAddresses[3] == getMlnTokenAddress(),
+            "validateTakeOrderParams: taker asset does not match mlnToken"
+        );
+        require(
+            _orderValues[1] == _orderValues[6],
+            "validateTakeOrderParams: fill taker quantity must equal taker quantity"
+        );
     }
 }
