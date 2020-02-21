ba83c6a42ec9e9d2db641061236ef81984faa865
luongnt95 <luongnt.58@gmail.com>
2020-02-21 21:53:27 +0700

    refactor: callOnExchange with encoded data param

--- src/exchanges/adapters/EngineAdapter.sol
+++ src/exchanges/adapters/EngineAdapter.sol
@@ -5,11 +5,63 @@ import "../libs/ExchangeAdapter.sol";
 import "../libs/OrderTaker.sol";
 import "../../dependencies/WETH.sol";
 import "../../engine/IEngine.sol";
+import "../../fund/policies/TradingSignatures.sol";
 
 /// @title EngineAdapter Contract
 /// @author Melonport AG <team@melonport.com>
 /// @notice Trading adapter to Melon Engine
-contract EngineAdapter is ExchangeAdapter, OrderTaker {
+contract EngineAdapter is ExchangeAdapter, OrderTaker, TradingSignatures {
+    /// @notice Extract arguments for risk management validations
+    /// @param _methodSelector method selector of TAKE_ORDER, ...
+    /// @param _encodedArgs Encoded arguments for a specific exchange
+    /// @notice rskMngAddrs [0] makerAddress
+    /// @notice rskMngAddrs [1] takerAddress
+    /// @notice rskMngAddrs [2] makerAsset
+    /// @notice rskMngAddrs [3] takerAsset
+    /// @notice rskMngAddrs [4] makerFeeAsset
+    /// @notice rskMngAddrs [5] takerFeeAsset
+    /// @notice rskMngVals [0] makerAssetAmount
+    /// @notice rskMngVals [1] takerAssetAmount
+    /// @notice rskMngVals [2] fillAmout
+    function extractRiskManagementArgsOf(
+        bytes4 _methodSelector,
+        bytes calldata _encodedArgs
+    )
+        external
+        pure
+        override
+        returns (address[6] memory, uint256[3] memory)
+    {
+        address[6] memory rskMngAddrs;
+        uint256[3] memory rskMngVals;
+
+        if (_methodSelector == TAKE_ORDER) {
+            (
+                address[2] memory orderAddresses,
+                uint256[2] memory orderValues
+            ) = __decodeTakeOrderArgs(_encodedArgs);
+
+            rskMngAddrs = [
+                address(0),
+                address(this),
+                orderAddresses[0],
+                orderAddresses[1],
+                address(0),
+                address(0)
+            ];
+            rskMngVals = [
+                orderValues[0],
+                orderValues[1],
+                orderValues[1]
+            ];
+        }
+        else {
+            revert("methodSelector doesn't exist");
+        }
+
+        return (rskMngAddrs, rskMngVals);
+    }
+
     /// @notice Buys Ether from the Melon Engine, selling MLN (takeOrder)
     /// @param _targetExchange Address of the Melon Engine
     /// @param _orderValues [0] Expected min ETH quantity (maker quantity)
@@ -20,11 +72,7 @@ contract EngineAdapter is ExchangeAdapter, OrderTaker {
     /// @param _fillData Encoded data to pass to OrderFiller
     function __fillTakeOrder(
         address _targetExchange,
-        address[8] memory _orderAddresses,
-        uint256[8] memory _orderValues,
-        bytes[4] memory _orderData,
-        bytes32 _identifier,
-        bytes memory _signature,
+        bytes memory _encodedArgs,
         bytes memory _fillData
     )
         internal
@@ -63,24 +111,25 @@ contract EngineAdapter is ExchangeAdapter, OrderTaker {
     /// - [1] Melon Engine (_targetExchange)
     function __formatFillTakeOrderArgs(
         address _targetExchange,
-        address[8] memory _orderAddresses,
-        uint256[8] memory _orderValues,
-        bytes[4] memory _orderData,
-        bytes32 _identifier,
-        bytes memory _signature
+        bytes memory _encodedArgs
     )
         internal
         view
         override
         returns (address[] memory, uint256[] memory, address[] memory)
     {
+        (
+            address[2] memory orderAddresses,
+            uint256[2] memory orderValues
+        ) = __decodeTakeOrderArgs(_encodedArgs);
+
         address[] memory fillAssets = new address[](2);
-        fillAssets[0] = _orderAddresses[2]; // maker asset
-        fillAssets[1] = _orderAddresses[3]; // taker asset
+        fillAssets[0] = orderAddresses[0]; // maker asset
+        fillAssets[1] = orderAddresses[1]; // taker asset
 
         uint256[] memory fillExpectedAmounts = new uint256[](2);
-        fillExpectedAmounts[0] = _orderValues[0]; // maker fill amount
-        fillExpectedAmounts[1] = _orderValues[1]; // taker fill amount
+        fillExpectedAmounts[0] = orderValues[0]; // maker fill amount
+        fillExpectedAmounts[1] = orderValues[1]; // taker fill amount
 
         address[] memory fillApprovalTargets = new address[](2);
         fillApprovalTargets[0] = address(0); // Fund (Use 0x0)
@@ -98,27 +147,43 @@ contract EngineAdapter is ExchangeAdapter, OrderTaker {
     /// @param _orderAddresses [3] MLN token (taker asset)
     function __validateTakeOrderParams(
         address _targetExchange,
-        address[8] memory _orderAddresses,
-        uint256[8] memory _orderValues,
-        bytes[4] memory _orderData,
-        bytes32 _identifier,
-        bytes memory _signature
+        bytes memory _encodedArgs
     )
         internal
         view
         override
     {
+        (
+            address[2] memory orderAddresses,
+            uint256[2] memory orderValues
+        ) = __decodeTakeOrderArgs(_encodedArgs);
+
         require(
-            _orderAddresses[2] == __getNativeAssetAddress(),
+            orderAddresses[0] == __getNativeAssetAddress(),
             "__validateTakeOrderParams: maker asset does not match nativeAsset"
         );
         require(
-            _orderAddresses[3] == __getMlnTokenAddress(),
+            orderAddresses[1] == __getMlnTokenAddress(),
             "__validateTakeOrderParams: taker asset does not match mlnToken"
         );
-        require(
-            _orderValues[1] == _orderValues[6],
-            "__validateTakeOrderParams: fill taker quantity must equal taker quantity"
+    }
+
+    function __decodeTakeOrderArgs(
+        bytes memory _encodedArgs
+    )
+        internal
+        pure
+        returns (
+            address[2] memory orderAddresses,
+            uint256[2] memory orderValues
+        )
+    {
+        return abi.decode(
+            _encodedArgs,
+            (
+                address[2],
+                uint256[2]
+            )
         );
     }
 }
--- src/fund/vault/Trading.sol
+++ src/fund/vault/Trading.sol
@@ -6,6 +6,8 @@ import "../../registry/IRegistry.sol";
 import "../hub/SpokeAccessor.sol";
 import "../policies/IPolicyManager.sol";
 import "../policies/TradingSignatures.sol";
+import "../../version/IRegistry.sol";
+import "../../exchanges/libs/ExchangeAdapter.sol";
 
 contract Trading is DSAuth, SpokeAccessor, TradingSignatures {
     struct Exchange {
@@ -48,87 +50,70 @@ contract Trading is DSAuth, SpokeAccessor, TradingSignatures {
     // PUBLIC FUNCTIONS
 
     /// @notice Universal method for calling exchange functions through adapters
-    /// @notice See adapter contracts for parameters needed for each exchange
+    /// @notice See decoder in adapters to know how to encode needed arguments for each exchange
+    /// @notice rskMngAddrs [0] makerAddress
+    /// @notice rskMngAddrs [1] takerAddress
+    /// @notice rskMngAddrs [2] makerAsset
+    /// @notice rskMngAddrs [3] takerAsset
+    /// @notice rskMngAddrs [4] makerFeeAsset
+    /// @notice rskMngAddrs [5] takerFeeAsset
+    /// @notice rskMngVals [0] makerAssetAmount
+    /// @notice rskMngVals [1] takerAssetAmount
+    /// @notice rskMngVals [2] fillAmout
     /// @param _exchangeIndex Index of the exchange in the "exchanges" array
-    /// @param _orderAddresses [0] Order maker
-    /// @param _orderAddresses [1] Order taker
-    /// @param _orderAddresses [2] maker asset
-    /// @param _orderAddresses [3] taker asset
-    /// @param _orderAddresses [4] fee recipient
-    /// @param _orderAddresses [5] sender address
-    /// @param _orderAddresses [6] maker fee asset
-    /// @param _orderAddresses [7] taker fee asset
-    /// @param _orderValues [0] maker asset amount
-    /// @param _orderValues [1] taker asset amount
-    /// @param _orderValues [2] maker fee
-    /// @param _orderValues [3] taker fee
-    /// @param _orderValues [4] expiration time (seconds)
-    /// @param _orderValues [5] Salt/nonce
-    /// @param _orderValues [6] Fill amount: amount of taker token to be traded
-    /// @param _orderValues [7] Dexy signature mode
-    /// @param _orderData [0] Encoded data specific to maker asset
-    /// @param _orderData [1] Encoded data specific to taker asset
-    /// @param _orderData [2] Encoded data specific to maker asset fee
-    /// @param _orderData [3] Encoded data specific to taker asset fee
     /// @param _identifier Order identifier
-    /// @param _signature Signature of order maker
+    /// @param _encodedArgs Encoded arguments for a specific exchange
     function callOnExchange(
         uint256 _exchangeIndex,
         string memory _methodSignature,
-        address[8] memory _orderAddresses,
-        uint256[8] memory _orderValues,
-        bytes[4] memory _orderData,
         bytes32 _identifier,
-        bytes memory _signature
+        bytes memory _encodedArgs
     )
         public
         spokeInitialized
     {
         bytes4 methodSelector = bytes4(keccak256(bytes(_methodSignature)));
-        __validateCallOnExchange(_exchangeIndex, methodSelector, _orderAddresses);
+        (
+            address[6] memory rskMngAddrs,
+            uint256[3] memory rskMngVals
+        ) = __getRiskManagementArgs(_exchangeIndex, methodSelector, _encodedArgs);
+        address adapter = exchanges[_exchangeIndex].adapter;
+        address targetExchange = exchanges[_exchangeIndex].exchange;
+
+        __validateCallOnExchange(_exchangeIndex, methodSelector, rskMngAddrs);
 
         IPolicyManager(__getRoutes().policyManager).preValidate(
             methodSelector,
             [
-                _orderAddresses[0],
-                _orderAddresses[1],
-                _orderAddresses[2],
-                _orderAddresses[3],
-                exchanges[_exchangeIndex].exchange
+                rskMngAddrs[0],
+                rskMngAddrs[1],
+                rskMngAddrs[2],
+                rskMngAddrs[3],
+                targetExchange
             ],
-            [
-                _orderValues[0],
-                _orderValues[1],
-                _orderValues[6]
-            ],
+            rskMngVals,
             _identifier
         );
-        (bool success, bytes memory returnData) = exchanges[_exchangeIndex].adapter.delegatecall(
+
+        (bool success, bytes memory returnData) = adapter.delegatecall(
             abi.encodeWithSignature(
                 _methodSignature,
-                exchanges[_exchangeIndex].exchange,
-                _orderAddresses,
-                _orderValues,
-                _orderData,
-                _identifier,
-                _signature
+                targetExchange,
+                _encodedArgs
             )
         );
+
         require(success, string(returnData));
         IPolicyManager(__getRoutes().policyManager).postValidate(
             methodSelector,
             [
-                _orderAddresses[0],
-                _orderAddresses[1],
-                _orderAddresses[2],
-                _orderAddresses[3],
-                exchanges[_exchangeIndex].exchange
+                rskMngAddrs[0],
+                rskMngAddrs[1],
+                rskMngAddrs[2],
+                rskMngAddrs[3],
+                targetExchange
             ],
-            [
-                _orderValues[0],
-                _orderValues[1],
-                _orderValues[6]
-            ],
+            rskMngVals,
             _identifier
         );
     }
@@ -150,10 +135,30 @@ contract Trading is DSAuth, SpokeAccessor, TradingSignatures {
         exchanges.push(Exchange(_exchange, _adapter));
     }
 
+    function __getRiskManagementArgs(
+        uint256 _exchangeIndex,
+        bytes4 _methodSelector,
+        bytes memory _encodedArgs
+    )
+        internal
+        returns (address[6] memory, uint256[3] memory)
+    {
+        address adapter = exchanges[_exchangeIndex].adapter;
+        (bool success, bytes memory returnData) = adapter.delegatecall(
+            abi.encodeWithSelector(
+                ExchangeAdapter(adapter).extractRiskManagementArgsOf.selector,
+                _methodSelector,
+                _encodedArgs
+            )
+        );
+        require(success, "Encoded arguments might not match");
+        return abi.decode(returnData, (address[6], uint256[3]));
+    }
+
     function __validateCallOnExchange(
         uint256 _exchangeIndex,
         bytes4 _methodSelector,
-        address[8] memory _orderAddresses
+        address[6] memory _rskMngAddrs
     )
         private
         view
@@ -177,14 +182,14 @@ contract Trading is DSAuth, SpokeAccessor, TradingSignatures {
 
         if (_methodSelector == TAKE_ORDER) {
             require(registry.assetIsRegistered(
-                _orderAddresses[2]), 'Maker asset not registered'
+                _rskMngAddrs[0]), 'Maker asset not registered'
             );
             require(registry.assetIsRegistered(
-                _orderAddresses[3]), 'Taker asset not registered'
+                _rskMngAddrs[1]), 'Taker asset not registered'
             );
-            if (_orderAddresses[7] != address(0)) {
+            if (_rskMngAddrs[5] != address(0)) {
                 require(
-                    registry.assetIsRegistered(_orderAddresses[7]),
+                    registry.assetIsRegistered(_rskMngAddrs[5]),
                     'Taker fee asset not registered'
                 );
             }
