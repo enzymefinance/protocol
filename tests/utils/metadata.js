be09efec8fbaa48b90522534ac9a6e0db1727b53
Sean Casey <sean@seanjcasey.com>
2020-02-21 11:11:43 -0400

    test: add util to getEventCountFromLogs

--- tests/utils/metadata.js
+++ tests/utils/metadata.js
@@ -22,6 +22,20 @@ export const getEventFromReceipt = (receiptEvents, contractName, eventName) => {
   return null;
 }
 
+export const getEventCountFromLogs = (logs, contractName, eventName) => {
+  const abi = getABI(contractName);
+  const eventAbi = abi.find(e => e.type === 'event' && e.name === eventName);
+
+  let counter = 0;
+  for (const log of Object.values(logs)) {
+    if (log.topics[0] === web3EthAbi.encodeEventSignature(eventAbi)) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
+
 export const getEventFromLogs = (logs, contractName, eventName) => {
   const abi = getABI(contractName);
   const eventAbi = abi.find(e => e.type === 'event' && e.name === eventName);
