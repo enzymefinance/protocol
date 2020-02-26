1f3644f237af04aac9a9e9c371ab8efec3a90539
Travis Jacobs <traviswjacobs@gmail.com>
2020-02-25 22:56:13 -0330

    refactor: clean up IPriceSource

--- src/prices/IPriceSource.sol
+++ src/prices/IPriceSource.sol
@@ -1,28 +1,13 @@
 pragma solidity 0.6.1;
 
-/// @notice Must return a value for an asset
 interface IPriceSource {
     function getQuoteAsset() external view returns (address);
-    function getLastUpdate() external view returns (uint);
-
-    /// @notice Returns false if asset not applicable, or price not recent
+    function getLastUpdate() external view returns (uint256);
+    function getReferencePriceInfo(address, address) external view returns (uint256, uint256);
+    function getPrice(address) external view returns (uint256, uint256);
+    function getPrices(address[] calldata) external view returns (uint256[] memory, uint256[] memory);
     function hasValidPrice(address) external view returns (bool);
     function hasValidPrices(address[] calldata) external view returns (bool);
-
-    /// @notice Return the last known price, and when it was issued
-    function getPrice(address _asset) external view returns (uint price, uint timestamp);
-    function getPrices(address[] calldata _assets) external view returns (uint[] memory prices, uint[] memory timestamps);
-
-    /// @notice Get price info, and revert if not valid
-    function getPriceInfo(address _asset) external view returns (uint price, uint decimals);
-    function getInvertedPriceInfo(address ofAsset) external view returns (uint price, uint decimals);
-
-    function getReferencePriceInfo(address _base, address _quote) external view returns (uint referencePrice, uint decimal);
-    function getOrderPriceInfo(address sellAsset, uint sellQuantity, uint buyQuantity) external view returns (uint orderPrice);
-    function existsPriceOnAssetPair(address sellAsset, address buyAsset) external view returns (bool isExistent);
-    function convertQuantity(
-        uint fromAssetQuantity,
-        address fromAsset,
-        address toAsset
-    ) external view returns (uint);
+    function getOrderPriceInfo(address, uint256, uint256) external view returns (uint256);
+    function convertQuantity(uint256, address, address) external view returns (uint256);
 }
