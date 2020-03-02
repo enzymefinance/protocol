e88fa462d2fab560f4cc5d0c199bce060fc49eba
Sean Casey <sean@seanjcasey.com>
2020-03-02 19:26:07 -0400

    refactor: add a SpokeAccessor contract for contracts that access spokes by sharing the same address (inherited or via delegate calls)

---
+++ src/fund/hub/SpokeAccessor.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.6.1;
+
+import "./ISpoke.sol";
+
+contract SpokeAccessor {
+    modifier spokeInitialized() {
+        require(ISpoke(address(this)).initialized(), "Spoke is not initialized");
+        _;
+    }
+
+    function __getHub() internal view returns (IHub) {
+        return ISpoke(address(this)).hub();
+    }
+
+    function __getRoutes() internal view returns (IHub.Routes memory) {
+        return ISpoke(address(this)).routes();
+    }
+}
