0ebb3fcf49d3f87ae00b17e4136eccb425d166c3
Sean Casey <sean@seanjcasey.com>
2020-04-29 07:36:39 -0400

    feat: add ChaiAdapter anad ChaiPriceSoruce oracle

---
+++ contracts/integrations/adapters/ChaiAdapter.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.6.8;
+
+import "../interfaces/IChai.sol";
+import "../utils/AdapterBase.sol";
+
+/// @title ChaiAdapter Contract
+/// @author Melon Council DAO <security@meloncoucil.io>
+/// @notice Adapter for Chai <https://github.com/dapphub/chai>
+contract ChaiAdapter is AdapterBase {
+    address immutable public CHAI;
+    address immutable public DAI;
+
+    constructor(address _registry, address _chai, address _dai) public AdapterBase(_registry) {
+        CHAI = _chai;
+        DAI = _dai;
+    }
+
+    /// @notice Provides a constant string identifier for an adapter
+    /// @return An identifier string
+    function identifier() external pure override returns (string memory) {
+        return "CHAI";
+    }
+
+    /// @notice Parses the expected assets to receive from a call on integration 
+    /// @param _selector The function selector for the callOnIntegration
+    /// @param _encodedCallArgs The encoded parameters for the callOnIntegration
+    /// @return spendAssets_ The assets to spend in the call
+    /// @return spendAssetAmounts_ The max asset amounts to spend in the call
+    /// @return incomingAssets_ The assets to receive in the call
+    /// @return minIncomingAssetAmounts_ The min asset amounts to receive in the call
+    function parseAssetsForMethod(bytes4 _selector, bytes calldata _encodedCallArgs)
+        external
+        view
+        override
+        returns (
+            address[] memory spendAssets_,
+            uint256[] memory spendAssetAmounts_,
+            address[] memory incomingAssets_,
+            uint256[] memory minIncomingAssetAmounts_
+        )
+    {
+        if (_selector == LEND_SELECTOR) {
+            (
+                uint256 daiAmount,
+                uint256 minChaiAmount
+            ) = __decodeCallArgs(_encodedCallArgs);
+
+            spendAssets_ = new address[](1);
+            spendAssets_[0] = DAI;
+            spendAssetAmounts_ = new uint256[](1);
+            spendAssetAmounts_[0] = daiAmount;
+
+            incomingAssets_ = new address[](1);
+            incomingAssets_[0] = CHAI;
+            minIncomingAssetAmounts_ = new uint256[](1);
+            minIncomingAssetAmounts_[0] = minChaiAmount;
+        }
+        else if (_selector == REDEEM_SELECTOR) {
+            (
+                uint256 chaiAmount,
+                uint256 minDaiAmount
+            ) = __decodeCallArgs(_encodedCallArgs);
+
+            spendAssets_ = new address[](1);
+            spendAssets_[0] = CHAI;
+            spendAssetAmounts_ = new uint256[](1);
+            spendAssetAmounts_[0] = chaiAmount;
+
+            incomingAssets_ = new address[](1);
+            incomingAssets_[0] = DAI;
+            minIncomingAssetAmounts_ = new uint256[](1);
+            minIncomingAssetAmounts_[0] = minDaiAmount;
+        }
+        else {
+            revert("parseIncomingAssets: _selector invalid");
+        }
+    }
+
+    /// @notice Lend Dai for Chai
+    /// @param _encodedCallArgs Encoded order parameters
+    /// @param _encodedAssetTransferArgs Encoded args for expected assets to spend and receive
+    function lend(bytes calldata _encodedCallArgs, bytes calldata _encodedAssetTransferArgs)
+        external
+        onlyVault
+        fundAssetsTransferHandler(_encodedAssetTransferArgs)
+    {
+        (uint256 daiAmount,) = __decodeCallArgs(_encodedCallArgs);
+        require(daiAmount > 0, "lend: daiAmount must be >0");
+
+        // Execute Lend on Chai
+        IERC20(DAI).approve(CHAI, daiAmount);
+        IChai(CHAI).join(msg.sender, daiAmount);
+    }
+
+    /// @notice Redeem Chai for Dai
+    /// @param _encodedCallArgs Encoded order parameters
+    /// @param _encodedAssetTransferArgs Encoded args for expected assets to spend and receive
+    function redeem(bytes calldata _encodedCallArgs, bytes calldata _encodedAssetTransferArgs)
+        external
+        onlyVault
+        fundAssetsTransferHandler(_encodedAssetTransferArgs)
+    {
+        (uint256 chaiAmount,) = __decodeCallArgs(_encodedCallArgs);
+        require(chaiAmount > 0, "redeem: chaiAmount must be >0");
+
+        // Execute Lend on Chai
+        // TODO: could use Vault as src and not use fundAssetsTransferHandler to save gas
+        IChai(CHAI).exit(address(this), chaiAmount);
+    }
+
+    // PRIVATE FUNCTIONS
+
+    function __decodeCallArgs(bytes memory _encodedCallArgs)
+        private
+        pure
+        returns (uint256 outgoingAmount_, uint256 minIncomingAmount_)
+    {
+        return abi.decode(_encodedCallArgs, (uint256,uint256));
+    }
+}
---
+++ contracts/prices/derivatives/ChaiPriceSource.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.6.8;
+
+import "./IDerivativePriceSource.sol";
+
+/// @title Chai Price Source
+/// @author Melon Council DAO <security@meloncoucil.io>
+/// @notice Price source oracle for Chai
+contract ChaiPriceSource is IDerivativePriceSource {
+    address immutable public CHAI;
+    address immutable public DAI;
+    address immutable public DSR_POT;
+
+    constructor(address _chai, address _dai, address _dsrPot) public {
+        CHAI = _chai;
+        DAI = _dai;
+        DSR_POT = _dsrPot;
+    }
+
+    /// @notice Gets the rates for 1 unit of the derivative to its underlying assets
+    /// @param _derivative The derivative for which to get the rates
+    /// @return underlyings_ The underlying assets for the _derivative
+    /// @return rates_ The rates for the _derivative to the _underlyings
+    function getRatesToUnderlyings(address _derivative)
+        external
+        override
+        returns (address[] memory underlyings_, uint256[] memory rates_)
+    {
+        require(_derivative == CHAI, "getRatesToUnderlyings: only Chai is supported");
+
+        underlyings_ = new address[](1);
+        underlyings_[0] = DAI;
+        rates_ = new uint256[](1);
+        rates_[0] = __calcChaiRate();
+    }
+
+    /// @dev Calculation based on Chai source: https://github.com/dapphub/chai/blob/master/src/chai.sol
+    function __calcChaiRate() private returns (uint256) {
+        IPot pot = IPot(DSR_POT);
+        uint256 chi = (now > pot.rho()) ? pot.drip() : pot.chi();
+        return chi / 10 ** 9; // Refactor of mul(chi, 10 ** 18) / 10 ** 27
+    }
+}
+
+/// @notice Limited interface for Maker DSR's Pot contract
+/// @dev See DSR integration guide: https://github.com/makerdao/developerguides/blob/master/dai/dsr-integration-guide/dsr-integration-guide-01.md
+interface IPot {
+    function chi() external returns (uint256);
+    function rho() external returns (uint256);
+    function drip() external returns (uint256);
+}
