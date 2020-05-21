dff0f8dae98a075d609e5eff944f70c882449f4e
Travis Jacobs <7369273+travs@users.noreply.github.com>
2020-05-21 14:51:40 -0230

    chore: add mainnet addresses (thirdparty)

---
+++ mainnet_thirdparty_contracts.js
@@ -0,0 +1,46 @@
+module.exports = {
+  'kyber': {
+    'ConversionRates': '0x798AbDA6Cc246D0EDbA912092A2a3dBd3d11191B',
+    'KyberReserve': '0x63825c174ab367968EC60f061753D3bbD36A0D8F',
+    'KyberNetwork': '0x9ae49C0d7F8F9EF4B864e004FE86Ac8294E20950',
+    'KyberNetworkProxy': '0x818E6FECD516Ecc3849DAf6845e3EC868087B755',
+    'KyberWhiteList': '0x6e106a75D369d09A9Ea1DCc16Da844792aA669a3',
+    'ExpectedRate': '0x96B610046D63638d970E6243151311d8827D69a5',
+    'FeeBurner': '0x52166528FCC12681aF996e409Ee3a421a4e128A3'
+  },
+  'oasis': {
+    'OasisDexExchange': '0x794e6e91555438aFc3ccF1c5076A74F42133d08D',
+  },
+  'uniswap': {
+    'UniswapFactory': '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'
+  },
+  'zeroExV2': {
+    'ZeroExV2Exchange': '0x080bf510fcbf18b91105470639e9561022937712',
+    'ZeroExV2ERC20Proxy': '0x95e6f48254609a6ee006f7d493c8e5fb97094cef'
+  },
+  'zeroExV3': {
+    'ZeroExV3Exchange': '0x61935cbdd02287b511119ddb11aeb42f1593b7ef',
+    'ZeroExV3ERC20Proxy': '0x95e6f48254609a6ee006f7d493c8e5fb97094cef',
+    'ZeroExV3Staking': '0x2a17c35ff147b32f13f19f2e311446eeb02503f3 ',
+    'ZeroExV3StakingProxy': '0xa26e80e7dea86279c6d778d702cc413e6cffa777',
+    'ZeroExV3ZrxVault': '0xba7f8b5fb1b19c1211c5d49550fcd149177a5eaf'
+  },
+  'tokens': {
+    'WETH': '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    'MLN': '0xec67005c4e498ec7f55e092bd1d35cbc47c91892',
+    'ANT': '0x960b236A07cf122663c4303350609A66A7B288C0',
+    'BAT': '0x0d8775f648430679a709e98d2b0cb6250d2887ef',
+    'DAI': '0x6b175474e89094c44da98b954eedeac495271d0f',
+    'KNC': '0xdd974d5c2e2928dea5f71b9825b8b646686bd200',
+    'LINK': '0x514910771af9ca656af840dff83e8264ecf986ca',
+    'MANA': '0x0f5d2fb29fb7d3cfee444a200298f468908cc942',
+    'MKR': '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
+    'REP': '0x1985365e9f78359a9B6AD760e32412f4a445E862',
+    'REN': '0x408e41876cccdc0f92210600ef50372656052a38',
+    'RLC': '0x607F4C5BB672230e8672085532f7e901544a7375',
+    'SAI' : '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+    'USDC': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    'WBTC': '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+    'ZRX': '0xe41d2489571d322189246dafa5ebde1f4699f498'
+  }
+}
