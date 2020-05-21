974f4257c813205dd27c1e26e1370080666e07fc
Travis Jacobs <7369273+travs@users.noreply.github.com>
2020-05-21 14:51:23 -0230

    chore: adjust jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -7,30 +7,55 @@ const Web3 = require('web3');
 jest.setTimeout(1200000); // 20 mins
 
 // TODO: factor these keys into one place
-const PRIV_KEY_1 = '0xd3fdff38aaf7be159fc1c12c66982fea997df08ca5b91b399e437370d3681721';
-const PRIV_KEY_2 = '0x9cc70449981c6df178133db4c075c408876e8be3b147fa11f8ee947faa0b0011';
-const PRIV_KEY_3 = '0x53f76b9ee429500aacf3730228ab4fdc72683e952b48a8c4a923c04203d93a56';
+const pkeys = [
+  // '0xd3fdff38aaf7be159fc1c12c66982fea997df08ca5b91b399e437370d3681721',
+  // '0x9cc70449981c6df178133db4c075c408876e8be3b147fa11f8ee947faa0b0011',
+  // '0x53f76b9ee429500aacf3730228ab4fdc72683e952b48a8c4a923c04203d93a56',
+  '0xd3fdff38aaf7be159fc1c12c66982fea997df08ca5b91b399e437370d3681721',
+  '0x9cc70449981c6df178133db4c075c408876e8be3b147fa11f8ee947faa0b0011',
+  '0x53f76b9ee429500aacf3730228ab4fdc72683e952b48a8c4a923c04203d93a56',
+  '0x1a4b1a2941ef98ab3e8aa83572bc81d8fe178a2d21ee42b888fd0597848746de',
+  '0xb5556102fef8ffc4e044cd6708039787ed97bb5860fd1bff2fcac88d77ff70eb',
+  '0x35dae058318c0869a4bd3acc659e345a60d71aa86d37b6ba299391e86d6e0b21',
+  '0xba830c02b1aff9bc5a6e194bab37a3865e26bc5b33402c6f292552c7f89f9732',
+  '0x6750fdc727aeb0854e57018b02e0e144820d15eed1a9e950fff367484e93aa48',
+  '0x071570dd341ce1056771af8f4efd0fe3a0874eacc10228b9c4d626b007102e21',
+  '0x2760966c32dd5179176ab8066895148f2bdfa3072427b9904660a0555d1c32e7'
+];
 
 global.startChain = async () => {
-  // const startingBalance = Web3.utils.toWei('1000', 'ether');
+  const startingBalance = Web3.utils.toWei('10000000', 'ether');
   const provider = ganache.provider({
     fork: 'http://127.0.0.1:8545',
-    // network_id: 1,
-    // accounts: [
-    //   {
-    //     secretKey: PRIV_KEY_1,
-    //     balance: startingBalance,
-    //   }, {
-    //     secretKey: PRIV_KEY_2,
-    //     balance: startingBalance,
-    //   }, {
-    //     secretKey: PRIV_KEY_3,
-    //     balance: startingBalance,
-    //   },
-    // ],
+    mnemonic: 'exhibit now news planet fame thank swear reform tilt accident bitter axis',
+    network_id: 1,
+    gasLimit: '0x989680',
+    accounts: [
+      {
+        secretKey: pkeys[0],
+        balance: startingBalance,
+      }, {
+        secretKey: pkeys[1],
+        balance: startingBalance,
+      }, {
+        secretKey: pkeys[2],
+        balance: startingBalance,
+      }, {
+        secretKey: pkeys[3],
+        balance: startingBalance,
+      }, {
+        secretKey: pkeys[4],
+        balance: startingBalance,
+      },
+    ],
   });
 
-  return new Web3(provider, null, { transactionConfirmationBlocks: 1 });
+  const web3 = new Web3(provider, null, { transactionConfirmationBlocks: 1 });
+
+  for (const pkey of pkeys) {
+    web3.eth.accounts.wallet.add(pkey);
+  }
+  return web3;
 }
 
 expect.extend({
