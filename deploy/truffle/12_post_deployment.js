2dbf09c6b87f4c3e2c19fa6a619c1f885463a1ac
Travis Jacobs <7369273+travs@users.noreply.github.com>
2020-05-25 14:02:04 -0230

    refactor: add post-deployment migration

--- deploy/truffle/12_post_deployment.js
+++ deploy/truffle/12_post_deployment.js
@@ -1,3 +1,9 @@
-module.exports = async () => {
+const mainnetAddrs = require('../../mainnet_thirdparty_contracts');
+const WETH = artifacts.require('WETH');
 
+module.exports = async _ => {
+  const [deployer, manager, investor] = await web3.eth.getAccounts();
+  const weth = await WETH.at(mainnetAddrs.tokens.WETH);
+  await weth.deposit({ value: web3.utils.toWei('10', 'ether')});
+  await weth.transfer(investor, web3.utils.toWei('10', 'ether'));
 }
