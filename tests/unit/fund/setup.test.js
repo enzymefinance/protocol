a435f2bbd750b8c6e8ad603766f5a992b7fd931a
Travis Jacobs <7369273+travs@users.noreply.github.com>
2020-06-04 10:10:26 -0230

    tests: fix setup.test.js

--- tests/unit/fund/setup.test.js
+++ tests/unit/fund/setup.test.js
@@ -1,24 +1,24 @@
 import { toWei } from 'web3-utils';
 import { send } from '~/deploy/utils/deploy-contract';
-import { partialRedeploy } from '~/deploy/scripts/deploy-system';
 import { CONTRACT_NAMES } from '~/tests/utils/constants';
-import getAccounts from '~/deploy/utils/getAccounts';
+import { getDeployed } from '~/tests/utils/getDeployed';
+import * as mainnetAddrs from '~/mainnet_thirdparty_contracts';
 
+let web3;
 let deployer, manager, user;
 let defaultTxOpts, managerTxOpts, userTxOpts;
 let fundFactory;
 
 beforeAll(async () => {
-  [deployer, manager, user] = await getAccounts();
+  web3 = await startChain();
+  [deployer, manager, user] = await web3.eth.getAccounts();
   defaultTxOpts = { from: deployer, gas: 8000000 };
   managerTxOpts = { ...defaultTxOpts, from: manager };
   userTxOpts = { ...defaultTxOpts, from: user };
 
-  const deployed = await partialRedeploy([CONTRACT_NAMES.FUND_FACTORY]);
-  const contracts = deployed.contracts;
-  fundFactory = contracts[CONTRACT_NAMES.FUND_FACTORY];
-  const weth = contracts.WETH;
-  const mln = contracts.MLN;
+  const mln = getDeployed(CONTRACT_NAMES.MLN, web3, mainnetAddrs.tokens.MLN);
+  const weth = getDeployed(CONTRACT_NAMES.WETH, web3, mainnetAddrs.tokens.WETH);
+  fundFactory = getDeployed(CONTRACT_NAMES.FUND_FACTORY, web3);
   
   await send(
     fundFactory,
@@ -32,7 +32,8 @@ beforeAll(async () => {
       weth.options.address,
       [mln.options.address, weth.options.address],
     ],
-    managerTxOpts
+    managerTxOpts,
+    web3
   );
 });
 
@@ -40,10 +41,10 @@ test('continue setup of a fund', async () => {
   const amguTxValue = toWei('0.01', 'ether')
   const userTxOptsWithAmgu = { ...userTxOpts, value: amguTxValue };
   
-  await send(fundFactory, 'createFeeManagerFor', [manager], userTxOptsWithAmgu);
-  await send(fundFactory, 'createPolicyManagerFor', [manager], userTxOptsWithAmgu);
-  await send(fundFactory, 'createSharesFor', [manager], userTxOptsWithAmgu);
-  await send(fundFactory, 'createVaultFor', [manager], userTxOptsWithAmgu);
-  const res = await send(fundFactory, 'completeFundSetupFor', [manager], userTxOptsWithAmgu);
+  await send(fundFactory, 'createFeeManagerFor', [manager], userTxOptsWithAmgu, web3);
+  await send(fundFactory, 'createPolicyManagerFor', [manager], userTxOptsWithAmgu, web3);
+  await send(fundFactory, 'createSharesFor', [manager], userTxOptsWithAmgu, web3);
+  await send(fundFactory, 'createVaultFor', [manager], userTxOptsWithAmgu, web3);
+  const res = await send(fundFactory, 'completeFundSetupFor', [manager], userTxOptsWithAmgu, web3);
   expect(res).toBeTruthy();
 });
