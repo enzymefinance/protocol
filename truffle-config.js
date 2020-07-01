a86ccc399b35ee9f61ef3cc0b99b2b1f0e61b72e
Sebastian Siemssen <fubhy@fubhy.com>
2020-07-01 10:13:28 +0200

    chore: remove compiler optimization settings

--- truffle-config.js
+++ truffle-config.js
@@ -5,19 +5,15 @@ module.exports = {
       docker: true,
       parser: 'solcjs',
       settings: {
-        optimizer: {
-          enabled: true,
-          runs: 200
-        },
         evmVersion: 'istanbul'
       }
     }
   },
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: '*' // Match any network id
     }
   },
   contracts_directory: './src',
