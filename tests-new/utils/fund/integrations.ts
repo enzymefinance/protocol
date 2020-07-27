9b1c9aba8c42283d0920fe19a536eadcb98de7d6
Sean Casey <sean@seanjcasey.com>
2020-07-27 19:04:07 -0400

    test: update IIntegrationAdapter type

--- tests-new/utils/fund/integrations.ts
+++ tests-new/utils/fund/integrations.ts
@@ -1,5 +1,40 @@
+import { Call, Contract } from '@crestproject/crestproject';
 import { ethers } from 'ethers';
 import { encodeArgs } from '../common';
+
+export interface IIntegrationAdapter {
+  parseAssetsForMethod: Call<
+    (
+      _selector: ethers.utils.BytesLike,
+      _encodedCallArgs: ethers.utils.BytesLike,
+    ) => {
+      spendAssets_: string[];
+      spendAssetAmounts_: ethers.BigNumber[];
+      incomingAssets_: string[];
+      minIncomingAssetAmounts_: ethers.BigNumber[];
+    }
+  >;
+}
+
+export async function assetTransferArgs(
+  adapter: IIntegrationAdapter,
+  selector: ethers.utils.BytesLike,
+  encodedCallArgs: ethers.utils.BytesLike,
+) {
+  const {
+    spendAssets_,
+    spendAssetAmounts_,
+    incomingAssets_,
+  } = await adapter.parseAssetsForMethod(selector, encodedCallArgs);
+  console.log('spendAssets_', spendAssets_);
+  console.log('spendAssetAmounts_', spendAssetAmounts_);
+  console.log('incomingAssets_', incomingAssets_);
+
+  return encodeArgs(
+    ['address[]', 'uint[]', 'address[]'],
+    [spendAssets_, spendAssetAmounts_, incomingAssets_],
+  );
+}
 
 export function kyberTakeOrder(
   incomingAsset: string,
