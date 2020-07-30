a17fc103ade22bf72e62f9c649aa5eb0749781ee
Sebastian Siemssen <fubhy@fubhy.com>
2020-07-30 16:24:55 +0200

    test: remove old tests and refactor and cleanup repo

---
+++ tests/contracts.ts
@@ -0,0 +1,44 @@
+// Fund
+export { FundFactory } from './codegen/FundFactory';
+export { FeeManager } from './codegen/FeeManager';
+export { PolicyManager } from './codegen/PolicyManager';
+export { Shares } from './codegen/Shares';
+export { Vault } from './codegen/Vault';
+export { Hub } from './codegen/Hub';
+
+// Core
+export { Registry } from './codegen/Registry';
+export { Engine } from './codegen/Engine';
+export { KyberPriceFeed } from './codegen/KyberPriceFeed';
+export { ValueInterpreter } from './codegen/ValueInterpreter';
+export { SharesRequestor } from './codegen/SharesRequestor';
+export { FeeManagerFactory } from './codegen/FeeManagerFactory';
+export { ManagementFee } from './codegen/ManagementFee';
+export { PerformanceFee } from './codegen/PerformanceFee';
+export { PolicyManagerFactory } from './codegen/PolicyManagerFactory';
+export { SharesFactory } from './codegen/SharesFactory';
+export { VaultFactory } from './codegen/VaultFactory';
+
+// Adapters
+export { AirSwapAdapter } from './codegen/AirSwapAdapter';
+export { KyberAdapter } from './codegen/KyberAdapter';
+export { EngineAdapter } from './codegen/EngineAdapter';
+export { OasisDexAdapter } from './codegen/OasisDexAdapter';
+export { UniswapAdapter } from './codegen/UniswapAdapter';
+export { UniswapV2Adapter } from './codegen/UniswapV2Adapter';
+export { ZeroExV2Adapter } from './codegen/ZeroExV2Adapter';
+export { ZeroExV3Adapter } from './codegen/ZeroExV3Adapter';
+
+// Policies
+export { UserWhitelist } from './codegen/UserWhitelist';
+export { AssetBlacklist } from './codegen/AssetBlacklist';
+export { AssetWhitelist } from './codegen/AssetWhitelist';
+export { MaxConcentration } from './codegen/MaxConcentration';
+export { MaxPositions } from './codegen/MaxPositions';
+export { PriceTolerance } from './codegen/PriceTolerance';
+
+// Testing
+export { ERC20 } from './codegen/ERC20';
+export { WETH } from './codegen/WETH';
+export { PreminedToken } from './codegen/PreminedToken';
+export { MockKyberNetwork } from './codegen/MockKyberNetwork';
