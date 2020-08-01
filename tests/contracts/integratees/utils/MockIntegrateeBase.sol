3954f3b8a8129521019bca0c8030537bdeb748fb
Sean Casey <sean@seanjcasey.com>
2020-08-01 15:53:20 -0400

    test: refactor MockIntegratee into rate provider base contracts to be re-used as price source bases

--- tests/contracts/integratees/utils/MockIntegrateeBase.sol
+++ tests/contracts/integratees/utils/MockIntegrateeBase.sol
@@ -1,64 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.6.8;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../../utils/NormalizedRateProviderBase.sol";
 
-abstract contract MockIntegrateeBase {
-    using SafeMath for uint256;
+abstract contract MockIntegrateeBase is NormalizedRateProviderBase {
 
-    address constant ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    uint256 constant ETH_DECIMALS = 18;
-    uint256 constant RATE_NORMALIZATION_DECIMALS = 18;
-
-    mapping (address => mapping(address => uint256)) assetToAssetRate;
-
-    function setRate(address _baseAsset, address _quoteAsset, uint256 _rate) external {
-        assetToAssetRate[_baseAsset][_quoteAsset] = _rate;
-    }
-
-    function __calcDenormalizedQuoteAssetAmount(
-        uint256 _baseAssetDecimals,
-        uint256 _baseAssetAmount,
-        uint256 _quoteAssetDecimals,
-        uint256 _rate
+    constructor(
+        address[] memory _specialAssets,
+        uint8[] memory _specialAssetDecimals,
+        uint256 _ratePrecision
     )
-        internal
-        pure
-        returns (uint256)
-    {
-        return _rate.mul(_baseAssetAmount).div(
-            10 ** (RATE_NORMALIZATION_DECIMALS.add(_baseAssetDecimals).sub(_quoteAssetDecimals))
-        );
-    }
-
-    function __getDecimalsForAsset(address _asset) internal view returns (uint256) {
-        if (_asset == ETH_ADDRESS) {
-            return ETH_DECIMALS;
-        }    
-        return uint256(ERC20(_asset).decimals());
-    }
-
-    function __getRate(address _baseAsset, address _quoteAsset) internal view returns (uint256) {
-        if (_baseAsset == _quoteAsset) {
-            return 10 ** RATE_NORMALIZATION_DECIMALS;
-        }
-
-        // 1. Check for a direct rate
-        uint256 directRate = assetToAssetRate[_baseAsset][_quoteAsset];
-        if (directRate > 0) {
-            return directRate;
-        }
-
-        // 2. Check for inverse direct rate
-        uint256 iDirectRate = assetToAssetRate[_quoteAsset][_baseAsset];
-        if (iDirectRate > 0) {
-            return 10 ** (RATE_NORMALIZATION_DECIMALS.mul(2)).div(iDirectRate);
-        }
-
-        // 4. Else return 1
-        return 10 ** RATE_NORMALIZATION_DECIMALS;
-    }
+        public
+        NormalizedRateProviderBase(_specialAssets, _specialAssetDecimals, _ratePrecision)
+    {}
 
     function __swap(
         address[] memory _assetsToIntegratee,
