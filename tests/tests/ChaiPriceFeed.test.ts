09aa034ffd867bbb0f69e899f87723c1db8a60b8
Sebastian Siemssen <fubhy@fubhy.com>
2020-08-07 13:33:42 +0200

    test: more chai price source tests

--- tests/tests/ChaiPriceFeed.test.ts
+++ tests/tests/ChaiPriceFeed.test.ts
@@ -1,5 +1,6 @@
-import { BuidlerProvider } from '@crestproject/crestproject';
+import { BuidlerProvider, randomAddress } from '@crestproject/crestproject';
 import { configureTestDeployment } from '../deployment';
+import { BigNumber, utils } from 'ethers';
 
 let tx;
 
@@ -31,5 +32,75 @@ describe('ChaiPriceFeed', () => {
     });
   });
 
-  it.todo('Write more tests!');
+  describe('getRatesToUnderlyings', () => {
+    it('only supports chai', async () => {
+      const {
+        system: { chaiPriceFeed },
+        config: {
+          tokens: { chai },
+        },
+      } = await provider.snapshot(snapshot);
+      const derivative = randomAddress();
+
+      tx = chaiPriceFeed.getRatesToUnderlyings(derivative);
+      await expect(tx).rejects.toBeRevertedWith('only Chai is supported');
+
+      tx = chaiPriceFeed.getRatesToUnderlyings(chai);
+      await expect(tx).resolves.toBeReceipt();
+    });
+
+    it('returns rate for underlying dai', async () => {
+      const {
+        system: { chaiPriceFeed },
+        config: {
+          tokens: { chai, dai },
+        },
+      } = await provider.snapshot(snapshot);
+
+      tx = chaiPriceFeed.getRatesToUnderlyings(chai);
+      await expect(tx).resolves.toBeReceipt();
+
+      tx = await chaiPriceFeed.getRatesToUnderlyings.args(chai).call();
+      expect(tx).toMatchObject({
+        // NOTE: The rate changes by 1% on every call to drip().
+        rates_: [utils.parseEther('0.99')],
+        underlyings_: [dai.address],
+      });
+
+      tx = chaiPriceFeed.getRatesToUnderlyings(chai);
+      await expect(tx).resolves.toBeReceipt();
+
+      tx = await chaiPriceFeed.getRatesToUnderlyings.args(chai).call();
+      expect(tx).toMatchObject({
+        // NOTE: The rate changes by 1% on every call to drip().
+        rates_: [utils.parseEther('0.9801')],
+        underlyings_: [dai.address],
+      });
+    });
+
+    it('calls drip() if necessary', async () => {
+      const {
+        system: { chaiPriceFeed },
+        config: {
+          tokens: { chai },
+          mocks: {
+            priceSources: { chai: chaiPriceSource },
+          },
+        },
+      } = await provider.snapshot(snapshot);
+
+      const before = await provider.getBlock('latest');
+      tx = chaiPriceSource.rho();
+      await expect(tx).resolves.toBeLteBigNumber(before.timestamp);
+
+      tx = chaiPriceFeed.getRatesToUnderlyings(chai);
+      await expect(tx).resolves.toBeReceipt();
+
+      const after = await provider.getBlock('latest');
+      tx = chaiPriceSource.rho();
+      await expect(tx).resolves.toBeGteBigNumber(after.timestamp);
+
+      expect(chaiPriceSource.drip).toHaveBeenCalledOnContract();
+    });
+  });
 });
