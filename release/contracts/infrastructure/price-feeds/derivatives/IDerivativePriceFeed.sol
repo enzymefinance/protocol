4ea6b93eda2ad180e72d43569a8f37cb768f9c80
Sean Casey <sean@seanjcasey.com>
2020-09-01 18:25:43 -0400

    chore: add note about rate normalization to price feed interfaces (#7)

--- release/contracts/infrastructure/price-feeds/derivatives/IDerivativePriceFeed.sol
+++ release/contracts/infrastructure/price-feeds/derivatives/IDerivativePriceFeed.sol
@@ -4,6 +4,7 @@ pragma solidity 0.6.8;
 /// @title IDerivativePriceFeed Interface
 /// @author Melon Council DAO <security@meloncoucil.io>
 /// @notice Simple interface for derivative price source oracle implementations
+/// @dev IMPORTANT: all rates must be "normalized" to 18 decimals
 interface IDerivativePriceFeed {
     function getRatesToUnderlyings(address) external returns (address[] memory, uint256[] memory);
 
