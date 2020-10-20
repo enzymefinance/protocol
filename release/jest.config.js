558c3efea12c56f45416ac1da953c898913ca5a0
Sebastian Siemssen <fubhy@fubhy.com>
2020-10-20 10:34:44 +0200

    chore: use archive node for e2e tests (#73)

--- release/jest.config.js
+++ release/jest.config.js
@@ -1,5 +1,43 @@
 const { whales } = require('./utils/network/config.json');
 
+if (!process.env.MAINNET_ARCHIVE_NODE) {
+  console.warn('=====================================================');
+  console.warn('WARNING: Skipping end-to-end tests.');
+  console.warn('');
+  console.warn(
+    'You must specify a mainnet archive node endpoint (MAINNET_ARCHIVE_NODE) to run the end-to-end tests.',
+  );
+  console.warn('=====================================================');
+}
+
+const e2e = {
+  displayName: 'e2e',
+  preset: '@crestproject/ganache',
+  testMatch: ['**/?(*.)+(e2e).[jt]s?(x)'],
+  testTimeout: 120000,
+  testEnvironmentOptions: {
+    ganacheProviderOptions: {
+      gasLimit: 0x989680,
+      default_balance_ether: 10000000000000,
+      unlocked_accounts: Object.values(whales),
+      fork_block_number: 11091788,
+      fork: process.env.MAINNET_ARCHIVE_NODE,
+    },
+  },
+};
+
+const unit = {
+  displayName: 'unit',
+  preset: '@crestproject/buidler',
+  testEnvironmentOptions: {
+    buidlerConfigs: [
+      require.resolve('./buidler.config'),
+      require.resolve('@melonproject/persistent/buidler.config'),
+      require.resolve('@melonproject/utils/buidler.config'),
+    ],
+  },
+};
+
 module.exports = {
   globals: {
     'ts-jest': {
@@ -9,37 +47,5 @@ module.exports = {
     },
   },
 
-  projects: [
-    {
-      displayName: 'ganache',
-      preset: '@crestproject/ganache',
-      testMatch: ['**/?(*.)+(e2e).[jt]s?(x)'],
-      testTimeout: 120000,
-      testEnvironmentOptions: {
-        ganacheProviderOptions: {
-          gasLimit: 0x989680,
-          default_balance_ether: 10000000000000,
-          fork: 'https://mainnet.infura.io/v3/ffe0be31d7d34594b3decc6c7778e9ad',
-          // NOTE: You can unlock arbitrary accounts to gather their tokens into our
-          // test accounts and then trade with them for instance.
-          unlocked_accounts: Object.values(whales),
-          // NOTE: Currently we can only test on the latest block because we do not have
-          // a paid account on infura. This is bad because we want our tests to run within
-          // a predictable environment so we should definitely set the fork block number.
-          // fork_block_number: 12345
-        },
-      },
-    },
-    {
-      displayName: 'buidler',
-      preset: '@crestproject/buidler',
-      testEnvironmentOptions: {
-        buidlerConfigs: [
-          require.resolve('./buidler.config'),
-          require.resolve('@melonproject/persistent/buidler.config'),
-          require.resolve('@melonproject/utils/buidler.config'),
-        ],
-      },
-    },
-  ],
+  projects: process.env.MAINNET_ARCHIVE_NODE ? [unit, e2e] : [unit],
 };
