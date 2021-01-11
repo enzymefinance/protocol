e723dcea1e496052f93e7826646b5aae6c6975ab
Sean Casey <sean@seanjcasey.com>
2021-01-11 09:56:11 -0400

    refactor: add copyright and license description to all contracts (#321)

--- contracts/release/interfaces/IChai.sol
+++ contracts/release/interfaces/IChai.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0
+
+/*
+    This file is part of the Enzyme Protocol.
+
+    (c) Enzyme Council <council@enzyme.finance>
+
+    For the full license information, please view the LICENSE
+    file that was distributed with this source code.
+*/
+
 pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
