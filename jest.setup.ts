9a3448eb232545f7ed4fb9f0f60dd0ad6afb9c60
Sebastian Siemssen <fubhy@fubhy.com>
2021-06-22 22:16:02 +0200

    chore: update dependencies for new and updated jest assertions (#491)

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,10 +1,10 @@
-import { setBeAroundBigNumberAssertionTolerance, setGasCostAssertionTolerance } from '@enzymefinance/hardhat';
+import { setToBeAroundBigNumberTolerance, setToCostLessThanTolerance } from '@enzymefinance/hardhat';
 import { deployProtocolFixture, unlockAllWhales } from '@enzymefinance/testutils';
 
-setBeAroundBigNumberAssertionTolerance(0.01); // 1%
-setGasCostAssertionTolerance(0.03); // 3%
+setToBeAroundBigNumberTolerance(0.01); // 1%
+setToCostLessThanTolerance(0.03); // 3%
 
 beforeAll(async () => {
-  global.whales = await unlockAllWhales();
-  global.fork = await deployProtocolFixture();
+  (global as any).whales = await unlockAllWhales();
+  (global as any).fork = await deployProtocolFixture();
 });
