91f3b146ae6667981a14c3918efb2917b7b6435f
Sebastian Siemssen <fubhy@fubhy.com>
2022-03-11 16:17:38 +0100

    chore: add task to verify convex wrapper (#817)

--- tasks/verify.ts
+++ tasks/verify.ts
@@ -48,3 +48,22 @@ task('verify-comptroller-proxy', 'Verifies the comptroller proxy contract')
       contract: 'contracts/release/core/fund/comptroller/ComptrollerProxy.sol:ComptrollerProxy',
     });
   });
+
+task('verify-convex-wrapper', 'Verifies a convex wrapper proxy contract')
+  .addPositionalParam('factory', 'The proxy factory contract address')
+  .addPositionalParam('pid', 'The pool id')
+  .setAction(async (params, hre) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { ConvexCurveLpStakingWrapperFactory, encodeFunctionData } = require('@enzymefinance/protocol');
+
+    const contract = new ConvexCurveLpStakingWrapperFactory(params.factory, hre.ethers.provider);
+    const wrapper = await contract.getWrapperForConvexPool(params.pid);
+
+    const init = encodeFunctionData(utils.FunctionFragment.fromString('init(uint256)'), [params.pid]);
+
+    await hre.run('verify:verify', {
+      address: wrapper,
+      constructorArguments: [init, params.factory],
+      contract: 'contracts/release/utils/beacon-proxy/BeaconProxy.sol:BeaconProxy',
+    });
+  });
