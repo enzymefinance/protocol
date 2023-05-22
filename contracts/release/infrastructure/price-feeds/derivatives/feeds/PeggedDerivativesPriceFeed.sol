61067d5d20f95f2229f842e5f336bbcd783fab73
Sean Casey <sean@seanjcasey.com>
2023-05-22 10:00:20 -0400

    feat: price feed for derivatives pegged to single underlyings

---
+++ contracts/release/infrastructure/price-feeds/derivatives/feeds/PeggedDerivativesPriceFeed.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/*
+    This file is part of the Enzyme Protocol.
+
+    (c) Enzyme Council <council@enzyme.finance>
+
+    For the full license information, please view the LICENSE
+    file that was distributed with this source code.
+*/
+
+pragma solidity 0.6.12;
+
+import "./utils/PeggedDerivativesPriceFeedBase.sol";
+
+/// @title PeggedDerivativesPriceFeed Contract
+/// @author Enzyme Council <security@enzyme.finance>
+/// @notice Price feed for multiple derivatives that are pegged 1:1 to their underlyings,
+/// and have the same decimals as their underlying
+abstract contract PeggedDerivativesPriceFeed is PeggedDerivativesPriceFeedBase {
+    constructor(address _fundDeployer) public PeggedDerivativesPriceFeedBase(_fundDeployer) {}
+}
