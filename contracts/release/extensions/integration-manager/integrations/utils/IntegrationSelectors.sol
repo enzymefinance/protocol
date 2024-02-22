3025c5517d911e657ab04588ad9357cc7edf630d
Sean Casey <sean@seanjcasey.com>
2024-02-22 07:40:19 -0400

    refactor: change integration selectors to internal visibility

--- contracts/release/extensions/integration-manager/integrations/utils/IntegrationSelectors.sol
+++ contracts/release/extensions/integration-manager/integrations/utils/IntegrationSelectors.sol
@@ -17,21 +17,22 @@ pragma solidity >=0.6.0 <0.9.0;
 /// @dev Selectors are created from their signatures rather than hardcoded for easy verification
 abstract contract IntegrationSelectors {
     // Trading
-    bytes4 public constant TAKE_MULTIPLE_ORDERS_SELECTOR = bytes4(keccak256("takeMultipleOrders(address,bytes,bytes)"));
-    bytes4 public constant TAKE_ORDER_SELECTOR = bytes4(keccak256("takeOrder(address,bytes,bytes)"));
+    bytes4 internal constant TAKE_MULTIPLE_ORDERS_SELECTOR =
+        bytes4(keccak256("takeMultipleOrders(address,bytes,bytes)"));
+    bytes4 internal constant TAKE_ORDER_SELECTOR = bytes4(keccak256("takeOrder(address,bytes,bytes)"));
 
     // Lending
-    bytes4 public constant LEND_SELECTOR = bytes4(keccak256("lend(address,bytes,bytes)"));
-    bytes4 public constant REDEEM_SELECTOR = bytes4(keccak256("redeem(address,bytes,bytes)"));
+    bytes4 internal constant LEND_SELECTOR = bytes4(keccak256("lend(address,bytes,bytes)"));
+    bytes4 internal constant REDEEM_SELECTOR = bytes4(keccak256("redeem(address,bytes,bytes)"));
 
     // Staking
-    bytes4 public constant STAKE_SELECTOR = bytes4(keccak256("stake(address,bytes,bytes)"));
-    bytes4 public constant UNSTAKE_SELECTOR = bytes4(keccak256("unstake(address,bytes,bytes)"));
+    bytes4 internal constant STAKE_SELECTOR = bytes4(keccak256("stake(address,bytes,bytes)"));
+    bytes4 internal constant UNSTAKE_SELECTOR = bytes4(keccak256("unstake(address,bytes,bytes)"));
 
     // Rewards
-    bytes4 public constant CLAIM_REWARDS_SELECTOR = bytes4(keccak256("claimRewards(address,bytes,bytes)"));
+    bytes4 internal constant CLAIM_REWARDS_SELECTOR = bytes4(keccak256("claimRewards(address,bytes,bytes)"));
 
     // Combined
-    bytes4 public constant LEND_AND_STAKE_SELECTOR = bytes4(keccak256("lendAndStake(address,bytes,bytes)"));
-    bytes4 public constant UNSTAKE_AND_REDEEM_SELECTOR = bytes4(keccak256("unstakeAndRedeem(address,bytes,bytes)"));
+    bytes4 internal constant LEND_AND_STAKE_SELECTOR = bytes4(keccak256("lendAndStake(address,bytes,bytes)"));
+    bytes4 internal constant UNSTAKE_AND_REDEEM_SELECTOR = bytes4(keccak256("unstakeAndRedeem(address,bytes,bytes)"));
 }
